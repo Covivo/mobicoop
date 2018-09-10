6e2299d1575e4b7479eac2e4bf08d39597753464
MatthD <matthias.dieudonne@gmail.com>
2018-09-10 10:26:25 +0200

    PHPLinter work

--- interfaces/mobicoop/config/postinstall.js
+++ interfaces/mobicoop/config/postinstall.js
@@ -4,11 +4,17 @@
 const request = require('request-promise-native');
 const fs = require('fs');
 
+const fileOptions = {
+  url: 'https://cs.sensiolabs.org/download/php-cs-fixer-v2.phar',
+  encoding: null
+};
+
 // Create the .phar file to check symfony lint
 let phpCsFixer = fs.createWriteStream('vendor/bin/php-cs-fixer.phar');
-request('https://cs.sensiolabs.org/download/php-cs-fixer-v2.phar')
+request(fileOptions)
 .then(function (body) {
   phpCsFixer.write(body);
+  phpCsFixer.end();
 }).catch(function (err) {
   console.error(`an error happend while downlaod php-cs-fixer ${err}`)
 })
