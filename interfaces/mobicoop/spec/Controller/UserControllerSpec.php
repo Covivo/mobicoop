ba689e4cf87ffd4a03778ee248ccbb28a83bea5f
MatthD <matthias.dieudonne@gmail.com>
2018-10-03 12:11:06 +0200

    Pantehr + Client + Kernel ... ok for functionnal tests

--- interfaces/mobicoop/spec/Controller/UserControllerSpec.php
+++ interfaces/mobicoop/spec/Controller/UserControllerSpec.php
@@ -3,16 +3,17 @@
 namespace App\Spec\Controller;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\DomCrawler\Form;
-use Goutte\Client;
 
-const LOCAL_PATH = 'http://localhost:8081';
+const LOCAL_PORT = '4242';
+const LOCAL_IP = '127.0.0.1';
+const LOCAL_URL = 'http://'.LOCAL_IP.':'.LOCAL_PORT;
 
 /* Functional tests */
 describe('UserController', function () {
     // Given is used to save variables in context👌
-    given('client',function(){
-        return new Client();
-    });
+    // given('client',function(){
+    //     return new Client();
+    // });
     describe('/users', function () {
         it('Users page should return status code 200', function () {
             $request = $this->request->create('/users', 'GET');
@@ -25,26 +26,39 @@ describe('UserController', function () {
             $request = $this->request->create('/', 'GET');
             $response = $this->kernel->handle($request);
             $status = $response->getStatusCode();
-            $crawler = new Crawler($response->getContent(),LOCAL_PATH);
+            $crawler = new Crawler($response->getContent(),LOCAL_URL);
             $link = $crawler->selectLink('Users')->link();
             $uri = $link->getUri();
-            expect($uri)->toEqual(LOCAL_PATH . '/users');
-            expect($uri)->not->toEqual(LOCAL_PATH . '/random');
+
+            expect($uri)->toEqual(LOCAL_URL . '/users');
+            expect($uri)->not->toEqual(LOCAL_URL . '/random');
         });
-        it('Should be able to really access /users pages', function(){
-                $realCrawler = $this->client->request('GET', LOCAL_PATH . '/users');
-                $link = $realCrawler->selectLink('Users')->link();
-                $realCrawler = $this->client->click($link);
-                $h1 = $realCrawler->filter('h1.title')->text();
-                expect($realCrawler->getUri())->toBe(LOCAL_PATH.'/users');
-                expect(trim($h1))->toBe('Coviride Demo application - Users');
+        it('Should be able to access /users pages with a client', function(){
+            $realCrawler = $this->client->request('GET', LOCAL_URL . '/users');
+            $link = $realCrawler->selectLink('Users')->link();
+            $realCrawler = $this->client->click($link);
+            $h1 = $realCrawler->filter('h1.title')->text();
+            // $this->http->takeScreenshot('screen.png'); //I let this here for an exemple 🤪
+
+            expect($realCrawler->getUri())->toBe(LOCAL_URL.'/users');
+            expect(trim($h1))->toBe('Coviride Demo application - Users');
         });
+        // This test is used for functionnal with a real navigator tests
+        // it('Should be able to really access /users pages with a navigator', function(){
+        //     $realCrawler = $this->panther->request('GET', LOCAL_URL . '/users');
+        //     $link = $realCrawler->selectLink('Users')->link();
+        //     $realCrawler = $this->panther->click($link);
+        //     $h1 = $realCrawler->filter('h1.title')->text();
+        //     // $this->http->takeScreenshot('screen.png'); //I let this here for an exemple 🤪
+
+        //     expect($realCrawler->getUri())->toBe(LOCAL_URL.'/users');
+        //     expect(trim($h1))->toBe('Coviride Demo application - Users');
+        // });
     });
     describe('/user', function () {
         it('User page without id should return status code 404', function () {
             $request = $this->request->create('/user', 'GET');
             $response = $this->kernel->handle($request);
-            
             $status = $response->getStatusCode();
             
             expect($status)->toEqual(404);
@@ -54,12 +68,11 @@ describe('UserController', function () {
         it('User create page should return status code 200 and contains a givenName form input', function () {
             $request = $this->request->create('/user/create', 'GET');
             $response = $this->kernel->handle($request);
-            
             $status = $response->getStatusCode();
-            $crawler = new Crawler($response->getContent(),'http://localhost:8081/user/create');
+            $crawler = new Crawler($response->getContent(),LOCAL_URL.'/user/create');
             $form = $crawler->filter('form')->form();
+
             expect($form->has('user_form[givenName]'))->toBe(true);
         });
     });
-
 });
