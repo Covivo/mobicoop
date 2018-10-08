971cb6577b2a2995ce1b32dbeb30f0df32259517
sofiane belaribi <sofiane.belaribi@etu.univ-lyon1.fr>
2018-10-08 16:38:19 +0200

    fixed functionnal tests with Panther

--- interfaces/mobicoop/kahlan-config.php
+++ interfaces/mobicoop/kahlan-config.php
@@ -35,10 +35,10 @@ Filters::apply($this, 'bootstrap', function($next) {
         $this->request = Request::createFromGlobals();
         $this->kernel  = new Kernel('test', false);
         $panther = new ExtendedPanther();
-        //$client = new Client($this->kernel);
+        $client = new Client($this->kernel);
         // Create webserver for functionnals advanced test
         $this->panther = $panther::createWebServer();
-        // $this->client = $client;
+         $this->client = $client;
     });
 
     return $next();
