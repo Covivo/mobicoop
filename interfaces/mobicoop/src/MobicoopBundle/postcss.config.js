4f551a93f81506e514cf219fb399116c0f981bc6
sofiane belaribi <sofiane.belaribi@etu.univ-lyon1.fr>
2018-10-10 14:26:41 +0200

    deleted bundle

--- interfaces/mobicoop/src/MobicoopBundle/postcss.config.js
+++
@@ -1,9 +0,0 @@
-/*
-* This file is use for post-css assert, for exemple to compile to all navigator 🤩
-*/
-module.exports = {
-  plugins: {
-    // add browserslist config to package.json (see below)
-    autoprefixer: {}
-  }
-}
