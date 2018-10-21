f5b6038115b445b5050b1276e4b48aa2a8aac671
MatthD <matthias.dieudonne@gmail.com>
2018-10-21 19:41:24 +0200

    Change wy to start interface

--- interfaces/mobicoop/webpack.config.js
+++ interfaces/mobicoop/webpack.config.js
@@ -18,7 +18,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-     .addEntry('mobicoop', './assets/js/app.js')
+     .addEntry('app', './assets/js/app.js')
 
     /*
      * FEATURE CONFIG
