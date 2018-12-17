eafd1c834afc574900e3c2052f4911c332ab5fbe
sofiane belaribi <sofiane.belaribi@etu.univ-lyon1.fr>
2018-12-17 11:00:42 +0100

    Rearrange gitlab-ci & cleanup js

--- helpers/hooks/download-bundle.js
+++ helpers/hooks/download-bundle.js
@@ -14,24 +14,24 @@ let branchRoot = getRepoInfo().branch;
 // Has already been clone .. just go out
 if (fs.existsSync(bundlePath)){
   console.log(kuler('Bundle has already been download .. nothing to do 😴','yellow'));
-  return;
+  process.exit(0);
 }
 
 git
-.silent(true)
-.clone('http://gitlab.com/mobicoop/MobicoopBundle', function (err,res) {
-  if(err){
-    console.error(kuler(err,'red'));
-    console.error(kuler('Cannot clone Bundle repo','red'));
-    process.exit(1);
-  }
-  console.log(kuler('Bundle has been cloned 👌','green'));
-  let bundleGit = require('simple-git')(bundlePath);
-  bundleGit.checkout(branchRoot,function(err,res){
-    if(err){
-      console.error(kuler(`Cannot change bundle branch there is no ${branchRoot} branch in bundle 🤯; please change the branch of the monorepo first 🤞`,'red'))
-      process.exit(1);
-    }
-    console.log(kuler(`Monorepo & bundle are now on the ${branchRoot} branch 💪`,'green'));
-  })
-})
+    .silent(true)
+    .clone('http://gitlab.com/mobicoop/MobicoopBundle', function (err,res) {
+      if(err){
+        console.error(kuler(err,'red'));
+        console.error(kuler('Cannot clone Bundle repo','red'));
+        process.exit(1);
+      }
+      console.log(kuler('Bundle has been cloned 👌','green'));
+      let bundleGit = require('simple-git')(bundlePath);
+      bundleGit.checkout(branchRoot,function(err,res){
+        if(err){
+          console.error(kuler(`Cannot change bundle branch there is no ${branchRoot} branch in bundle 🤯; please change the branch of the monorepo first 🤞`,'red'));
+          process.exit(1);
+        }
+        console.log(kuler(`Monorepo & bundle are now on the ${branchRoot} branch 💪`,'green'));
+      })
+    });
