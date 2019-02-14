ad68fb9d73c9a7e51c3765326b4ca9d1b3375374
Sylvain Briat <sylvain.briat@covivo.eu>
2019-02-14 13:34:36 +0100

    tests correction

--- interfaces/mobicoop/src/MobicoopBundle/spec/Controller/UserControllerSpec.php
+++ interfaces/mobicoop/src/MobicoopBundle/spec/Controller/UserControllerSpec.php
@@ -36,15 +36,6 @@ describe('UserController', function () {
     // given('client',function(){
     //     return new Client();
     // });
-    describe('/users', function () {
-        it('Users page should return status code 200', function () {
-            $request = $this->request->create('/users', 'GET');
-            $response = $this->kernel->handle($request);
-            $status = $response->getStatusCode();
-
-            expect($status)->toEqual(200);
-        });
-    });
     describe('/user', function () {
         it('User page without id should return status code 404', function () {
             $request = $this->request->create('/user', 'GET');
