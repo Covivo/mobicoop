de2d2c9a61636f66b86339e13eac82629ae84d51
MatthD <matthias.dieudonne@gmail.com>
2019-03-19 17:02:10 +0100

    Start refactoring to vue

--- interfaces/mobicoop/src/MobicoopBundle/Resources/assets/js/page/ad/create.js
+++ interfaces/mobicoop/src/MobicoopBundle/Resources/assets/js/page/ad/create.js
@@ -14,6 +14,36 @@ import Timepicker from '../../components/Timepicker';
 Vue.use(Buefy);
 
 new Vue({
+  delimiters: ['${', '}'],
   el: '#app',
-  components: { Geocomplete, Datepicker, Timepicker}
+  'template': '#ad-create-template',
+  components: { Geocomplete, Datepicker, Timepicker},
+  props:{
+    frequency: {
+      type: String,
+      default: ''
+    },
+    role: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: ''
+    },
+    outward: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      timeStart: new Date(),
+      timeReturn: new Date(),
+      days: ['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche']
+    };
+  },
+  mounted(){
+    console.log('freq', this.outward);
+  }
 })
