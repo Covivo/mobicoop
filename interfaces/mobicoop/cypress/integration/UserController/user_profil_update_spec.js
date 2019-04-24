b40888ac592903297ddf0e4dba7f6d743dff7aa1
celine <b3ck54@gmail.com>
2019-04-24 14:41:02 +0200

    Test Cypress

---
+++ interfaces/mobicoop/cypress/integration/UserController/user_profil_update_spec.js
@@ -0,0 +1,43 @@
+
+describe('Profil', () => {
+    it('user_login -> Connexion mobicoop -> localhost:8081', function() {
+        /* Home */
+        cy.visit('/')
+        cy.contains('Connexion').click()
+        cy.url().should('include', '/utilisateur/connexion')
+
+        /* Connexion */
+        // Email
+        cy.get('input[id=user_login_form_username]')
+        .should('have.attr','placeholder','Saisissez votre adresse email')
+        .type('totosmith@email.com')
+
+        // Password
+        cy.get('input[id=user_login_form_password]')
+        .should('have.attr','placeholder','Saisissez votre mot de passe')
+        .type('motdepasse')
+
+        cy.contains('Se connecter').click()
+  
+
+        /* Profil */
+        cy.contains('Mon profil').click()
+        cy.url().should('include', '/utilisateur/profil')
+
+        /* Update */
+        cy.contains('Mettre à jour').click()
+        cy.url().should('include', '/utilisateur/profil/modifier')
+
+        /* Gender */
+        cy.get('select[id=user_form_gender]')
+        .select('2')
+        .should('have.value', '2')
+
+        // Change phone number
+        cy.get('input[id=user_form_telephone]').clear()
+        .should('have.attr','placeholder','Saisissez votre numéro de téléphone')
+        .type('0610111214')
+
+        cy.get('button[id=user_form_submit]').click()   // à mettre modifier, valider....
+    })
+})
