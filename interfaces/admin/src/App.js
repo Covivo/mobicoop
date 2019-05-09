aa1096b0eb1f587a91ac5e49d393d5ba29bd4aba
Sylvain Briat <sylvain.briat@covivo.eu>
2019-05-09 21:18:41 +0200

    modify theme

--- interfaces/admin/src/App.js
+++ interfaces/admin/src/App.js
@@ -15,10 +15,21 @@ import { CommunityShow } from './Component/Community/Show';
 import { CommunityEdit } from './Component/Community/Edit';
 import { CommunityCreate } from './Component/Community/Create';
 import { CommunityList } from './Component/Community/List';
+import { cyan, lightBlue, teal } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
     palette: {
-        type: 'light'
+      primary: cyan,
+      secondary: lightBlue,
+      error: teal,
+      // Used by `getContrastText()` to maximize the contrast between the background and
+      // the text.
+      contrastThreshold: 3,
+      // Used to shift a color's luminance by approximately
+      // two indexes within its tonal palette.
+      // E.g., shift from Red 500 to Red 300 or Red 700.
+      tonalOffset: 0.2,
+      type: 'light'
     },
 });
 
