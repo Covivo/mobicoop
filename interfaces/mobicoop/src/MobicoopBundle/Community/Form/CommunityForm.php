06d530bd5e1404c3f3d4c05c6d960681eff183be
Remi Wortemann <remi.wortemann@gmail.com>
2019-05-23 14:53:23 +0200

    create community view and form

---
+++ interfaces/mobicoop/src/MobicoopBundle/Community/Form/CommunityForm.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Copyright (c) 2018, MOBICOOP. All rights reserved.
+ * This project is dual licensed under AGPL and proprietary licence.
+ ***************************
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <gnu.org/licenses>.
+ ***************************
+ *    Licence MOBICOOP described in the file
+ *    LICENSE
+ **************************/
+
+namespace Mobicoop\Bundle\MobicoopBundle\Community\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Validator\Constraints\GroupSequence;
+use Mobicoop\Bundle\MobicoopBundle\Community\Entity\Community;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Mobicoop\Bundle\MobicoopBundle\Geography\Form\AddressForm;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+
+/**
+ * Community form.
+ *
+ */
+class CommunityForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('name')
+        ->add('private')
+        ->add('description')
+        ->add('fullDescription')
+        ->add('submit', SubmitType::class);
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Community::class
+        ]);
+    }
+}
