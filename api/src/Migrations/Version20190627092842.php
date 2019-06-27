400d6eea83865473a2626a8d78c8f55dc069c38d
Sylvain Briat <sylvain.briat@covivo.eu>
2019-06-27 11:58:54 +0200

    add calculation and analyzing dates; add analyze and matching in massManager

---
+++ api/src/Migrations/Version20190627092842.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190627092842 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE mass ADD analyzed_date DATETIME DEFAULT NULL, ADD calculated_date DATETIME DEFAULT NULL, CHANGE analyze_date analyzing_date DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE mass ADD analyze_date DATETIME DEFAULT NULL, DROP analyzing_date, DROP analyzed_date, DROP calculated_date');
+    }
+}
