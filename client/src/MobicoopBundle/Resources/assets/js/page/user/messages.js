6a4cfaf7bb9222aa0e5ed0f27241fe05dfc3d8da
maxime <maxime.bardot@gmail.com>
2019-07-11 09:13:02 +0200

    Assets for user/messages

--- client/src/MobicoopBundle/Resources/assets/js/page/user/messages.js
+++ client/src/MobicoopBundle/Resources/assets/js/page/user/messages.js
@@ -1,7 +1,7 @@
 'use strict';
 import Vue from 'vue'
-import Vuetify from 'vuetify'
+//import Vuetify from 'vuetify'
 import '../../../css/page/user/messages.scss';
 
-Vue.use(Vuetify)
+//Vue.use(Vuetify)
 
