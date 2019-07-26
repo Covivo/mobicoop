1ff3b8b70536b890d70a30d435a663073739db7a
maxime <maxime.bardot@gmail.com>
2019-07-26 10:45:21 +0200

    New architecture

--- client/src/MobicoopBundle/Resources/assets/js/page/user/messages.js
+++ client/src/MobicoopBundle/Resources/assets/js/page/user/messages.js
@@ -1,18 +1,32 @@
 'use strict';
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import VueI18n from 'vue-i18n'
 import 'vuetify/dist/vuetify.min.css'
 import '../../../css/page/user/messages.scss';
 import md from "material-design-icons-iconfont"
 
+// Vue components
+import Messages from '../../components/user/Messages';
+import Vueheader from '../../components/Vueheader';
+import Vuefooter from '../../components/Vuefooter';
+
+// import traductions
+import messages from '../../../../translations/translations.json';
+
+Vue.use(VueI18n)
 Vue.use(Vuetify)
 Vue.use(md)
 
-// Vue components
-import Messages from '../../components/user/Messages';
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: 'fr', // set locale
+  messages, // set locale messages
+});
 
 new Vue({
-  el: '#messages',
+  i18n,
+  el: '#app',
   vuetify: new Vuetify({
     theme: {
       themes: {
@@ -24,6 +38,8 @@ new Vue({
     },
   }),
   components: {
-    Messages
+    Messages,
+    Vueheader,
+    Vuefooter
   }
 })
