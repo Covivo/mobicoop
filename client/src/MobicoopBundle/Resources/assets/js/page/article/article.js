48a84dec2cd8d2f8a44ce42b2235fb7b6291a4c9
MatthD <matthias.dieudonne@gmail.com>
2019-07-29 14:35:50 +0200

    Fix communities pages indent + hyda + article & communities vuejs

--- client/src/MobicoopBundle/Resources/assets/js/page/article/article.js
+++ client/src/MobicoopBundle/Resources/assets/js/page/article/article.js
@@ -1,37 +1,19 @@
 'use strict';
 
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import VueI18n from 'vue-i18n'
-import 'vuetify/dist/vuetify.min.css'; 
-import md from "material-design-icons-iconfont"; 
+// any CSS you require will output into a single css file (app.css in this case)
+import { Vue, vuetify, i18n } from '../../config/vue-config'
 import '../../../css/page/article/article.scss';
 
 // Vue components
 import Vueheader from '../../components/Vueheader';
 import Vuefooter from '../../components/Vuefooter';
 
-// import traductions
-import messages from '../../../../translations/translations.json';
-
-Vue.use(VueI18n);
-Vue.use(Vuetify);
-Vue.use(md);
-
-// Create VueI18n instance with options
-const i18n = new VueI18n({
-  locale: 'fr', // set locale
-  messages, // set locale messages
-});
-
 new Vue({
-  i18n,
   el: '#app',
-  vuetify: new Vuetify(),
+  vuetify,
+  i18n,
   components: {
     Vueheader,
     Vuefooter
   }
 })
-
-
