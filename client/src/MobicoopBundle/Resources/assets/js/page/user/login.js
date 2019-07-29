4c8efa593b1ce3396d3d70c00c3ff3d59410067f
MatthD <matthias.dieudonne@gmail.com>
2019-07-29 11:37:23 +0200

    Fix login page vuetify

--- client/src/MobicoopBundle/Resources/assets/js/page/user/login.js
+++ client/src/MobicoopBundle/Resources/assets/js/page/user/login.js
@@ -1,33 +1,16 @@
 'use strict';
 
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import VueI18n from 'vue-i18n'
-import 'vuetify/dist/vuetify.min.css'; 
-import md from "material-design-icons-iconfont"; 
+import { Vue, vuetify, i18n } from '../../config/vue-config'
 import '../../../css/page/user/login.scss';
 
 // Vue components
 import Vueheader from '../../components/Vueheader';
 import Vuefooter from '../../components/Vuefooter';
 
-// import traductions
-import messages from '../../../../translations/translations.json';
-
-Vue.use(VueI18n);
-Vue.use(Vuetify);
-Vue.use(md);
-
-// Create VueI18n instance with options
-const i18n = new VueI18n({
-  locale: 'fr', // set locale
-  messages, // set locale messages
-});
-
 new Vue({
   i18n,
   el: '#app',
-  vuetify: new Vuetify(),
+  vuetify,
   components: {
     Vueheader,
     Vuefooter
