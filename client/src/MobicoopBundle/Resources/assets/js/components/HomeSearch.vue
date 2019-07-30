e749b92d02e54548ae9e4f5dce50e3c6f8fad2b0
Remi Wortemann <remi.wortemann@gmail.com>
2019-07-30 16:09:08 +0200

    HomeSearch component

--- client/src/MobicoopBundle/Resources/assets/js/components/HomeSearch.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/HomeSearch.vue
@@ -1,15 +1,143 @@
 <template>
-  <v-content>
-    <v-layout
-      align-center
-      justify-center
+  <v-content color="secondary">
+    <v-container
+      text-xs-center
+      grid-list-md
+      fluid
     >
-      <v-flex xs8>
-        <h1>{{ $t('title') }}</h1>
-        <h3 v-html="$t('subtitle')" />
-        <GeoComplete :url="geoSearchUrl" />
-      </v-flex>
-    </v-layout>
+      <v-layout
+        justify-center
+        align-center
+        class="mt-5"
+      >
+        <v-flex xs8>
+          <h1>
+            {{ $t('title') }}
+          </h1>
+          <h3
+            v-html="$t('subtitle')"
+          />
+        </v-flex>
+      </v-layout>
+      <v-layout
+        class="mt-5"
+        align-center
+      >
+        <v-flex
+          xs3
+          offset-xs2
+        >
+          <GeoComplete :url="geoSearchUrl" />
+        </v-flex>
+        <v-flex
+          class="text-center"
+          xs1
+        >
+          <img
+            src="images/PictoInterchanger.svg"
+            alt="Intervertir origin et destination"
+          >
+        </v-flex>
+        <v-flex xs3>
+          <GeoComplete :url="geoSearchUrl" />
+        </v-flex>
+      </v-layout>
+      <v-layout
+        class="mt-5"
+        align-center
+      >
+        <v-flex
+          xs2
+          offset-xs2
+        >
+          <p>{{ $t('switch.label') }}</p>
+        </v-flex>
+        <v-flex
+          xs2
+          row
+          class="text-left"
+        >
+          <v-switch
+            v-model="regular"
+            inset
+          />
+          <v-icon>mdi-help-circle-outline</v-icon>
+        </v-flex>
+      </v-layout>
+      <v-layout
+        class="mt-5"
+        align-center
+      >
+        <v-flex
+          xs3
+          offset-xs2
+        >
+          <v-menu
+            v-model="menu2"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            full-width
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="date"
+                :label="$t('datePicker.label')"
+                readonly
+                v-on="on"
+              />
+            </template>
+            <v-date-picker
+              v-model="date"
+              scrollable
+            >
+              <v-spacer />
+              <v-btn
+                text
+                color="primary"
+                @click="menu2 = false"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                text
+                color="primary"
+                @click="$refs.dialog.save(date)"
+              >
+                OK
+              </v-btn>
+            </v-date-picker>
+          </v-menu>
+        </v-flex>
+      </v-layout>
+      <v-layout
+        class="mt-5"
+        align-center
+      >
+        <v-flex
+          xs2
+          offset-xs2
+        >
+          <v-btn
+            rounded
+            outlined
+          >
+            {{ $t('buttons.shareAnAd.label') }}
+          </v-btn>
+        </v-flex>
+        <v-flex xs2>
+          <v-btn
+            color="success"
+            rounded
+            flat
+          >  
+            {{ $t('buttons.search.label') }}
+          </v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-content>
 </template>
 
@@ -33,6 +161,13 @@ export default {
       type: String,
       default: ""
     }
-  }
+  },
+  data () {
+    return {
+      regular: false,
+      date: new Date().toISOString().substr(0, 10),
+      menu2: false,
+    }
+  },
 }
 </script>
