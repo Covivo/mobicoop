b786e36b3410640b0ad4f3d4229454953e9644a6
maxime <maxime.bardot@gmail.com>
2019-07-31 16:54:44 +0200

    Create sub components for My Profile

--- client/src/MobicoopBundle/Resources/assets/js/components/user/Profile.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/user/Profile.vue
@@ -14,11 +14,11 @@
             grow
           >
             <v-tab>{{ $t("navBar.buttons.myProfile.label") }}</v-tab>
-            <v-tab-item>rien</v-tab-item>
+            <v-tab-item><UpdateProfile /></v-tab-item>
             <v-tab>{{ $t("models.user.password.label") }}</v-tab>
             <v-tab-item><ChangePassword /></v-tab-item>
             <v-tab>{{ $t("navBar.buttons.myProposals.label") }}</v-tab>
-            <v-tab-item>rien</v-tab-item>
+            <v-tab-item><MyProposals /></v-tab-item>
           </v-tabs>
         </v-flex>
       </v-layout>
@@ -26,10 +26,14 @@
   </v-content>
 </template>
 <script>
+import UpdateProfile from '@js/components/user/UpdateProfile';
 import ChangePassword from '@js/components/user/ChangePassword';
+import MyProposals from '@js/components/user/MyProposals';
 export default {
   components: {
-    ChangePassword
+    UpdateProfile,
+    ChangePassword,
+    MyProposals
   },
   props: {
   },
