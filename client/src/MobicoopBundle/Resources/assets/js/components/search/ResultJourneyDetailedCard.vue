cc850bc260a287eea9406163dbcbed49bef30d5f
Celine Jacquet <celinejacquet@iMac-de-Matthias.local>
2019-08-26 16:37:49 +0200

    test display origin, destination, date

--- client/src/MobicoopBundle/Resources/assets/js/components/search/ResultJourneyDetailedCard.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/search/ResultJourneyDetailedCard.vue
@@ -22,7 +22,9 @@
           </v-row>
 
           <!-- date -->
-          <v-row>  
+          <v-row
+            class="date-uppercase"
+          >  
             <p>
               {{ displaydate(date) }}
             </p>
@@ -31,15 +33,18 @@
 
         <!-- origin -->
         <v-col
-          class="text-left text-address-size"
+          class="text-left"
         >
-          <v-row>
-            <span
-              class="d-inline-block text-truncate"
-              style="max-width: 150px;"
-            >
-              {{ origin }}
-            </span>
+          <v-row
+            class="text-city-size"
+          >
+            {{ splitCityOrigin() }}
+          </v-row>
+          <v-row
+            class="d-inline-block text-truncate text-address-size"
+            style="max-width: 100px;"
+          >
+            {{ splitAddressOrigin() }}
           </v-row>
         </v-col> 
 
@@ -49,29 +54,26 @@
             :color="secondary"
             size="32"
           >
-            mdi-ray-start
-          </v-icon>  
-          <v-icon
-            :color="secondary"
-            size="32"
-          >
-            mdi-ray-end
+            mdi-ray-start-end
           </v-icon>
         </v-col> 
 
         <!-- destination --> 
-        <v-col 
-          class="text-left text-address-size"
+        <v-col
+          class="text-left"
         >
-          <v-row>
-            <span
-              class="d-inline-block text-truncate"
-              style="max-width: 150px;"
-            >
-              {{ destination }}
-            </span>
+          <v-row
+            class="text-city-size"
+          >
+            {{ splitCityDestination() }}
           </v-row>
-        </v-col>
+          <v-row
+            class="d-inline-block text-truncate text-address-size"
+            style="max-width: 100px;"
+          >
+            {{ splitAddressDestination() }}
+          </v-row>
+        </v-col> 
 
         <!--seats --> 
         <v-col>
@@ -84,12 +86,13 @@
         </v-col>
       </v-row>
 
-      <!--     <v-btn
+      <!-- TEST DRIVER PASSENGER WITH A BUTTON --> 
+      <v-btn
         color="primary"
         fab
         large
         depressed
-        @click="driver"
+        @click="testmethod('driver')"
       >
         <v-icon>mdi-account-supervisor</v-icon><br>
       </v-btn>
@@ -99,10 +102,10 @@
         fab
         large
         depressed
-        @click="passenger"
+        @click="testmethod('passenger')"
       >
         <v-icon>mdi-steering</v-icon><br>
-      </v-btn> -->
+      </v-btn>
     </v-container>
   </v-content>
 </template>
@@ -147,20 +150,54 @@ export default {
   },
   data: function() {
     return {
-      driver : null,
-      passenger : null
+      resultAddressOrigin: null,
+      resultCityOrigin:  null,
+      resultAddressDestination: null,
+      resultCityDestination:  null,
+      city: null,
+      address: null,
     };
   },
+   
   methods : {
     displaydate(date){
       return moment (new Date(date)).utcOffset("+00:00").format('ddd DD/MM ')
     },
-  }
+    // TODO : FIND AN OTHER METHOD, JUST FOR THE MOMENT :: BAD [" ... "], NOT OPTIMIZED
+    splitCityOrigin () {
+      this.resultCityOrigin= this.origin.split(', ')
+      return this.city= this.resultCityOrigin.slice(1)
+    },
+    splitAddressOrigin () {
+      this.resultAddressOrigin = this.origin.split(', ')
+      return this.address = this.resultAddressOrigin.slice(0,1)
+    },
+    splitCityDestination () {
+      this.resultCityDestination = this.destination.split(', ')
+      return this.city= this.resultCityDestination.slice(1)
+    },
+    splitAddressDestination () {
+      this.resultAddressDestination = this.destination.split(', ')
+      return this.address = this.resultAddressDestination.slice(0,1)
+    },
+    // TODO : FIND AN METHOD IN ORDER CHANGE ICON IF PASSENGER,DRIVER OR BOTH
+    testmethod: function (message) {
+      alert(message)
+    }
+  }  
 }
 </script>
 
   <style scoped>
+  .text-city-size {
+  font-size: 16px;
+    font-weight: bold;
+  }
   .text-address-size {
-  font-size: 17px;
+  font-size: 14px;
+  }
+
+  .date-uppercase {
+        text-transform: capitalize;
   }
   </style>
