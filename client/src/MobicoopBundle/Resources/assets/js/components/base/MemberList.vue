29988a083e3742e0d2bfb9b9db2b481a267bc1f5
Faucheur Guillaume <gfaucheur@ausy-group.com>
2019-08-30 15:49:43 +0000

    List of members

--- client/src/MobicoopBundle/Resources/assets/js/components/base/MemberList.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/base/MemberList.vue
@@ -1,8 +1,77 @@
-<template />
+<template>
+  <v-card>
+    <v-card-title>
+      {{ $t('List of members') }}
+      <div class="flex-grow-1" />
+      <v-text-field
+        v-model="search"
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+      />
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="users"
+      :search="search"
+    />
+  </v-card>
+</template>
 
 <script>
+
+import moment from "moment";
+import { merge } from "lodash";
+import CommonTranslations from "@translations/translations.json";
+import Translations from "@translations/components/home/HomeSearch.json";
+import TranslationsClient from "@clientTranslations/components/home/HomeSearch.json";
+
+let TranslationsMerged = merge(Translations, TranslationsClient);
+
 export default {
-  name: "MemberList"
+  i18n: {
+    messages: TranslationsMerged,
+    sharedMessages: CommonTranslations
+  },
+  props:{
+    users: {
+      type: Array,
+      default: null
+    }
+  },
+  data () {
+    return {
+      search: '',
+      headers: [
+        {
+          text: 'Id',
+          align: 'left',
+          sortable: false,
+          value: 'id',
+        },
+        { text: 'Nom', value: 'familyName' },
+        { text: 'Prenom', value: 'givenName' },
+        { text: 'Telephone', value: 'telephone' },
+        { text: 'Status', value: 'status' }
+      ]
+    }
+  },
+  computed: {
+    // creation of the url to call
+    urlToCall() {
+      return `${this.baseUrl}/${this.route}/${this.origin.addressLocality}/${this.destination.addressLocality}/${this.origin.latitude}/${this.origin.longitude}/${this.destination.latitude}/${this.destination.longitude}/${this.computedDateFormat}/resultats`;
+    },
+    searchUnavailable() {
+      return (!this.origin || !this.destination || this.loading == true)
+    },
+    computedDateFormat() {
+      moment.locale(this.locale);
+      return this.date
+        ? moment(this.date).format(this.$t("ui.i18n.date.format.fullNumericDate"))
+        : moment(new Date()).format(this.$t("ui.i18n.date.format.fullNumericDate"));
+    }
+  }
 }
 </script>
 
--- client/src/MobicoopBundle/Resources/assets/js/config/community/communities.js
+++ client/src/MobicoopBundle/Resources/assets/js/config/community/communities.js
@@ -11,7 +11,7 @@ import Vuetify, {
   // header
   VToolbar, VToolbarTitle, VBtn, VImg,
   // footer
-  VFooter, VCardText
+  VFooter, VCardText, VDataTable, VChip, VTextField
 } from 'vuetify/lib'
 
 Vue.use(Vuetify, {
@@ -21,7 +21,7 @@ Vue.use(Vuetify, {
     // header
     VToolbar, VToolbarTitle, VBtn, VImg,
     // footer
-    VFooter, VCardText
+    VFooter, VCardText, VDataTable, VChip, VTextField
   }
 })
 Vue.use(VueI18n)
