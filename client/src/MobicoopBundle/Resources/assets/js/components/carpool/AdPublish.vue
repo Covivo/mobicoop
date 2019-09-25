004194850e60cc31c4bd9d21ba3cb1bc8890c14f
maxime <maxime.bardot@gmail.com>
2019-09-25 12:02:12 +0200

    Controls on the publication of an ad

--- client/src/MobicoopBundle/Resources/assets/js/components/carpool/AdPublish.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/carpool/AdPublish.vue
@@ -589,6 +589,7 @@ export default {
       bike: false,
       backSeats: false,
       schedules: null,
+      returnTrip:null,
       route: null,
       price: null,
       pricePerKm: this.defaultPriceKm,
@@ -626,21 +627,30 @@ export default {
       if (this.distance<=0 || this.duration<=0 || !this.origin || !this.destination || !this.route) return false;
       // punctual date validation
       if (!this.regular && !(this.outwardDate && this.outwardTime)) return false;
+      // punctual roundtrip date validation
+      if (!this.regular && this.returnTrip && !(this.returnDate && this.returnTime)) return false;
       // regular date validation
       if (this.regular && !this.schedules) return false;
+      // regular schedules validation
+      if(this.step==2 && this.regular && (this.schedules==null || this.schedules.length==0)) return false;
       // validation ok
       return true;
     },
     validNext() {
       // For the next button
-
-      //((driver && step < 7) || (step<5)) && origin != null && destination != null && (passenger || driver) && (regular || outwardDate)
-
       if(this.origin == null || this.destination == null) return false;
       if(!this.passenger && !this.driver) return false;
       if(!this.regular && !this.outwardDate) return false;
       if(!this.driver && this.step>5) return false;
       if(this.step>=7) return false;
+
+      // Specifics by steps
+      // Step 2 regular : you have to setup at least one schedule
+      if(this.step==2 && this.regular && (this.schedules==null || this.schedules.length==0)) return false;
+      // Step 2 punctual : you have to set the outward time
+      if(this.step==2 && !this.regular && !(this.outwardDate && this.outwardTime)) return false;
+      // Step 2 punctual, round-trip chosen : you have to set the outward date & time
+      if(this.step==2 && !this.regular && this.returnTrip && !(this.returnDate && this.returnTime)) return false;
 
       return true;
     },
@@ -726,6 +736,7 @@ export default {
       this.returnDate = planification.returnDate;
       this.returnTime = planification.returnTime;
       this.schedules = planification.schedules;
+      this.returnTrip = planification.returnTrip;
     },
     routeChanged(route) {
       this.route = route;
