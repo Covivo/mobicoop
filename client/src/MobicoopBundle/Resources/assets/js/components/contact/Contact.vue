99faa4ddfbb9ede4897ff557322f70c573378753
Benoit Delboé <benoit@lyon.happy-dev.fr>
2019-10-14 14:09:47 +0200

    create contact page files

---
+++ client/src/MobicoopBundle/Resources/assets/js/components/contact/Contact.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-content>
+    <p>salut</p>
+  </v-content>
+</template>
+
+<script>
+import {merge} from "lodash";
+import CommonTranslations from "@translations/translations.json";
+import Translations from "@translations/components/home/HomeSearch.json";
+import TranslationsClient from "@clientTranslations/components/home/HomeSearch.json";
+
+let TranslationsMerged = merge(Translations, TranslationsClient);
+export default {
+  i18n: {
+    messages: TranslationsMerged,
+    sharedMessages: CommonTranslations
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
