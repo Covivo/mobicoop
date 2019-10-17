23d6c396166bd9c2957a96a05471d00dbf47041f
Celine JACQUET <b3ck54@gmail.com>
2019-10-17 11:39:54 +0200

    Component Gallery + webpack update

---
+++ client/src/MobicoopBundle/Resources/assets/js/components/utilities/MGallery.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container fluid>
+    <v-row
+      align-content-sm
+      justify="center"
+      justify-sm
+    >
+      <v-col
+        v-for="item in items"
+        :key="item.index"
+        class="d-flex child-flex"
+        cols="1"
+        align-self="center"
+      >
+        <a
+          :href="item.link"
+        >
+          <v-img
+            width="50"
+            :src="item.src"
+            :alt="item.alt"
+          />
+        </a>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+
+import Logos from "@componentsData/base/MFooter/Logos.json"
+
+export default {
+
+  data() {
+    return {
+      items: []
+    };
+  },
+  mounted() {
+    this.getLogos();
+  },
+  methods:{
+    getLogos() {
+      for (let Logo in Logos)
+      {
+        this.items.push(Logos[Logo]);
+      }
+    }
+  }
+}
+
+</script>
