b5f363bbc88f593c56e8e0bd1e0cadd4e811da01
Benoit Delboé <benoit@lyon.happy-dev.fr>
2019-10-23 16:23:48 +0200

    update css usage

--- client/src/MobicoopBundle/Resources/assets/js/app.js
+++ client/src/MobicoopBundle/Resources/assets/js/app.js
@@ -4,6 +4,9 @@ import { Vue, vuetify, i18n } from '@js/config/app'
 
 import components from '@js/config/components'
 
+// Import css
+import '@css/main.scss'
+
 new Vue({
   el: '#app',
   vuetify,
