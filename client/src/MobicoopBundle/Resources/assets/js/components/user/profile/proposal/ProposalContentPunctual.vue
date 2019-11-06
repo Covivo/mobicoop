fffcf0862beeab1b712bf94c2f816e9a7582f0aa
Benoit Delboé <benoit@lyon.happy-dev.fr>
2019-11-06 17:38:27 +0100

    fix issue when punctual carpool only has outward

--- client/src/MobicoopBundle/Resources/assets/js/components/user/profile/proposal/ProposalContentPunctual.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/user/profile/proposal/ProposalContentPunctual.vue
@@ -1,27 +1,27 @@
 <template>
   <v-container>
-    <v-row>
+    <v-row v-if="hasOutward">
       <schedules
         date-time-format="ui.i18n.date.format.shortDate"
         :outward-times="[proposal.outward.criteria.fromDate]"
       />
     </v-row>
-    <v-row>
+    <v-row v-if="hasOutward">
       <v-col
         cols="6"
         class="pa-0"
       >
         <route-summary
-          :origin="proposal.outward.waypoints[0].address"
-          :destination="proposal.outward.waypoints[proposal.outward.waypoints.length - 1].address"
-          :type="proposal.outward.criteria.frequency"
+          :origin="origin"
+          :destination="destination"
+          :type="frequency"
           :time="proposal.outward.criteria.fromTime"
           text-color-class="primary--text text--darken-3"
           icon-color="accent"
         />
       </v-col>
     </v-row>
-    <v-row>
+    <v-row v-if="hasReturn">
       <schedules
         :is-return="true"
         :is-outward="false"
@@ -29,15 +29,15 @@
         :return-times="[proposal.return.criteria.fromDate]"
       />
     </v-row>
-    <v-row>
+    <v-row v-if="hasReturn">
       <v-col
         cols="6"
         class="pa-0"
       >
         <route-summary
-          :origin="proposal.outward.waypoints[proposal.outward.waypoints.length - 1].address"
-          :destination="proposal.outward.waypoints[0].address"
-          :type="proposal.outward.criteria.frequency"
+          :origin="destination"
+          :destination="origin"
+          :type="frequency"
           :time="proposal.return.criteria.fromTime"
           text-color-class="primary--text text--darken-3"
           icon-color="accent"
@@ -48,6 +48,7 @@
 </template>
 
 <script>
+import {isEmpty} from "lodash";
 import RouteSummary from '@components/carpool/utilities/RouteSummary.vue';
 import Schedules from '@components/user/profile/proposal/Schedules.vue';
 
@@ -60,6 +61,26 @@ export default {
     proposal: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    hasOutward () {
+      return !isEmpty(this.proposal.outward);
+    },
+    hasReturn () {
+      return !isEmpty(this.proposal.return);
+    },
+    frequency () {
+      return this.hasOutward ? this.proposal.outward.criteria.frequency : 
+        this.hasReturn ? this.proposal.return.criteria.frequency : null;
+    },
+    origin () {
+      return this.hasOutward ? this.proposal.outward.waypoints[0].address : 
+        this.hasReturn ? this.proposal.return.waypoints[0].address : null;
+    },
+    destination () {
+      return this.hasOutward ? this.proposal.outward.waypoints[this.proposal.outward.waypoints.length - 1].address :
+        this.hasReturn ? this.proposal.return.waypoints[this.proposal.outward.waypoints.length - 1].address : null;
     }
   }
 }
