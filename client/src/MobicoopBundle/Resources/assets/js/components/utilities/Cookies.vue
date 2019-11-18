df49b7ddd1f08b65be1ce1238cf5ca4653a33155
Celine JACQUET <b3ck54@gmail.com>
2019-11-18 11:28:40 +0100

    Custom Theme cookie banner

--- client/src/MobicoopBundle/Resources/assets/js/components/utilities/Cookies.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/utilities/Cookies.vue
@@ -1,5 +1,6 @@
 <template>
   <cookie-law
+    theme="custom-theme"
     :button-text="$t('iUnderstand')"
   >
     <div slot="message">
@@ -27,3 +28,22 @@ export default {
   }
 }
 </script>
+ <style lang="scss" >
+   .Cookie--custom-theme {
+     .Cookie__button {
+     background: #f045ab;
+     padding: 8px;
+     color: #fff;
+     border-width: 2px 4px;
+     border-radius: 40px;
+     font-size: 1em;
+     margin: 16px;
+     }
+     .Cookie__buttons {
+       align-self: center;
+       white-space: nowrap;
+     }
+     padding:20px;
+     background: white;
+     }
+ </style>
