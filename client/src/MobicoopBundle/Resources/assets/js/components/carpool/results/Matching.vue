78461c04bef5c6ea55aac608c86fd11ad93d76b4
Maxime Bardot <maxime.bardot@gmail.com>
2019-11-21 10:07:47 +0100

    Skeleton loader

--- client/src/MobicoopBundle/Resources/assets/js/components/carpool/results/Matching.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/carpool/results/Matching.vue
@@ -1,15 +1,5 @@
 <template>
   <div>
-    <!--loading overlay-->
-    <v-overlay 
-      :value="loading"
-      absolute
-    >
-      <v-progress-circular
-        indeterminate
-      />
-    </v-overlay>
-
     <v-container fluid>
       <v-row
         justify="center"
@@ -55,25 +45,47 @@
           </v-row>
 
           <!-- Matching results -->
-          <v-row 
-            v-for="(result,index) in results"
-            :key="index"
-            justify="center"
-          >
-            <v-col
-              cols="12"
-              align="left"
+          <div v-if="loading">
+            <v-row
+              v-for="n in 3"
+              :key="n"
+              class="text-left"
             >
-              <!-- Matching result -->
-              <matching-result
-                :result="result"
-                :user="user"
-                :distinguish-regular="distinguishRegular"
-                :carpooler-rate="carpoolerRate"
-                @carpool="carpool(result)"
-              />
-            </v-col>
-          </v-row>
+              <v-col cols="12">
+                <v-skeleton-loader
+                  ref="skeleton"
+                  type="article"
+                  class="mx-auto"
+                />
+                <v-skeleton-loader
+                  ref="skeleton"
+                  type="actions"
+                  class="mx-auto"
+                />
+              </v-col>
+            </v-row>
+          </div>
+          <div v-else>
+            <v-row 
+              v-for="(result,index) in results"
+              :key="index"
+              justify="center"
+            >
+              <v-col
+                cols="12"
+                align="left"
+              >
+                <!-- Matching result -->
+                <matching-result
+                  :result="result"
+                  :user="user"
+                  :distinguish-regular="distinguishRegular"
+                  :carpooler-rate="carpoolerRate"
+                  @carpool="carpool(result)"
+                />
+              </v-col>
+            </v-row>
+          </div>
         </v-col>
       </v-row>
     </v-container>
