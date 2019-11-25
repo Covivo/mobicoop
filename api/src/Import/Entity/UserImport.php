f55b11798a2edf52709750c6f2a6f7113b3b29aa
Sylvain Briat <sylvain.briat@covivo.eu>
2019-11-25 10:03:09 +0100

    creation of the Import domain in api

---
+++ api/src/Import/Entity/UserImport.php
@@ -0,0 +1,269 @@
+<?php
+
+/**
+ * Copyright (c) 2019, MOBICOOP. All rights reserved.
+ * This project is dual licensed under AGPL and proprietary licence.
+ ***************************
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <gnu.org/licenses>.
+ ***************************
+ *    Licence MOBICOOP described in the file
+ *    LICENSE
+ **************************/
+
+namespace App\Import\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
+use App\User\Entity\User;
+
+/**
+ * A user imported from an external system.
+ * 
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ * @ApiResource(
+ *      attributes={
+ *          "force_eager"=false,
+ *          "normalization_context"={"groups"={"read"}, "enable_max_depth"="true"},
+ *          "denormalization_context"={"groups"={"write"}}
+ *      },
+ *      collectionOperations={
+ *          "treat"={
+ *              "method"="GET",
+ *              "path"="/user_imports/treat",
+ *              "normalization_context"={"groups"={"write"}},
+ *          }
+ *      },
+ *      itemOperations={
+ *      }
+ * )
+ * 
+ */
+class UserImport
+{
+    const STATUS_IMPORTED = 0;  // the external user has been imported, no treatment has been made yet
+    const STATUS_PENDING = 1;   // a treatment is pending
+    const STATUS_TREATED = 2;   // the treatment has been made successfully
+    const STATUS_ERROR = 3;     // the treatment has failed
+
+    /**
+     * @var int The id of this imported user.
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Groups("read")
+     * @ApiProperty(identifier=true)
+     */
+    private $id;
+    
+    /**
+     * @var User|null User imported in the platform.
+     *
+     * @ORM\OneToOne(targetEntity="\App\User\Entity\User", cascade={"persist", "remove"}, orphanRemoval=true)
+     * @Groups({"read","write"})
+     * @MaxDepth(1)
+     */
+    private $user;
+
+    /**
+     * @var string|null The user id in the external system.
+     *
+     * @ORM\Column(type="string", length=255, nullable=false)
+     * @Groups({"read","write"})
+     */
+    private $userExternalId;
+
+    /**
+     * @var string|null The identifier of the external system.
+     *
+     * @ORM\Column(type="string", length=255, nullable=false)
+     * @Groups({"read","write"})
+     */
+    private $origin;
+
+    /**
+     * @var int Import status.
+     *
+     * @ORM\Column(type="smallint")
+     * @Groups({"read","write"})
+     */
+    private $status;
+
+    /**
+     * @var \DateTimeInterface Creation date of the user import.
+     *
+     * @ORM\Column(type="datetime")
+     * @Groups({"read","write"})
+     */
+    private $createdDate;
+
+    /**
+     * @var \DateTimeInterface Update date of the user import.
+     *
+     * @ORM\Column(type="datetime")
+     * @Groups({"read","write"})
+     */
+    private $updatedDate;
+
+    /**
+     * @var \DateTimeInterface Start date of the treatment.
+     *
+     * @ORM\Column(type="datetime")
+     * @Groups({"read","write"})
+     */
+    private $treatmentStartDate;
+
+    /**
+     * @var \DateTimeInterface End date of the treatment.
+     *
+     * @ORM\Column(type="datetime")
+     * @Groups({"read","write"})
+     */
+    private $treatmentEndDate;
+
+    public function __construct($status = null)
+    {
+        if (is_null($status)) {
+            $status = self::STATUS_IMPORTED;
+        }
+        $this->setStatus($status);
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getUserExternalId(): string
+    {
+        return $this->userExternalId;
+    }
+
+    public function setUserExternalId(string $userExternalId): self
+    {
+        $this->userExternalId = $userExternalId;
+
+        return $this;
+    }
+
+    public function getOrigin(): string
+    {
+        return $this->origin;
+    }
+
+    public function setOrigin(string $origin): self
+    {
+        $this->origin = $origin;
+
+        return $this;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedDate(): ?\DateTimeInterface
+    {
+        return $this->createdDate;
+    }
+
+    public function setCreatedDate(\DateTimeInterface $createdDate): self
+    {
+        $this->createdDate = $createdDate;
+
+        return $this;
+    }
+
+    public function getUpdatedDate(): ?\DateTimeInterface
+    {
+        return $this->updatedDate;
+    }
+
+    public function setUpdatedDate(\DateTimeInterface $updatedDate): self
+    {
+        $this->updatedDate = $updatedDate;
+
+        return $this;
+    }
+
+    public function getTreatmentStartDate(): ?\DateTimeInterface
+    {
+        return $this->treatmentStartDate;
+    }
+
+    public function setTreatmentStartDate(\DateTimeInterface $treatmentStartDate): self
+    {
+        $this->treatmentStartDate = $treatmentStartDate;
+
+        return $this;
+    }
+
+    public function getTreatmentEndDate(): ?\DateTimeInterface
+    {
+        return $this->treatmentEndDate;
+    }
+
+    public function setTreatmentEndDate(\DateTimeInterface $treatmentEndDate): self
+    {
+        $this->treatmentEndDate = $treatmentEndDate;
+
+        return $this;
+    }
+
+    // DOCTRINE EVENTS
+
+    /**
+     * Creation date.
+     *
+     * @ORM\PrePersist
+     */
+    public function setAutoCreatedDate()
+    {
+        $this->setCreatedDate(new \Datetime());
+    }
+
+    /**
+     * Update date.
+     *
+     * @ORM\PreUpdate
+     */
+    public function setAutoUpdatedDate()
+    {
+        $this->setUpdatedDate(new \Datetime());
+    }
+
+}
