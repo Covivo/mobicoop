c2debd6b2a2cb5a9db75d221c8f2b0e5566d36f0
remi wortemann <remi.wortemann@gmail.com>
2020-01-07 12:40:16 +0100

    fix ad deletion

--- api/src/Carpool/Controller/ProposalDelete.php
+++ api/src/Carpool/Controller/ProposalDelete.php
@@ -57,7 +57,6 @@ class ProposalDelete
         if (is_null($data)) {
             throw new \InvalidArgumentException($this->translator->trans("bad proposal id is provided"));
         }
-        
         $data = $this->proposalManager->deleteProposal($data, json_decode($this->request->getContent(), true));
         return $data;
     }
