09ae825169bde4ea73ae453eb51c138293607e1c
Maxime Bardot <maxime.bardot@gmail.com>
2020-01-23 12:09:32 +0100

    #1293 - Text community infos

--- client/src/MobicoopBundle/Resources/assets/js/components/community/CommunityHelp.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/community/CommunityHelp.vue
@@ -28,11 +28,7 @@
           <v-card flat>
             <v-card-text>
               <p>
-                Une communauté est un regroupement d’inscrits ayant un intérêt en commun.
-                Elle permet par exemple d’identifier les salariés d’une entreprise, d’une zone d’activités,
-                les personnes fréquentant une salle de spectacle
-                ou encore les habitants d’une commune, pour faciliter les mises en relation des
-                covoitureurs.
+                {{ $t("whatsCommunity") }}
               </p>
             </v-card-text>
           </v-card>
@@ -41,21 +37,14 @@
           <v-card flat>
             <v-card-text>
               <div>
-                La création d’une communauté permet :
+                {{ $t("communityforwhat.title") }}
               </div>
               <br>
               <ul>
-                <li>- à ses membres de se voir mutuellement et d’échanger entre eux</li>
-                <li>
-                  - de faciliter la mise en relation des covoitureurs en augmentant la confiance entre
-                  membres
-                </li>
-                <li>- de disposer d’un widget personnalisé à installer par exemple sur un intranet</li>
-                <li>
-                  - de disposer, pour le créateur de la communauté, de statistiques sur les inscrits et
-                  les
-                  covoitureurs de sa communauté
-                </li>
+                <li>{{ $t("communityforwhat.list.firstLine") }}</li>
+                <li>{{ $t("communityforwhat.list.secondLine") }}</li>
+                <li>{{ $t("communityforwhat.list.thirdLine") }}</li>
+                <li>{{ $t("communityforwhat.list.fourthLine") }}</li>
               </ul>
             </v-card-text>
           </v-card>
@@ -64,11 +53,7 @@
           <v-card flat>
             <v-card-text>
               <p>
-                Toute personne qui le souhaite peut créer une communauté dans OuestGo à condition qu’elle soit
-                inscrite sur le site et ait envie d’animer et de dynamiser le covoiturage à son niveau. Créer une
-                communauté se fait en quelques clics sur ouestgo.fr. Cela vous permettra d’accéder au front office
-                librement. Si vous souhaitez aller plus loin dans l’animation et accéder au back office, contactez
-                xxxxx@megalis.xxx. L’ensemble des fonctionnalités d’une communauté est accessible gratuitement.
+                {{ $t("whoCanCreate") }}
               </p>
             </v-card-text>
           </v-card>
@@ -78,9 +63,13 @@
   </v-container>
 </template>
 <script>
-  
+import { merge } from "lodash";
+import Translations from "@translations/components/community/CommunityHelp.json";
+import TranslationsClient from "@clientTranslations/components/community/CommunityHelp.json";
+let TranslationsMerged = merge(Translations, TranslationsClient);  
 export default {
   i18n: {
+    messages: TranslationsMerged
   }
 }
 </script>
