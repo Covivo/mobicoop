18b7cb9dae52e4e369a28720ec82a42834e29a9a
Maxime Bardot <maxime.bardot@gmail.com>
2020-02-17 09:43:22 +0100

    Dynamic feeds and sanitized iframe

--- client/src/MobicoopBundle/Resources/assets/js/components/utilities/social/MSocialFeeds.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/utilities/social/MSocialFeeds.vue
@@ -1,15 +1,37 @@
 <template>
-  <v-row dense>
-    <v-col
-      v-for="item in items"
-      :key="item.id"
-      class="text-center pa-lg-10 pa-md-2"
+  <v-container>
+    <v-row
+      v-if="!loading"
+      dense
     >
-      <MSocialFeedsItem
-        :i-frame-string="item.iFrame"
-      />
-    </v-col>
-  </v-row> 
+      <v-col
+        v-for="item in items"
+        :key="item.id"
+        class="text-center pa-lg-10 pa-md-2"
+      >
+        <MSocialFeedsItem
+          :i-frame-string="sanitizedIFrame(item.iFrame)"
+        />
+      </v-col>
+    </v-row>
+    <v-row
+      v-else
+      dense
+    >
+      <v-col
+        v-for="n in 3"
+        :key="n"
+        class="text-center pa-lg-10 pa-md-2"
+      >
+        <v-skeleton-loader
+          class="mx-auto"
+          :width="width"
+          :height="height"
+          type="card"
+        />
+      </v-col>
+    </v-row> 
+  </v-container>
 </template>
 <script>
 import Translations from "@translations/components/utilities/social/MSocialFeeds.json";
@@ -24,7 +46,10 @@ export default {
   },
   data() {
     return {
-      items:[]
+      items:[],
+      loading:true,
+      width:"100%",
+      height:"600"
     }
   },
   mounted(){
@@ -33,19 +58,25 @@ export default {
   methods:{
     getArticles(){
       let params = {};
+      this.loading = true;
       axios.post(this.$t("urlGetArticles"), params)
         .then(response => {
-          console.error(response.data);
+          // console.error(response.data);
+          this.items = response.data;
         })
         .catch(function (error) {
           // console.log(error);
         })
         .finally(() => {
+          this.loading = false;
           this.$emit("refreshActionsCompleted");
         });
 
 
       return []
+    },
+    sanitizedIFrame(iframe){
+      return iframe.replace(/width="[0-9]+"/, 'width="'+this.width+'"').replace(/height="[0-9]+"/, 'height="'+this.height+'"');
     }
   }
 }
