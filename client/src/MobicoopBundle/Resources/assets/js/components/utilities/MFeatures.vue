64ce3b3bec2ed55f4b6a76c12e5ed6e9bc370c84
Maxime Bardot <maxime.bardot@gmail.com>
2020-02-17 16:12:05 +0100

    tweak features

--- client/src/MobicoopBundle/Resources/assets/js/components/utilities/MFeatures.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/utilities/MFeatures.vue
@@ -1,40 +1,61 @@
 <template>
-  <v-content :style="'background-image:url(\''+backgroundImage+'\');background-position:top right;'">
-    <v-row
-      justify="center"
-    >
+  <v-container
+    fluid
+  >
+    <v-row justify="center">
       <v-col
-        cols="7"
-        class="text-center"
-      >
-        <v-card-text
-          class="display-1 text-uppercase"
-          :class="(textThin) ? 'text--'+colorTitle+' font-weight-thin' : 'text--'+colorTitle"
-        >
-          {{ title }}
-        </v-card-text>
-        <v-card-text
-          class="headline"
-          :class="(textThin) ? 'text--'+colorSubtitle+' font-weight-thin' : 'text--'+colorSubtitle"
-        >
-          {{ subtitle }}
-        </v-card-text>
-      </v-col>
-    </v-row>
-    <v-row
-      justify="center"
-      class="mt-10"
-    >
-      <MFeaturesItem
-        v-for="(item, index) in items"
-        :key="index"
-        :title="item.title"
-        :subtitle="item.subtitle"
-        :image="item.image"
-        :width="210"
+        cols="0"
+        lg="2"
+        class="backgroundImageColumn"
+        :style="(backgroundImageLeft) ? 'background-image:url(\''+backgroundImageLeft+'\');background-position:top center;' : ''"
       />
-    </v-row>  
-  </v-content>  
+      <v-col
+        cols="10"
+        lg="8"
+      >
+        <v-row
+          justify="center"
+        >
+          <v-col
+            cols="7"
+            class="text-center"
+          >
+            <v-card-text
+              class="display-1 text-uppercase"
+              :class="(textThin) ? 'text--'+colorTitle+' font-weight-thin' : 'text--'+colorTitle"
+            >
+              {{ title }}
+            </v-card-text>
+            <v-card-text
+              class="headline"
+              :class="(textThin) ? 'text--'+colorSubtitle+' font-weight-thin' : 'text--'+colorSubtitle"
+            >
+              {{ subtitle }}
+            </v-card-text>
+          </v-col>
+        </v-row>
+        <v-row
+          justify="center"
+          class="mt-10"
+        >
+          <MFeaturesItem
+            v-for="(item, index) in items"
+            :key="index"
+            :title="item.title"
+            :subtitle="item.subtitle"
+            :image="item.image"
+            :width="210"
+          />
+        </v-row>  
+      </v-col>
+      <v-col
+        cols="0"
+        lg="2"
+        class="backgroundImageColumn"
+        :style="(backgroundImageRight) ? 'background-image:url(\''+backgroundImageRight+'\');background-position:top center;' : ''"
+      />
+    </v-row>
+  </v-container>  
 </template>
 <script>
 import MFeaturesItem from "@components/utilities/MFeaturesItem";
@@ -55,7 +76,11 @@ export default {
       type: Array,
       default: null
     },
-    backgroundImage:{
+    backgroundImageLeft:{
+      type:String,
+      default:null
+    },
+    backgroundImageRight:{
       type:String,
       default:null
     },
@@ -74,3 +99,8 @@ export default {
   }    
 }
 </script>
+<style lang="scss" scoped>
+.backgroundImageColumn{
+  background-size:contain;
+}
+</style>
