671a96a5464c45e9aebf99abb39a10bf9327168a
AppHeros <julien.deschampt@gmail.com>
2020-02-20 10:09:09 +0000

    Send email only subscribe

--- api/src/Communication/Service/NotificationManager.php
+++ api/src/Communication/Service/NotificationManager.php
@@ -93,9 +93,9 @@ class NotificationManager
      */
     public function notifies(string $action, User $recipient, ?object $object = null)
     {
-          if (!$this->enabled) {
-              return;
-          }
+        if (!$this->enabled) {
+            return;
+        }
 
         $notifications = null;
         // we check the user notifications
