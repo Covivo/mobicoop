9bef6ae6ef59ab7a261d96a240ecb02a3172d4c2
Maxime Bardot <maxime.bardot@gmail.com>
2020-03-23 16:20:05 +0100

    fixMigrations

--- api/src/Migrations/Version20200214103350.php
+++ api/src/Migrations/Version20200214103350.php
@@ -60,10 +60,10 @@ final class Version20200214103350 extends AbstractMigration
         $this->addSql('ALTER TABLE ptstep CHANGE ptleg_id ptleg_id INT DEFAULT NULL, CHANGE ptdeparture_id ptdeparture_id INT DEFAULT NULL, CHANGE ptarrival_id ptarrival_id INT DEFAULT NULL, CHANGE distance distance INT DEFAULT NULL, CHANGE duration duration INT DEFAULT NULL, CHANGE magnetic_direction magnetic_direction VARCHAR(10) DEFAULT NULL, CHANGE relative_direction relative_direction VARCHAR(10) DEFAULT NULL');
         $this->addSql('ALTER TABLE relay_point CHANGE community_id community_id INT DEFAULT NULL, CHANGE private private TINYINT(1) DEFAULT NULL, CHANGE places places SMALLINT DEFAULT NULL, CHANGE places_disabled places_disabled SMALLINT DEFAULT NULL, CHANGE free free TINYINT(1) DEFAULT NULL, CHANGE secured secured TINYINT(1) DEFAULT NULL, CHANGE official official TINYINT(1) DEFAULT NULL, CHANGE suggested suggested TINYINT(1) DEFAULT NULL, CHANGE permalink permalink VARCHAR(255) DEFAULT NULL, CHANGE updated_date updated_date DATETIME DEFAULT NULL');
         $this->addSql('ALTER TABLE relay_point_type CHANGE icon_id icon_id INT DEFAULT NULL, CHANGE created_date created_date DATETIME DEFAULT NULL, CHANGE updated_date updated_date DATETIME DEFAULT NULL');
-        $this->addSql('ALTER TABLE uright CHANGE parent_id parent_id INT DEFAULT NULL, CHANGE description description VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE role CHANGE parent_id parent_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE user_right CHANGE territory_id territory_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE user_role CHANGE territory_id territory_id INT DEFAULT NULL');
+        // $this->addSql('ALTER TABLE uright CHANGE parent_id parent_id INT DEFAULT NULL, CHANGE description description VARCHAR(255) DEFAULT NULL');
+        // $this->addSql('ALTER TABLE role CHANGE parent_id parent_id INT DEFAULT NULL');
+        // $this->addSql('ALTER TABLE user_right CHANGE territory_id territory_id INT DEFAULT NULL');
+        // $this->addSql('ALTER TABLE user_role CHANGE territory_id territory_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE solidary CHANGE assisted assisted TINYINT(1) DEFAULT NULL, CHANGE updated_date updated_date DATETIME DEFAULT NULL');
         $this->addSql('ALTER TABLE solidary_matching CHANGE updated_date updated_date DATETIME DEFAULT NULL');
         $this->addSql('ALTER TABLE structure CHANGE created_date created_date DATETIME DEFAULT NULL, CHANGE updated_date updated_date DATETIME DEFAULT NULL');
