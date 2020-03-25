e353b88c1b7adec37c85017853dfd1000e2eb365
Remi Wortemann <remi.wortemann@gmail.com>
2020-03-25 17:48:18 +0100

    ticket remove message switches from alerts section

---
+++ api/src/Migrations/Version20200325163400.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200309120000 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        // we remove all user_notification with media=internal_message
+        $this->addSql('DELETE FROM `user_notification` WHERE `notification_id` IN (SELECT `id` FROM `notification` WHERE `medium_id` = 1');
+    }
+}
