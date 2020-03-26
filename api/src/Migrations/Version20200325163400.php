3becc0c84b7f437799bc85ef0f341e25314e8b2d
Remi Wortemann <remi.wortemann@gmail.com>
2020-03-26 07:52:59 +0100

    update migration

--- api/src/Migrations/Version20200325163400.php
+++ api/src/Migrations/Version20200325163400.php
@@ -14,7 +14,8 @@ final class Version20200309120000 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
+        // we pass user_editable to false for all notifications with internal message as media since it's for now not activated
+        $this->addSql('UPDATE `notification` SET user_editable=0 WHERE medium_id=1');
         // we remove all user_notification with media=internal_message
         $this->addSql('DELETE FROM `user_notification` WHERE `notification_id` IN (SELECT `id` FROM `notification` WHERE `medium_id` = 1');
     }
