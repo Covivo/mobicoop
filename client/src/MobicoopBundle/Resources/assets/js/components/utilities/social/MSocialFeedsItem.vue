31489f08c46a78abd2c61c3fa457413390d371a7
b3ck54 <b3ck54@gmail.com>
2020-05-04 09:47:09 +0200

    WIP - redirection click iframe

--- client/src/MobicoopBundle/Resources/assets/js/components/utilities/social/MSocialFeedsItem.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/utilities/social/MSocialFeedsItem.vue
@@ -1,5 +1,8 @@
 <template>
-  <div v-html="iFrameString" />
+  <div  
+    @mouseenter="redirected()"
+    v-html="iFrameString"
+  />
 </template>
 <script>
 export default {
@@ -7,6 +10,30 @@ export default {
     iFrameString:{
       type: String,
       default: null
+    }
+  },
+  data() {
+    return {
+      source: null
+    }
+  },
+  mounted() {
+    // get src in string
+    let urlCurrent = JSON.parse(JSON.stringify(this.iFrameString.replace(/"/g,"'")));
+    let regex = /<iframe.*?src='(.*?)'/;
+    this.source = regex.exec(urlCurrent)[1];
+  },
+  methods:{
+    redirected(){
+      let source = this.source
+      let monitor = setInterval(function(){
+        let elem = document.activeElement;
+        if(elem && elem.tagName == 'IFRAME'){
+          clearInterval(monitor);
+          // return window.location.href = "src";
+          return window.open(source, "_blank");
+        }
+      }, 100);
     }
   }
 }
