55ab43e10111773099c77843e09c675c9f56069f
AppHeros <julien.deschampt@gmail.com>
2020-05-11 12:45:06 +0000

    Fix get communities

---
+++ api/src/Migrations/Version20200507160000.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Import item migration
+ */
+final class Version20200507160000 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+
+        // Add user_list, user_read items child to community_manager public and private
+        $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES ('8', '145')");
+        $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES ('8', '68')");
+        $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES ('9', '68')");
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    }
+}
