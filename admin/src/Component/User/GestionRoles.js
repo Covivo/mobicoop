a1cb4a20674b3f73719979cf23c9cfa4b1900ff1
Julien <julien.deschampt@gmail.com>
2020-05-12 14:52:07 +0200

    Change set roles

--- admin/src/Component/User/GestionRoles.js
+++ admin/src/Component/User/GestionRoles.js
@@ -58,15 +58,21 @@ const GestionRoles = ({record}) => {
     if (record.rolesTerritory) {
       // We clear fields in case of an Edit
       setFields([])
-      for (const territory in record.rolesTerritory) {
+      for (const [territory, roles] of Object.entries(record.rolesTerritory)) {
 
+      //If there ie no territory set on this pair
+      if (territory != 'null') {
           dataProvider.getOne('territories',{id: territory} )
               .then( ({ data }) =>  {
+                console.info(data)
                   //setCurrentTerritory(t => [...t, data.name])
-                        setFields(t => [...t, {'roles' : record.rolesTerritory[territory], 'territory' : territory,'territoryName' : data.name} ])
+                        setFields(t => [...t, {'roles' : roles, 'territory' : territory,'territoryName' : data.name} ])
               })
               .catch( error => {
             })
+          }else{
+            setFields(t => [...t, {'roles' : roles} ])
+          }
       }
     }
   }, [record]);
