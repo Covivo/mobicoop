8506459c80ae8dda7c2873c446566b9813ee0523
b3ck54 <b3ck54@gmail.com>
2020-05-20 16:39:22 +0200

    articles migration cookies and covid19

---
+++ api/src/Migrations/Version20200519161400.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200519161400 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('INSERT INTO article (`id`, `title`, `status`) VALUES (16, "COOKIES", 1)');
+        $this->addSql('INSERT INTO article (`id`, `title`, `status`) VALUES (17, "COVID-19", 1)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    }
+}
+
+
+
+
