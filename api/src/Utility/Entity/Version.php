5aafd49a0e082085332e9219647eb76ef52522ff
Sylvain Briat <sylvain.briat@covivo.eu>
2020-05-26 18:34:48 +0200

    app versioning system

---
+++ api/src/Utility/Entity/Version.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Copyright (c) 2020, MOBICOOP. All rights reserved.
+ * This project is dual licensed under AGPL and proprietary licence.
+ ***************************
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <gnu.org/licenses>.
+ ***************************
+ *    Licence MOBICOOP described in the file
+ *    LICENSE
+ **************************/
+
+namespace App\Utility\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * Versioning entity.
+ *
+ * @ApiResource(
+ *      attributes={
+ *          "force_eager"=false,
+ *          "normalization_context"={"groups"={"readVersion"}, "enable_max_depth"="true"},
+ *      },
+ *      collectionOperations={
+ *          "get"
+ *      },
+ *      itemOperations={
+ *          "get"={
+ *              "security"="is_granted('reject',object)"
+ *          }
+ *      }
+ * )
+ */
+class Version
+{
+    const DEFAULT_ID = 1;
+
+    /**
+     * @var int $id The id of this version.
+     *
+     * @ApiProperty(identifier=true)
+     * @Groups("readVersion")
+     */
+    private $id;
+
+    public function __construct()
+    {
+        $this->id = self::DEFAULT_ID;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
