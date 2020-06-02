3cbb2958ef188843fdeadfef032e4abbf189d0cd
jdemangeon <julien@marmelab.com>
2020-06-02 17:26:09 +0200

    Simplify Hydra Admin Schema

--- admin/src/dataProviderAdapter.js
+++ admin/src/dataProviderAdapter.js
@@ -1,4 +1,5 @@
 import pick from 'lodash.pick';
+import { fetchJson } from './fetchJson';
 
 /**
  * This file aims to fix some API weaknesses
@@ -56,10 +57,137 @@ const pickManagedSolidaryVolunteerData = (params) => ({
   ]),
 });
 
+/**
+ * Custom getOne Provider for "users"
+ * Because we need to map roles territies
+ */
+const getOneUser = async (provider, params) => {
+  const { data: user } = await provider.getOne('users', {
+    id: params.id.search('users') === -1 ? `users/${params.id}` : params.id,
+  });
+
+  const rolesTerritory = await Promise.all(
+    user.userAuthAssignments.map((element) =>
+      provider
+        .getOne('userAuthAssignments', { id: element })
+        .then(({ data }) => data)
+        .catch((error) => {
+          console.log('An error occured during user rights retrieving:', error);
+        })
+    )
+  );
+
+  user.rolesTerritory = rolesTerritory;
+
+  return { data: user };
+};
+
+const extractRoles = (fields) => {
+  const newRoles = [];
+
+  fields.forEach((v) => {
+    const territory = v.territory;
+    // There is many roles
+    if (Array.isArray(v.roles)) {
+      v.roles.forEach((r) => {
+        v != null ? newRoles.push({ authItem: r, territory }) : newRoles.push({ authItem: r });
+      });
+      // There is just 1 roles
+    } else {
+      v != null
+        ? newRoles.push({ authItem: v.roles, territory })
+        : newRoles.push({ authItem: v.roles });
+    }
+  });
+
+  return newRoles;
+};
+
+/**
+ * Apply a custom logic on user roles before update
+ */
+const updateUser = async (provider, params) => {
+  const newParams = { ...params };
+
+  newParams.data.userAuthAssignments =
+    newParams.data.fields != null
+      ? extractRoles(newParams.data.fields)
+      : newParams.data.rolesTerritory.map(({ territory, authItem }) =>
+          territory != null ? { authItem, territory } : { authItem }
+        );
+
+  return provider.update('users', {
+    id: newParams.id,
+    data: newParams.data,
+    previousData: newParams.data.previousData,
+  });
+};
+
+/**
+ * Apply custom logic on user create
+ */
+const createUser = async (provider, params) => {
+  const newParams = { ...params };
+
+  newParams.data.userAuthAssignments = extractRoles(newParams.data.fields);
+  newParams.data.addresses = [
+    {
+      ...newParams.data.address,
+      home: true,
+    },
+  ];
+
+  /* Add custom fields fo fit with api */
+  newParams.data.passwordSendType = 1;
+  newParams.data.language = 'fr_FR';
+  newParams.data.userDelegate = `/users/${global.localStorage.getItem('id')}`;
+  /* Add custom fields fo fit with api */
+
+  return fetchJson(process.env.REACT_APP_API + process.env.REACT_APP_CREATE_USER, {
+    method: 'POST',
+    body: JSON.stringify(newParams.data),
+  }).then(({ json }) => ({
+    data: { ...newParams.data, id: json.id },
+  }));
+};
+
 export const dataProviderAdapter = (originalProvider) => ({
   ...originalProvider,
+  getOne: (resource, params) => {
+    if (resource === 'users') {
+      return getOneUser(originalProvider, params);
+    }
+
+    return originalProvider.getOne(resource, params);
+  },
+  getMany: (resource, params) => {
+    if (resource === 'addresses') {
+      // Adapt resource access for addresses
+      return originalProvider.getMany('addresses/search', params);
+    }
+
+    return originalProvider.getMany(resource, params);
+  },
+  getList: (resource, params) => {
+    if (resource === 'communities') {
+      // Adapt resource access for communities
+      return originalProvider.getList('communities/manage', params);
+    }
+
+    return originalProvider.getList(resource, params);
+  },
+  create: (resource, params) => {
+    if (resource === 'users') {
+      return createUser(originalProvider, params);
+    }
+
+    return originalProvider.create(resource, params);
+  },
   update: (resource, params) => {
     let newParams = transformId({ ...params });
+    if (resource === 'users') {
+      return updateUser(originalProvider, newParams);
+    }
 
     if (resource === 'solidary_volunteers') {
       newParams = pickManagedSolidaryVolunteerData(newParams);
