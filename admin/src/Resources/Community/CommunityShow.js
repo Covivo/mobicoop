06e99a42d3e59ab53ac3b34650d5a41b20ceda56
Julien <julien.deschampt@gmail.com>
2020-06-02 14:24:50 +0200

    send email from community users show

--- admin/src/Resources/Community/CommunityShow.js
+++ admin/src/Resources/Community/CommunityShow.js
@@ -37,13 +37,12 @@ import { addressRenderer } from '../../utils/renderers';
 import { validationChoices, statusChoices } from './communityChoices';
 import isAuthorized from '../../auth/permissions';
 import EmailComposeButton from '../../components/email/EmailComposeButton';
-import ResetButton from '../../components/button/ResetButton';
 
 const UserBulkActionButtons = (props) => (
   <>
     {isAuthorized('mass_create') && <EmailComposeButton label="Email" {...props} />}
     {/* default bulk delete action */}
-    <ResetButton label="Reset email" {...props} />
+    <BulkDeleteButton {...props} />
   </>
 );
 
