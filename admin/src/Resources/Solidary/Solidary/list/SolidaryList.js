4cd2e1a5dba0a7cc5416ec242f76bc6b60f91a50
jdemangeon <julien@marmelab.com>
2020-06-08 18:01:08 +0200

    Solidary Listing

--- admin/src/Resources/Solidary/Solidary/list/SolidaryList.js
+++ admin/src/Resources/Solidary/Solidary/list/SolidaryList.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
-import { List, Datagrid, TextField, ShowButton } from 'react-admin';
+import {
+  List,
+  Datagrid,
+  TextField,
+  ShowButton,
+  ReferenceField,
+  DateField,
+  Filter,
+} from 'react-admin';
+
+const TodoField = ({ detail }) => <span>@TODO: {detail}</span>;
+
+const SolidaryFilter = (props) => <Filter {...props}>{/* TODO: See questions */}</Filter>;
 
 export const SolidaryList = (props) => (
-  <List {...props} title="Demandes solidaires > liste" perPage={25}>
+  <List
+    {...props}
+    bulkActionButtons={false}
+    filters={<SolidaryFilter />}
+    title="Demandes solidaires > liste"
+    perPage={25}
+  >
     <Datagrid>
-      <TextField source="id" label="Id" />
+      <TextField source="originId" label="ID" />
+      <ReferenceField source="subject" reference="subjects" link={false}>
+        <TextField source="label" />
+      </ReferenceField>
+      <TodoField label="Trajet demandé" detail="Champ 'trajet'" />
+      <TodoField label="Prénom" detail="/!\ Accès deep" />
+      <TodoField label="Nom" detail="/!\ Accès deep" />
+      <TodoField label="% avanc." detail="/!\ Accès deep" />
+      <TodoField label="Dernière action" detail="/!\ Accès deep" />
+      <DateField source="createdDate" label="Date" />
       <ShowButton />
     </Datagrid>
   </List>
