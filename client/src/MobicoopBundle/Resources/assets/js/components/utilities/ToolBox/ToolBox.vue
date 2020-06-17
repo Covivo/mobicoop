de4792ca33de6f959604bd49eedc89f4762a8008
Maxime Bardot <maxime.bardot@gmail.com>
2020-06-17 15:41:58 +0200

    New toolbox component

---
+++ client/src/MobicoopBundle/Resources/assets/js/components/utilities/ToolBox/ToolBox.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        cols="12"
+      >
+        <h1 class="display-1 primary--text text-center font-weight-bold">
+          {{ $t('title') }}
+        </h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Translations from "@translations/components/article/ToolBox.json";
+export default {
+  i18n: {
+    messages: Translations,
+  },
+  props: {},
+  data() {
+    return {
+    }
+  }
+};
+</script>
