c0e00db2fe441833368204c354961192146548c2
jdemangeon <julien@marmelab.com>
2020-06-22 16:58:34 +0200

    Fix Add Diary From Dashboard + Add User Delegation

--- admin/src/Resources/Solidary/Solidary/view/SolidaryAnimation.js
+++ admin/src/Resources/Solidary/Solidary/view/SolidaryAnimation.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import { useGetList } from 'react-admin';
+import { useGetList, useTranslate } from 'react-admin';
 import { Card, Grid, LinearProgress, List, Button } from '@material-ui/core';
-import CreateRelatedActionButton from './CreateRelatedActionButton';
 import SolidaryAnimationItem from './SolidaryAnimationItem';
+import { SolidaryAddDiaryPopup } from './SolidaryAddDiaryPopup';
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -13,51 +13,70 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+// Since sorting doesn't work, we sort animation by ourself
+const sortByCreatedAt = (a, b) => (new Date(a.createdAt) < new Date(b.createdAt) ? 1 : -1);
+
 const SolidaryAnimation = ({ record }) => {
   const classes = useStyles();
-  // List of actions
+  const translate = useTranslate();
+
   const { data, loaded } = useGetList(
     'solidary_animations',
     { page: 1, perPage: 100 },
     { field: 'createdDate', order: 'ASC' },
     { solidary: record.id }
   );
+
   const animations = Object.values(data) || [];
   const [seeAllAnimations, setSeeAllAnimations] = useState(false);
+  const [displayAddDiary, setDisplayAddDiary] = useState(false);
 
   return (
-    <Card raised className={classes.card}>
-      <Grid container direction="row" justify="space-between" alignItems="center" spacing={2}>
-        <Grid item>
-          <b>Dernière action</b>
+    <>
+      <Card raised className={classes.card}>
+        <Grid container direction="row" justify="space-between" alignItems="center" spacing={2}>
+          <Grid item>
+            <b>Dernière action</b>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              onClick={() => setDisplayAddDiary(true)}
+            >
+              {translate('custom.solidaryAnimation.addAction')}
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <CreateRelatedActionButton record={record} />
-        </Grid>
-      </Grid>
-      {loaded ? (
-        animations && animations.length ? (
-          <>
-            <List>
-              {animations
-                .filter((a) => seeAllAnimations || a.id === animations[0].id)
-                .map((a) => (
-                  <SolidaryAnimationItem item={a} />
-                ))}
-            </List>
-            {!seeAllAnimations && animations.length > 1 && (
-              <Button onClick={() => setSeeAllAnimations((a) => !a)}>
-                Voir toutes les actions
-              </Button>
-            )}
-          </>
+        {loaded ? (
+          animations && animations.length ? (
+            <>
+              <List>
+                {animations
+                  .filter((a) => seeAllAnimations || a.id === animations[0].id)
+                  .sort(sortByCreatedAt)
+                  .map((a) => (
+                    <SolidaryAnimationItem item={a} />
+                  ))}
+              </List>
+              {!seeAllAnimations && animations.length > 1 && (
+                <Button onClick={() => setSeeAllAnimations((a) => !a)}>
+                  Voir toutes les actions
+                </Button>
+              )}
+            </>
+          ) : (
+            <List>Pas encore d&apos;action pour cette demande</List>
+          )
         ) : (
-          <List>Pas encore d&apos;action pour cette demande</List>
-        )
-      ) : (
-        <LinearProgress />
+          <LinearProgress />
+        )}
+      </Card>
+      {displayAddDiary && record && (
+        <SolidaryAddDiaryPopup solidary={record} onClose={() => setDisplayAddDiary(false)} />
       )}
-    </Card>
+    </>
   );
 };
 
