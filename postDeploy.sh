cec0cfcfa1d6ed77b4030e0a26e11a4c920884c5
Sylvain Briat <sylvain.briat@covivo.eu>
2020-06-30 17:20:06 +0200

    pass env to crontab update script

--- postDeploy.sh
+++ postDeploy.sh
@@ -22,6 +22,9 @@ done
 cd /var/www/$VERSION/$INSTANCE/api;
 php bin/console doctrine:migrations:migrate --env=$VERSION_MIGRATE -n;
 
+# Crontab update
+python3 /var/www/$VERSION/$INSTANCE/scripts/updateCrontab.py -env $VERSION_MIGRATE
+
 #Admin build
 cd /var/www/$VERSION/$INSTANCE/admin;
 rm -Rf node_modules;
