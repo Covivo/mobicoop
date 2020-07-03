f6282b538e5925b81588eea742afc1bd42aaef6d
Maxime Bardot <maxime.bardot@gmail.com>
2020-07-03 17:28:56 +0200

    Watchers

--- client/src/MobicoopBundle/Resources/assets/js/components/utilities/DayListChips.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/utilities/DayListChips.vue
@@ -96,18 +96,61 @@ export default {
   data() {
     return {
       daysList: this.$t('days.list'),
-      monColor:(this.monActive) ? this.colorActive : this.colorInactive,
-      tueColor:(this.tueActive) ? this.colorActive : this.colorInactive,
-      wedColor:(this.wedActive) ? this.colorActive : this.colorInactive,
-      thuColor:(this.thuActive) ? this.colorActive : this.colorInactive,
-      friColor:(this.friActive) ? this.colorActive : this.colorInactive,
-      satColor:(this.satActive) ? this.colorActive : this.colorInactive,
-      sunColor:(this.sunActive) ? this.colorActive : this.colorInactive
+      mon:(this.monActive) ? true : false,
+      tue:(this.tueActive) ? true : false,
+      wed:(this.wedActive) ? true : false,
+      thu:(this.thuActive) ? true : false,
+      fri:(this.friActive) ? true : false,
+      sat:(this.satActive) ? true : false,
+      sun:(this.sunActive) ? true :false
     }
-  }, 
+  },
+  computed:{
+    monColor() { if(this.mon) { return this.colorActive }else {return this.colorInactive}},
+    tueColor() { if(this.tue) { return this.colorActive }else {return this.colorInactive}},
+    wedColor() { if(this.wed) { return this.colorActive }else {return this.colorInactive}},
+    thuColor() { if(this.thu) { return this.colorActive }else {return this.colorInactive}},
+    friColor() { if(this.fri) { return this.colorActive }else {return this.colorInactive}},
+    satColor() { if(this.sat) { return this.colorActive }else {return this.colorInactive}},
+    sunColor() { if(this.sun) { return this.colorActive }else {return this.colorInactive}}
+  },
+  watch:{
+    monActive(newValue){(newValue) ? this.mon = true : this.mon = false;},
+    tueActive(newValue){(newValue) ? this.tue = true : this.tue = false;},
+    wedActive(newValue){(newValue) ? this.wed = true : this.wed = false;},
+    thuActive(newValue){(newValue) ? this.thu = true : this.thu = false;},
+    friActive(newValue){(newValue) ? this.fri = true : this.fri = false;},
+    satActive(newValue){(newValue) ? this.sat = true : this.sat = false;},
+    sunActive(newValue){(newValue) ? this.sun = true : this.sun = false;}
+  },
   methods:{
     dayName(day){
       return (this.longDaysName) ? this.$t('days.'+day+'longName') : this.$t('days.'+day+'.shortName');
+    },
+    clickDay(day){
+      switch(day){
+      case this.daysList[0]:
+        this.mon != this.mon;
+        break;
+      case this.daysList[1]:
+        this.tue != this.tue;
+        break;
+      case this.daysList[2]:
+        this.wed != this.wed;
+        break;
+      case this.daysList[3]:
+        this.thu != this.thu;
+        break;
+      case this.daysList[4]:
+        this.fri != this.fri;
+        break;
+      case this.daysList[5]:
+        this.sat != this.sat;
+        break;
+      case this.daysList[6]:
+        this.sun != this.sun;
+        break;
+      }
     }
   }
 }
