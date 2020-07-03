13ed48e5cfc1e9cd019cb349e8159f0ec5f26d7a
Maxime Bardot <maxime.bardot@gmail.com>
2020-07-03 17:11:24 +0200

    data instead of methods for colors

--- client/src/MobicoopBundle/Resources/assets/js/components/utilities/DayListChips.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/utilities/DayListChips.vue
@@ -1,12 +1,46 @@
 <template>
   <div>
     <v-chip
-      v-for="(currentDay, index) in daysList"
-      :key="index"
-      :color="chipColor(currentDay)"
+      :color="monColor"
       class="mr-2"
     >
-      {{ dayName(currentDay) }}
+      {{ dayName(daysList[0]) }}
+    </v-chip>
+    <v-chip
+      :color="tueColor"
+      class="mr-2"
+    >
+      {{ dayName(daysList[1]) }}
+    </v-chip>
+    <v-chip
+      :color="wedColor"
+      class="mr-2"
+    >
+      {{ dayName(daysList[2]) }}
+    </v-chip>
+    <v-chip
+      :color="thuColor"
+      class="mr-2"
+    >
+      {{ dayName(daysList[3]) }}
+    </v-chip>
+    <v-chip
+      :color="friColor"
+      class="mr-2"
+    >
+      {{ dayName(daysList[4]) }}
+    </v-chip>
+    <v-chip
+      :color="satColor"
+      class="mr-2"
+    >
+      {{ dayName(daysList[5]) }}
+    </v-chip>
+    <v-chip
+      :color="sunColor"
+      class="mr-2"
+    >
+      {{ dayName(daysList[6]) }}
     </v-chip>
   </div>
 </template>
@@ -61,24 +95,19 @@ export default {
   },
   data() {
     return {
-      daysList: this.$t('days.list')
+      daysList: this.$t('days.list'),
+      monColor:(this.monActive) ? this.colorActive : this.colorInactive,
+      tueColor:(this.tueActive) ? this.colorActive : this.colorInactive,
+      wedColor:(this.wedActive) ? this.colorActive : this.colorInactive,
+      thuColor:(this.thuActive) ? this.colorActive : this.colorInactive,
+      friColor:(this.friActive) ? this.colorActive : this.colorInactive,
+      satColor:(this.satActive) ? this.colorActive : this.colorInactive,
+      sunColor:(this.sunActive) ? this.colorActive : this.colorInactive
     }
-  },  
+  }, 
   methods:{
     dayName(day){
       return (this.longDaysName) ? this.$t('days.'+day+'longName') : this.$t('days.'+day+'.shortName');
-    },
-    chipColor(day){
-      switch(day){
-      case this.daysList[0]: return (this.monActive) ? this.colorActive : this.colorInactive;
-      case this.daysList[1]: return (this.tueActive) ? this.colorActive : this.colorInactive;
-      case this.daysList[2]: return (this.wedActive) ? this.colorActive : this.colorInactive;
-      case this.daysList[3]: return (this.thuActive) ? this.colorActive : this.colorInactive;
-      case this.daysList[4]: return (this.friActive) ? this.colorActive : this.colorInactive;
-      case this.daysList[5]: return (this.satActive) ? this.colorActive : this.colorInactive;
-      case this.daysList[6]: return (this.sunActive) ? this.colorActive : this.colorInactive;
-      default: return this.colorInactive;
-      }
     }
   }
 }
