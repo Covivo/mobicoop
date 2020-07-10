5a758ec45e07a53dd9f1cfbe66563ac95634c012
Maxime Bardot <maxime.bardot@gmail.com>
2020-07-10 15:59:17 +0200

    PaymentProfile ORM entity

---
+++ api/src/Payment/Entity/PaymentProfile.php
@@ -0,0 +1,216 @@
+<?php
+
+/**
+ * Copyright (c) 2020, MOBICOOP. All rights reserved.
+ * This project is dual licensed under AGPL and proprietary licence.
+ ***************************
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <gnu.org/licenses>.
+ ***************************
+ *    Licence MOBICOOP described in the file
+ *    LICENSE
+ **************************/
+
+namespace App\Payment\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\PersistentCollection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use App\User\Entity\User;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * A payment profile
+ *
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ * @ApiResource(
+ *      attributes={
+ *          "force_eager"=false,
+ *          "normalization_context"={"groups"={"readPayment"}, "enable_max_depth"="true"},
+ *          "denormalization_context"={"groups"={"writePayment"}}
+ *      },
+ *      collectionOperations={"get","post"},
+ *      itemOperations={"get","put"}
+ * )
+ * @author Maxime Bardot <maxime.bardot@mobicoop.org>
+ */
+class PaymentProfile
+{
+    const STATUS_INACTIVE = 0;
+    const STATUS_ACTIVE = 1;
+
+    /**
+     * @var int The id of this payment profile
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Groups({"readPayment"})
+     */
+    private $id;
+
+    /**
+     * @var User The user owning this payment profile
+     *
+     * @ApiProperty(push=true)
+     * @ORM\ManyToOne(targetEntity="\App\User\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+     * @Groups({"readPayment","writePayment"})
+     * @MaxDepth(1)
+     * @Assert\NotBlank
+     */
+    private $user;
+
+    /**
+     * @var string The provider managing this payment profile
+     *
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"readPayment","writePayment"})
+     */
+    private $provider;
+
+    /**
+     * @var string The id used by the provider of this payment profile
+     *
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"readPayment","writePayment"})
+     */
+    private $identifier;
+
+    /**
+     * @var int The status of this payment profil (0 : Inactive, 1 : Active)
+     *
+     * @ORM\Column(type="integer")
+     * @Groups({"readPayment","writePayment"})
+     */
+    private $status;
+
+    /**
+     * @var \DateTimeInterface Creation date.
+     *
+     * @ORM\Column(type="datetime", nullable=true)
+     * @Groups({"readPayment"})
+     */
+    private $createdDate;
+
+    /**
+     * @var \DateTimeInterface Updated date.
+     *
+     * @ORM\Column(type="datetime", nullable=true)
+     * @Groups({"readPayment"})
+     */
+    private $updatedDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getProvider(): String
+    {
+        return $this->provider;
+    }
+
+    public function setProvider(String $provider): self
+    {
+        $this->provider = $provider;
+
+        return $this;
+    }
+
+    public function getIdentifier(): String
+    {
+        return $this->identifier;
+    }
+
+    public function setIdentifier(String $identifier): self
+    {
+        $this->identifier = $identifier;
+
+        return $this;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedDate(): ?\DateTimeInterface
+    {
+        return $this->createdDate;
+    }
+
+    public function setCreatedDate(\DateTimeInterface $createdDate): self
+    {
+        $this->createdDate = $createdDate;
+
+        return $this;
+    }
+
+    public function getUpdatedDate(): ?\DateTimeInterface
+    {
+        return $this->updatedDate;
+    }
+
+    public function setUpdatedDate(\DateTimeInterface $updatedDate): self
+    {
+        $this->updatedDate = $updatedDate;
+
+        return $this;
+    }
+
+    // DOCTRINE EVENTS
+
+    /**
+     * Creation date.
+     *
+     * @ORM\PrePersist
+     */
+    public function setAutoCreatedDate()
+    {
+        $this->setCreatedDate(new \Datetime());
+    }
+
+    /**
+     * Update date.
+     *
+     * @ORM\PreUpdate
+     */
+    public function setAutoUpdatedDate()
+    {
+        $this->setUpdatedDate(new \Datetime());
+    }
+}
