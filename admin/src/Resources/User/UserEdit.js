60381afe51355597132ed5c749fcc38b4a4ae36d
Julien <julien.deschampt@gmail.com>
2020-07-21 12:24:53 +0200

    fix hack roles 1.8

--- admin/src/Resources/User/UserEdit.js
+++ admin/src/Resources/User/UserEdit.js
@@ -26,6 +26,10 @@ const useStyles = makeStyles({
     display: 'inline-flex',
     marginRight: '1rem',
   },
+  hiddenInput: {
+    width: '0px',
+    display: 'inline-flex',
+  },
   footer: { marginTop: '2rem' },
 });
 
@@ -172,6 +176,13 @@ const UserEdit = (props) => {
             label={translate('custom.label.user.carpoolSetting.smoke')}
             choices={smoke}
             formClassName={classes.spacedHalfwidth}
+          />
+          <TextInput
+            fullWidth
+            label=''
+            type='hidden'
+            source="hidden"
+            formClassName={classes.hiddenInput}
           />
         </FormTab>
         <FormTab label={translate('custom.label.user.manageRoles')}>
