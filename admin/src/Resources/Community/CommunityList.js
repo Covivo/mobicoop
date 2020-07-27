1b8a4909eed66a3fea9c991f82d737090516edae
jdemangeon <julien@marmelab.com>
2020-07-27 12:01:59 +0200

    21772: Fix Advanced RA Feature Usage

--- admin/src/Resources/Community/CommunityList.js
+++ admin/src/Resources/Community/CommunityList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cloneElement } from 'react';
+
 import {
   List,
   Datagrid,
@@ -12,15 +12,10 @@ import {
   Filter,
   useTranslate,
   ImageField,
-  useListContext,
-  TopToolbar,
-  CreateButton,
-  ExportButton,
-  Button,
-  sanitizeListRestProps,
 } from 'react-admin';
 
 import FullNameField from '../User/FullNameField';
+import { hasCommunityEditRight } from '.';
 import { isAdmin } from '../../auth/permissions';
 
 const CommunityFilter = (props) => (
@@ -32,59 +27,17 @@ const CommunityPanel = ({ id, record, resource }) => (
   <div dangerouslySetInnerHTML={{ __html: record.fullDescription }} />
 );
 
-const ListActions = (props) => {
-  const {
-    className,
-    exporter,
-    filters,
-    maxResults,
-    ...rest
-  } = props;
-  const {
-    currentSort,
-    resource,
-    displayedFilters,
-    filterValues,
-    hasCreate,
-    basePath,
-    selectedIds,
-    showFilter,
-    total,
-  } = useListContext();
-  return (
-    <TopToolbar className={className} {...sanitizeListRestProps(rest)}>
-      {filters && cloneElement(filters, {
-        resource,
-        showFilter,
-        displayedFilters,
-        filterValues,
-        context: 'button',
-      })}
-      {
-        isAdmin() && <CreateButton basePath={basePath} />
-      }
-      <ExportButton
-        disabled={total === 0}
-        resource={resource}
-        sort={currentSort}
-        filterValues={filterValues}
-        maxResults={maxResults}
-      />
-    </TopToolbar>
-  );
-};
-
 export const CommunityList = (props) => {
   const translate = useTranslate();
 
   return (
     <List
       {...props}
+      hasCreate={isAdmin()}
       title="Communautés > liste"
       perPage={25}
       filters={<CommunityFilter />}
       sort={{ field: 'originId', order: 'DESC' }}
-      actions={<ListActions />}
     >
       <Datagrid expand={<CommunityPanel />}>
         <TextField source="originId" label="ID" sortBy="id" />
@@ -100,7 +53,7 @@ export const CommunityList = (props) => {
         />
         <FullNameField source="user" label={translate('custom.label.community.createdBy')} />
         <ShowButton />
-        <EditButton />
+        {hasCommunityEditRight() && <EditButton />}
       </Datagrid>
     </List>
   );
