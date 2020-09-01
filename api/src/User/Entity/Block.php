caab9afe5b5f1b2b3637e731890ba1ebfa33330e
Maxime Bardot <maxime.bardot@gmail.com>
2020-09-01 10:39:31 +0200

    Entity Block

---
+++ api/src/User/Entity/Block.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Copyright (c) 2020, MOBICOOP. All rights reserved.
+ * This project is dual licensed under AGPL and proprietary licence.
+ ***************************
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <gnu.org/licenses>.
+ ***************************
+ *    Licence MOBICOOP described in the file
+ *    LICENSE
+ **************************/
+
+namespace App\User\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * A Block between two Users
+ *
+ * Note : force eager is set to false to avoid max number of nested relations (can occure despite of maxdepth... https://github.com/api-platform/core/issues/1910)
+ *
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ */
+class Block
+{
+    /**
+     * @var int The id of this Block.
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Groups({"readBlock"})
+     * @ApiProperty(identifier=true)
+     */
+    private $id;
+
+    /**
+     * @var User The User who made the Block
+     *
+     * @ORM\ManyToOne(targetEntity="\App\User\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+    */
+    private $user;
+
+    /**
+     * @var User The User blocked by $user
+     *
+     * @ORM\ManyToOne(targetEntity="\App\User\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $blockedUser;
+
+    /**
+     * @var \DateTimeInterface Creation date of this Block.
+     *
+     * @ORM\Column(type="datetime")
+     * @Groups({"readBlock"})
+     */
+    private $createdDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getBlockedUser(): ?User
+    {
+        return $this->blockedUser;
+    }
+
+    public function setBlockedUser(User $blockedUser): self
+    {
+        $this->blockedUser = $blockedUser;
+
+        return $this;
+    }
+
+    public function getCreatedDate(): ?\DateTimeInterface
+    {
+        return $this->createdDate;
+    }
+
+    public function setCreatedDate(\DateTimeInterface $createdDate): self
+    {
+        $this->createdDate = $createdDate;
+
+        return $this;
+    }
+
+
+    // DOCTRINE EVENTS
+
+    /**
+     * Creation date.
+     *
+     * @ORM\PrePersist
+     */
+    public function setAutoCreatedDate()
+    {
+        $this->setCreatedDate(new \Datetime());
+    }
+}
