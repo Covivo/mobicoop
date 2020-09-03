ecc1b5284dab57f823be438b2f8be9b35b67b3a8
jdemangeon <julien@marmelab.com>
2020-09-03 14:33:03 +0200

    21272: Ajout Coordonnées GPS Point Relais

--- admin/src/Resources/RelayPoint/RelayPoint/RelayPointEdit.js
+++ admin/src/Resources/RelayPoint/RelayPoint/RelayPointEdit.js
@@ -13,6 +13,7 @@ import {
   NumberInput,
   FunctionField,
   ReferenceField,
+  useTranslate,
   FormDataConsumer,
 } from 'react-admin';
 
@@ -27,59 +28,72 @@ const statusChoices = [
   { id: 2, name: 'Inactif' },
 ];
 
-export const RelayPointEdit = (props) => (
-  <Edit {...props} title="Points relais > éditer">
-    <TabbedForm>
-      <FormTab label="Identité">
-        <ReferenceInput source="user" label="Créateur" reference="users" defaultValue={userId}>
-          <SelectInput optionText={<UserRenderer />} />
-        </ReferenceInput>
-        <TextInput source="name" label="Nom" validate={required()} />
-        <ReferenceField
-          source="address.id"
-          label="Adresse actuelle"
-          reference="addresses"
-          link={false}
-        >
-          <FunctionField render={addressRenderer} />
-        </ReferenceField>
-        <GeocompleteInput source="address" label="Nouvelle addresse" validate={required()} />
-        <SelectInput source="status" label="Status" choices={statusChoices} defaultValue={1} />
-        <ReferenceInput source="relayPointType.id" label="Types" reference="relay_point_types">
-          <SelectInput optionText="name" />
-        </ReferenceInput>
-        <TextInput source="description" label="Description" validate={required()} />
-        <RichTextInput
-          source="fullDescription"
-          label="Description complète"
-          validate={required()}
-        />
-      </FormTab>
-      <FormTab label="Communauté">
-        <ReferenceInput source="community" label="Communauté" reference="communities" resettable>
-          <SelectInput optionText="name" />
-        </ReferenceInput>
-        <FormDataConsumer>
-          {({ formData, ...rest }) =>
-            formData.community && (
-              <BooleanInput source="private" label="Privé à cette communauté" {...rest} />
-            )
-          }
-        </FormDataConsumer>
-      </FormTab>
-      <FormTab label="Propriétés">
-        <NumberInput source="places" label="Nombre de places" />
-        <NumberInput source="placesDisabled" label="Nombre de places handicapés" />
-        <BooleanInput source="free" label="Gratuit" defaultValue={true} />
-        <BooleanInput source="secured" label="Sécurisé" />
-        <BooleanInput source="official" label="Officiel" />
-        <BooleanInput source="suggested" label="Suggestion autocomplétion" />
-      </FormTab>
-      <FormTab label="Images">
-        <RelayPointImageUpload
-          label="Images"
-        />
-      </FormTab>
-    </TabbedForm>
-  </Edit>
-);
+const isLatitude = (lat) => isFinite(lat) && Math.abs(lat) <= 90;
+const isLongitude = (lng) => isFinite(lng) && Math.abs(lng) <= 180;
+
+export const RelayPointEdit = (props) => {
+  const translate = useTranslate();
+
+  const validateLatitude = (value) =>
+    !isLatitude(value) ? translate('custom.label.relayPoint.invalidLatitude') : undefined;
+
+  const validateLongitude = (value) =>
+    !isLongitude(value) ? translate('custom.label.relayPoint.invalidLongitude') : undefined;
+
+  return (
+    <Edit {...props} title="Points relais > éditer">
+      <TabbedForm>
+        <FormTab label="Identité">
+          <ReferenceInput source="user" label="Créateur" reference="users" defaultValue={userId}>
+            <SelectInput optionText={<UserRenderer />} />
+          </ReferenceInput>
+          <TextInput source="name" label="Nom" validate={required()} />
+          <ReferenceField
+            source="address.id"
+            label="Adresse actuelle"
+            reference="addresses"
+            link={false}
+          >
+            <FunctionField render={addressRenderer} />
+          </ReferenceField>
+          <GeocompleteInput source="address" label="Nouvelle addresse" />
+          <SelectInput source="status" label="Status" choices={statusChoices} defaultValue={1} />
+          <ReferenceInput source="relayPointType.id" label="Types" reference="relay_point_types">
+            <SelectInput optionText="name" />
+          </ReferenceInput>
+          <TextInput source="description" label="Description" validate={required()} />
+          <RichTextInput
+            source="fullDescription"
+            label="Description complète"
+            validate={required()}
+          />
+          <NumberInput source="address.latitude" validate={[validateLatitude]} />
+          <NumberInput source="address.longitude" validate={[validateLongitude]} />
+        </FormTab>
+        <FormTab label="Communauté">
+          <ReferenceInput source="community" label="Communauté" reference="communities" resettable>
+            <SelectInput optionText="name" />
+          </ReferenceInput>
+          <FormDataConsumer>
+            {({ formData, ...rest }) =>
+              formData.community && (
+                <BooleanInput source="private" label="Privé à cette communauté" {...rest} />
+              )
+            }
+          </FormDataConsumer>
+        </FormTab>
+        <FormTab label="Propriétés">
+          <NumberInput source="places" label="Nombre de places" />
+          <NumberInput source="placesDisabled" label="Nombre de places handicapés" />
+          <BooleanInput source="free" label="Gratuit" defaultValue={true} />
+          <BooleanInput source="secured" label="Sécurisé" />
+          <BooleanInput source="official" label="Officiel" />
+          <BooleanInput source="suggested" label="Suggestion autocomplétion" />
+        </FormTab>
+        <FormTab label="Images">
+          <RelayPointImageUpload label="Images" />
+        </FormTab>
+      </TabbedForm>
+    </Edit>
+  );
+};
