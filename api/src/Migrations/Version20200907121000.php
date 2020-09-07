acb1954e0dcc9cbb9671227c2f545c397969daf4
Maxime Bardot <maxime.bardot@gmail.com>
2020-09-07 12:16:24 +0200

    typo in sql request

--- api/src/Migrations/Version20200907121000.php
+++ api/src/Migrations/Version20200907121000.php
@@ -13,9 +13,9 @@ final class Version20200907121000 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // add dashboard access
-        $this->addSql("INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES (239, NULL, 1, 'block_create', 'Create a Block (block or unblock a User)')");
+        $this->addSql("INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES (239, NULL, 1, 'block_create', 'Create a Block. Block or unblock a User')");
         $this->addSql("INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES (240, NULL, 1, 'block_blocked', 'List the Users blocked by a specific User')");
-        $this->addSql("INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES (241, NULL, 1, 'block_blockedby', 'List the Users currently blocking a specific User");
+        $this->addSql("INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES (241, NULL, 1, 'block_blockedby', 'List the Users currently blocking a specific User')");
 
         $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES (3, 239)");
         $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES (3, 240)");
