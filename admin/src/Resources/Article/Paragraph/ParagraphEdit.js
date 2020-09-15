9f3ef26d999d01e2b193edec114022a8c6f18ed6
Herve07h22 <herve.rincent@free.fr>
2020-09-15 14:33:56 +0200

    fixed issue #19939 (cannot edit paragraph)

--- admin/src/Resources/Article/Paragraph/ParagraphEdit.js
+++ admin/src/Resources/Article/Paragraph/ParagraphEdit.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import RichTextInput from 'ra-input-rich-text';
 
-import { Edit, SimpleForm, required, SelectInput, NumberInput, TextField } from 'react-admin';
-
-const statusChoices = [
-  { id: 0, name: "En cours d'édition" },
-  { id: 1, name: 'En ligne' },
-];
+import {
+  Edit,
+  SimpleForm,
+  required,
+  SelectInput,
+  NumberInput,
+  TextField,
+  useTranslate,
+} from 'react-admin';
 
 export const ParagraphEdit = (props) => {
-  const redirect = `/sections/`;
+  const translate = useTranslate();
+  const statusChoices = [
+    { id: 0, name: translate('custom.label.article.label.draft') },
+    { id: 1, name: translate('custom.label.article.label.published') },
+  ];
+
+  const section = props.location.state && props.location.state.section;
+  const sectionUri = encodeURIComponent(section);
+  const redirect = section ? `/sections/${sectionUri}/show/paragraphs` : 'show';
 
   return (
-    <Edit {...props} title="Articles > éditer un paragraphe">
+    <Edit
+      {...props}
+      title={translate('custom.label.article.title.edit_paragraph')}
+      undoable={false}
+    >
       <SimpleForm redirect={redirect}>
-        <TextField label="Section" source="section.title" />
-        <SelectInput source="status" label="Statut" choices={statusChoices} />
-        <RichTextInput source="text" label="Texte" validate={required()} />
-        <NumberInput source="position" label="Position" />
+        <TextField label={translate('custom.label.article.label.section')} source="section.title" />
+        <SelectInput
+          source="status"
+          label={translate('custom.label.article.label.status')}
+          choices={statusChoices}
+        />
+        <RichTextInput
+          source="text"
+          label={translate('custom.label.article.label.content')}
+          validate={required()}
+        />
+        <NumberInput source="position" label={translate('custom.label.article.label.position')} />
       </SimpleForm>
     </Edit>
   );
