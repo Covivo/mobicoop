0e3c21df9117adb29e9ca0deb20b420241c952e5
Nicolas Moutaud <nicolas.moutaud@epitech.eu>
2020-10-27 01:47:32 +0100

    feat: #21800 solidary regular ask with several slots

--- admin/src/Resources/Solidary/Solidary/view/NeedsAndStructure.js
+++ admin/src/Resources/Solidary/Solidary/view/NeedsAndStructure.js
@@ -3,7 +3,45 @@ import PropTypes from 'prop-types';
 import { Grid } from '@material-ui/core';
 
 export const NeedsAndStructure = ({ record }) => {
-  const { needs, solidaryUserStructure, operator } = record;
+  const { needs, solidaryUserStructure, operator, days, outwardTimes, returnTimes } = record;
+
+  const createSlots = () => {
+    const newSlots = [];
+
+    for (const [key, value] of Object.entries(days)) {
+      if (value) {
+        let found = false;
+        for (let i = 0; i < newSlots.length; i += 1) {
+          if (
+            outwardTimes[key] === newSlots[i].outwardTimes &&
+            returnTimes[key] === newSlots[i].returnTimes
+          ) {
+            newSlots[i].days[key] = value;
+            found = true;
+          }
+        }
+        if (!found) {
+          newSlots.push({
+            id: newSlots.length,
+            days: {
+              mon: false,
+              tue: false,
+              wed: false,
+              thu: false,
+              fri: false,
+              sat: false,
+              sun: false,
+              [key]: value,
+            },
+            outwardTimes: outwardTimes[key],
+            returnTimes: returnTimes[key],
+          });
+        }
+      }
+    }
+    console.log('[EDITION] RESULTAT FINALS SLOTS: ', newSlots);
+    return newSlots;
+  };
 
   return (
     <>
@@ -27,7 +65,7 @@ export const NeedsAndStructure = ({ record }) => {
           <b>Opérateur ayant enregistré la demande&nbsp;:</b>
         </Grid>
         <Grid item md={3} xs={6}>
-          {operator ? `${operator.givenName} ${operator.familyName}&nbsp;` : 'Non renseigné.'}
+          {operator ? `${operator.givenName} ${operator.familyName}` : 'Non renseigné.'}
         </Grid>
       </Grid>
     </>
