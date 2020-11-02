cf76cb36fcda8b2df7e404f0b1a4a2cca5b6b270
Celine <b3ck54@gmail.com>
2020-11-02 15:37:38 +0100

    change id auth item

--- api/src/Migrations/Version20201101101600.php
+++ api/src/Migrations/Version20201101101600.php
@@ -18,13 +18,13 @@ final class Version20201101101600 extends AbstractMigration
 
         $this->addSql("
         INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES 
-        ('242', NULL, '1', 'relay_point_map_list','View the list of relay point maps');
+        ('246', NULL, '1', 'relay_point_map_list','View the list of relay point maps');
         ");
 
 
         $this->addSql("
         INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES 
-        ('5', '242');
+        ('5', '246');
         ");
     }
 
