f04de123027aa7cde011d3e9005c054280546fc8
Maxime Bardot <maxime.bardot@gmail.com>
2021-02-08 17:28:16 +0100

    User DataPersister

---
+++ api/src/User/Interoperability/DataPersister/UserDataPersister.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright (c) 2021, MOBICOOP. All rights reserved.
+ * This project is dual licensed under AGPL and proprietary licence.
+ ***************************
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <gnu.org/licenses>.
+ ***************************
+ *    Licence MOBICOOP described in the file
+ *    LICENSE
+ **************************/
+
+namespace App\User\Interoperability\DataPersister;
+
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
+use App\User\Interoperability\Ressource\User;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Security;
+use App\User\Service\UserManager;
+
+final class UserDataPersister implements ContextAwareDataPersisterInterface
+{
+    private $request;
+    private $security;
+    private $userManager;
+
+    public function __construct(RequestStack $requestStack, Security $security, UserManager $userManager)
+    {
+        $this->request = $requestStack->getCurrentRequest();
+        $this->security = $security;
+        $this->userManager = $userManager;
+    }
+
+    public function supports($data, array $context = []): bool
+    {
+        return $data instanceof User && isset($context['collection_operation_name']) &&  $context['collection_operation_name'] == 'interop_post';
+    }
+
+    public function persist($data, array $context = [])
+    {
+        // call your persistence layer to save $data
+        if (is_null($data)) {
+            throw new \InvalidArgumentException("No user provided");
+        }
+
+        return $data;
+    }
+
+    public function remove($data, array $context = [])
+    {
+        // call your persistence layer to delete $data
+    }
+}
