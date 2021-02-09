0a384bacfb89f37e918db3382f1cf3c4f7ed6961
Maxime Bardot <maxime.bardot@gmail.com>
2021-02-09 09:44:47 +0100

    No _self rights for now

--- api/src/Migrations/Version20210208163000.php
+++ api/src/Migrations/Version20210208163000.php
@@ -14,17 +14,12 @@ final class Version20210208163000 extends AbstractMigration
     {
         // Add the interoperability right for User resource
 
-        $this->addSql("INSERT INTO `auth_rule` (`id`, `name`) VALUES ('30', 'InteropUserSelf');");
-        
         $this->addSql("INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES (257, NULL, '2', 'ROLE_INTEROPERABILITY', 'Interoperability platform');");
         $this->addSql("INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES (258, NULL, '1', 'interop_user_create', 'Create a User via interoperability');");
         $this->addSql("INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES (259, NULL, '1', 'interop_user_read', 'Read a User via interoperability');");
         $this->addSql("INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES (260, NULL, '1', 'interop_user_update', 'Update a User via interoperability');");
         $this->addSql("INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES (261, NULL, '1', 'interop_user_delete', 'Delete a User via interoperability');");
         $this->addSql("INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES (262, NULL, '1', 'interop_user_list', 'List Users via interoperability');");
-        $this->addSql("INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES (263, '30', '1', 'interop_user_read_self', 'Read its own profile via interoperability');");
-        $this->addSql("INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES (264, '30', '1', 'interop_user_update_self', 'Update its own profile via interoperability');");
-        $this->addSql("INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES (265, '30', '1', 'interop_user_delete_self', 'Delete its own profile via interoperability');");
 
         $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES ('257', '22');");
         $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES ('22', '258');");
@@ -32,12 +27,6 @@ final class Version20210208163000 extends AbstractMigration
         $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES ('22', '260');");
         $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES ('22', '261');");
         $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES ('22', '262');");
-        $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES ('263', '259');");
-        $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES ('264', '260');");
-        $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES ('265', '261');");
-        $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES ('4', '263');");
-        $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES ('4', '264');");
-        $this->addSql("INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES ('4', '265');");
     }
 
     public function down(Schema $schema) : void
