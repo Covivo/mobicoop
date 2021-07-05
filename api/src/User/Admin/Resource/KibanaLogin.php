3fe7788c37027136e0d7a43ceb506e7393c97a61
Max <maxime.bardot@gmail.com>
2021-07-05 14:05:13 +0200

    Evol#2652 Kibana logins

---
+++ api/src/User/Admin/Resource/KibanaLogin.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * Copyright (c) 2021, MOBICOOP. All rights reserved.
+ * This project is dual licensed under AGPL and proprietary licence.
+ ***************************
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <gnu.org/licenses>.
+ ***************************
+ *    Licence MOBICOOP described in the file
+ *    LICENSE
+ **************************/
+
+namespace App\User\Admin\Resource;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\User\Entity\User;
+
+/**
+ * Kibana Logins
+ *
+ * @ApiResource(
+ *      attributes={
+ *          "force_eager"=false,
+ *          "normalization_context"={"groups"={"readKibanaLogin"}, "enable_max_depth"="true"},
+ *      },
+ *      collectionOperations={
+ *          "ADMIN_kibana_logins"={
+ *              "method"="GET",
+ *              "security"="is_granted('access_admin',object)",
+ *              "path"="/admin/kibana_logins",
+ *              "swagger_context" = {
+ *                  "tags"={"Administration"}
+ *              }
+ *          },
+ *      },
+ *      itemOperations={
+ *          "get"={
+ *              "security"="is_granted('reject',object)",
+ *              "path"="/admin/kibana_logins/{id}",
+ *              "swagger_context" = {
+ *                  "summary"="Not implemented",
+ *                  "tags"={"Administration"}
+ *              }
+ *          }
+ *      }
+ * )
+ * @author Maxime Bardot <maxime.bardot@mobicoop.org>
+ */
+class KibanaLogin
+{
+    const DEFAULT_ID = 999999999999;
+    
+    /**
+     * @var int The id of this KibanaLogin
+     *
+     * @ApiProperty(identifier=true)
+     * @Groups({"readKibanaLogin"})
+     */
+    private $id;
+
+    /**
+     * @var string Username
+     *
+     * @Groups({"readKibanaLogin"})
+     */
+    private $username;
+
+    /**
+     * @var string Password
+     *
+     * @Groups({"readKibanaLogin"})
+     */
+    private $password;
+
+    public function __construct()
+    {
+        $this->id = self::DEFAULT_ID;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+        
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        
+        return $this;
+    }
+}
