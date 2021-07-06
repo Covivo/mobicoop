99c1e2e5c0ac226bab4b3e9b846f1b6fdbd2c737
Max <maxime.bardot@gmail.com>
2021-07-06 15:58:37 +0200

    module store for gamificationNotifications

--- client/src/MobicoopBundle/Resources/assets/js/app.js
+++ client/src/MobicoopBundle/Resources/assets/js/app.js
@@ -2,25 +2,12 @@
 
 import 'babel-polyfill';
 import { Vue, vuetify, i18n } from '@js/config/app'
-import Vuex from 'vuex'
+import { store } from './store';
 
 import components from '@js/config/components'
 
 // Import css
 import '@css/main.scss'
-
-Vue.use(Vuex);
-
-export const store = new Vuex.Store({
-  state: {
-    gamificationNotifications: {}
-  },
-  mutations: {
-    updateGamificationNotifications (state, gamificationNotifications) {
-      state.gamificationNotifications = gamificationNotifications;
-    }
-  }
-});
 
 new Vue({
   el: '#app',
