68ac5c56053086ca13310d0e526b5e2b080fe8dc
Worti <remi.wortemann@gmail.com>
2021-07-06 10:34:19 +0200

    resolve issue 2634

--- client/src/MobicoopBundle/Resources/assets/js/components/payment/Payment.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/payment/Payment.vue
@@ -1202,8 +1202,8 @@ export default {
       let payments = [];
       // if punctual 
       if (this.frequency == 1) {
-        // we check if we have made an action of payment on the currentItem and we send it only if that's the case
-        if (this.currentItem.mode) {
+        // we check if we have made an action of payment on the currentItem and we send it only if that's not the case
+        if (!this.currentItem.mode) {
           payments.push({"id":this.currentItem.id, "mode":2, "status":3});
         }
       } else {
@@ -1237,7 +1237,6 @@ export default {
           console.error(error);
         });
     },
-    
   }
 };
 </script>
