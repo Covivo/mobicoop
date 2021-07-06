6e4da0de1ab25c33daad8875da7ce7caeb198bbb
Max <maxime.bardot@gmail.com>
2021-07-06 15:36:14 +0200

    Add vuex + stire

--- client/src/MobicoopBundle/Resources/assets/js/app.js
+++ client/src/MobicoopBundle/Resources/assets/js/app.js
@@ -2,16 +2,31 @@
 
 import 'babel-polyfill';
 import { Vue, vuetify, i18n } from '@js/config/app'
+import Vuex from 'vuex'
 
 import components from '@js/config/components'
 
 // Import css
 import '@css/main.scss'
 
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    gamificationNotifications: {}
+  },
+  mutations: {
+    updateGamificationNotifications (state, gamificationNotifications) {
+      state.gamificationNotifications = gamificationNotifications;
+    }
+  }
+});
+
 new Vue({
   el: '#app',
   vuetify,
   i18n,
   token: '',
+  store,
   components: components
 })
