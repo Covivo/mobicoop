10271be1b87c5765ced245b8eebff31835218da9
b3ck54 <b3ck54@gmail.com>
2021-07-28 15:13:31 +0200

    if user is connected we claim the search

--- api/src/Carpool/Service/AdManager.php
+++ api/src/Carpool/Service/AdManager.php
@@ -815,6 +815,14 @@ class AdManager
         if (!is_null($proposal->getUser())) {
             $ad->setUserId($proposal->getUser()->getId());
         }
+        else{
+            // If the User is connected, we claim the search
+            if($this->security->getUser() instanceof User){
+                $this->claimAd($proposal->getId());
+                $ad->setUserId($this->security->getUser()->getId());
+            }
+        }
+        
         $ad->setCreatedDate($proposal->getCreatedDate());
         $aFilters = [];
         if (!is_null($filters)) {
