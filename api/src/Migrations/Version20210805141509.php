d94caad2e85880e5959a55b7ef2b8867cc39f8aa
Sylvain Briat <sylvain.briat@covivo.eu>
2021-08-05 16:19:02 +0200

    create external sources in relay point model

---
+++ api/src/Migrations/Version20210805141509.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210805141509 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE relay_point ADD external_id VARCHAR(255) DEFAULT NULL, ADD external_author VARCHAR(255) DEFAULT NULL, ADD external_updated_date DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE relay_point DROP external_id, DROP external_author, DROP external_updated_date');
+    }
+}
