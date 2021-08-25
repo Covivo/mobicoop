43d0899598f37e28e84ebcff308fb8b9a02e4bdf
Max <maxime.bardot@gmail.com>
2021-08-25 11:01:34 +0200

    Fix HasAtLeastNCarpooledCo2Saved Rule

--- api/src/Gamification/Rule/HasAtLeastNCarpooledCo2Saved.php
+++ api/src/Gamification/Rule/HasAtLeastNCarpooledCo2Saved.php
@@ -24,21 +24,12 @@
 namespace App\Gamification\Rule;
 
 use App\Gamification\Interfaces\GamificationRuleInterface;
-use App\User\Service\UserManager;
 
 /**
  *  Check that the requester is the author of the related Ad
  */
 class HasAtLeastNCarpooledCo2Saved implements GamificationRuleInterface
 {
-    private $userManager;
-
-    public function __construct(UserManager $userManager)
-    {
-        $this->userManager = $userManager;
-    }
-
-
     /**
      * Has at least N saved CO²
      *
@@ -50,7 +41,7 @@ class HasAtLeastNCarpooledCo2Saved implements GamificationRuleInterface
     public function execute($requester, $log, $sequenceItem)
     {
         // we check if the user has at least saved N CO²
-        $savedCo2 = $this->userManager->getProfileSummary($log->getUser())->getSavedCo2();
+        $savedCo2 = $log->getUser()->getSavedCo2();
         if ($savedCo2 >= $sequenceItem->getMinCount()) {
             return true;
         }
