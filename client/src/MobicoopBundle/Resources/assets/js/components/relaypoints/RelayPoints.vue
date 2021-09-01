b2b2b30dd2a2797a1230c4f988907f082debe575
Maxime Bardot <maxime.bardot@gmail.com>
2021-09-01 11:56:04 +0200

    #2944 client translations for relaypoints

--- client/src/MobicoopBundle/Resources/assets/js/components/relaypoints/RelayPoints.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/relaypoints/RelayPoints.vue
@@ -74,18 +74,25 @@
 
 <script>
 import maxios from "@utils/maxios";
+import { merge } from "lodash";
 import {messages_en, messages_fr, messages_eu, messages_nl} from "@translations/components/relayPoints/RelayPoints/";
+import {messages_client_en, messages_client_fr, messages_client_eu, messages_client_nl} from "@clientTranslations/components/relayPoints/RelayPoints/";
 import Search from "@components/carpool/search/Search";
 import MMap from "@components/utilities/MMap/MMap"
 import L from "leaflet";
 
+let MessagesMergedEn = merge(messages_en, messages_client_en);
+let MessagesMergedNl = merge(messages_nl, messages_client_nl);
+let MessagesMergedFr = merge(messages_fr, messages_client_fr);
+let MessagesMergedEu = merge(messages_eu, messages_client_eu);  
+
 export default {
   i18n: {
     messages: {
-      'en': messages_en,
-      'nl': messages_nl,
-      'fr': messages_fr,
-      'eu':messages_eu
+      'en': MessagesMergedEn,
+      'nl': MessagesMergedNl,
+      'fr': MessagesMergedFr,
+      'eu': MessagesMergedEu
     }
   },
   components: {
