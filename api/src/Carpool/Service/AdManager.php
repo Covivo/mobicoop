ce49511ec35146b60965a8a1721ecceb0a5397a8
Max <maxime.bardot@gmail.com>
2021-09-13 11:02:59 +0200

    Don't remove the self matching in claim Ad process

--- api/src/Carpool/Service/AdManager.php
+++ api/src/Carpool/Service/AdManager.php
@@ -966,17 +966,19 @@ class AdManager
         if ($proposal->getProposalLinked()) {
             $proposal->getProposalLinked()->setUser($this->security->getUser());
         }
+
+        // 1.36.0 : We let the self matching be
         // we remove the self matchings eventually
-        foreach ($proposal->getMatchingOffers() as $matchingOffer) {
-            if ($matchingOffer->getProposalOffer()->getUser()->getId() == $this->security->getUser()->getId()) {
-                $proposal->removeMatchingOffer($matchingOffer);
-            }
-        }
-        foreach ($proposal->getMatchingRequests() as $matchingRequest) {
-            if ($matchingRequest->getProposal()->getUser()->getId() == $this->security->getUser()->getId()) {
-                $proposal->removeMatchingRequest($matchingRequest);
-            }
-        }
+        // foreach ($proposal->getMatchingOffers() as $matchingOffer) {
+        //     if ($matchingOffer->getProposalOffer()->getUser()->getId() == $this->security->getUser()->getId()) {
+        //         $proposal->removeMatchingOffer($matchingOffer);
+        //     }
+        // }
+        // foreach ($proposal->getMatchingRequests() as $matchingRequest) {
+        //     if ($matchingRequest->getProposal()->getUser()->getId() == $this->security->getUser()->getId()) {
+        //         $proposal->removeMatchingRequest($matchingRequest);
+        //     }
+        // }
         $this->entityManager->persist($proposal);
         $this->entityManager->flush();
 
