9b3503db7bcb244d1cffe49e1e3aca612587d78d
b3ck54@gmail.com <b3ck54@gmail.com>
2021-10-04 15:15:02 +0200

    delete findAll in repo

--- api/src/Carpool/Repository/AskRepository.php
+++ api/src/Carpool/Repository/AskRepository.php
@@ -52,11 +52,6 @@ class AskRepository
         return $this->repository->findBy($criteria, $orderBy, $limit, $offset);
     }
     
-    public function findAll(): ?array
-    {
-        return $this->repository->findAll();
-    }
-    
     /**
      * Get the Asks of a user
      *
