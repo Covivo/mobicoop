3095147e7ead784cbaa464cb6109ebf7d3c03fd8
Worti <remi.wortemann@gmail.com>
2021-11-25 13:32:45 +0100

    resolve issue 3084

---
+++ api/src/Geography/ProviderFactory/GoogleMapsFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the BazingaGeocoderBundle package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace App\Geography\ProviderFactory;
+
+use Bazinga\GeocoderBundle\ProviderFactory\AbstractFactory;
+use App\Geography\ProviderFactory\GoogleMaps;
+use Geocoder\Provider\Provider;
+use Http\Discovery\HttpClientDiscovery;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class GoogleMapsFactory extends AbstractFactory
+{
+    protected static $dependencies = [
+        ['requiredClass' => GoogleMaps::class, 'packageName' => 'geocoder-php/google-maps-provider'],
+    ];
+
+    protected function getProvider(array $config): Provider
+    {
+        $httplug = $config['httplug_client'] ?: $this->httpClient ?? HttpClientDiscovery::find();
+
+        return new GoogleMaps($httplug, $config['region'], $config['api_key']);
+    }
+
+    protected static function configureOptionResolver(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'httplug_client' => null,
+            'region' => null,
+            'api_key' => null,
+        ]);
+
+        $resolver->setAllowedTypes('httplug_client', ['object', 'null']);
+        $resolver->setAllowedTypes('region', ['string', 'null']);
+        $resolver->setAllowedTypes('api_key', ['string', 'null']);
+    }
+}
