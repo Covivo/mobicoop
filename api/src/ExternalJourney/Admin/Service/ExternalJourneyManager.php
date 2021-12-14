c24f5b59dac3b47a78d7e4626c00fa1ed05155eb
Max <maxime.bardot@gmail.com>
2021-12-14 17:11:41 +0100

    WIP : Build a carpool result for admin from RDEX

--- api/src/ExternalJourney/Admin/Service/ExternalJourneyManager.php
+++ api/src/ExternalJourney/Admin/Service/ExternalJourneyManager.php
@@ -23,8 +23,9 @@
 
 namespace App\ExternalJourney\Admin\Service;
 
-use App\ExternalJourney\Service\ExternalJourneyManager as ServiceExternalJourneyManager;
+use App\ExternalJourney\Entity\ExternalJourneyProvider;
 use Symfony\Component\HttpFoundation\Request;
+use GuzzleHttp\Client;
 
 /**
  * External journey service in administration context
@@ -33,15 +34,201 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class ExternalJourneyManager
 {
-    private $externalJourneyManager;
+    private const EXTERNAL_JOURNEY_HASH = "sha256";         // hash algorithm
 
-    public function __construct(ServiceExternalJourneyManager $externalJourneyManager)
+    private $operator;
+    private $clients;
+    private $providers;
+
+    private $data;
+    private $currentProvider;
+
+    public function __construct(?array $operator = [], ?array $clients = [], ?array $providers = [])
     {
-        $this->externalJourneyManager = $externalJourneyManager;
+        $this->operator = $operator;
+        $this->clients = $clients;
+        foreach ($providers as $providerName=>$details) {
+            $provider = new ExternalJourneyProvider();
+            $provider->setName($providerName);
+            $provider->setUrl($details['url']);
+            $provider->setResource($details['resource']);
+            $provider->setApiKey($details['api_key']);
+            $provider->setPrivateKey($details['private_key']);
+            $this->providers[] = $provider;
+        }
+    }
+
+    public function getOperator()
+    {
+        return $this->operator;
+    }
+
+    public function getClients()
+    {
+        return $this->clients;
+    }
+
+    public function getProviders()
+    {
+        return $this->providers;
+    }
+
+    public function getData(): string
+    {
+        return $this->data;
+    }
+
+    public function setData(string $data)
+    {
+        $this->data = $data;
+    }
+
+    public function getCurrentProvider(): ExternalJourneyProvider
+    {
+        return $this->currentProvider;
+    }
+
+    public function setCurrentProvider(ExternalJourneyProvider $currentProvider)
+    {
+        $this->currentProvider = $currentProvider;
     }
 
     public function getExternalJourneys(Request $request, array $params): array
     {
-        return $this->externalJourneyManager->getExternalJourneys($request, $params);
+        $this->params = $params;
+
+        // initialize client API for any request
+        $client = new Client([
+            //10s because i'm working on long requests but you can change it
+            'timeout'  => 10.0,
+        ]);
+        // we collect search parameters here
+        $providerName = $request->get("provider");
+        $driver = $request->get("driver");
+        $passenger = $request->get("passenger");
+        $fromLatitude = $request->get("from_latitude");
+        $fromLongitude = $request->get("from_longitude");
+        $toLatitude = $request->get("to_latitude");
+        $toLongitude = $request->get("to_longitude");
+        $outwardMinDate = $request->get("outward_mindate");
+        $outwardMaxDate = $request->get("outward_maxdate");
+        $frequency = $request->get("frequency");
+        $rawJson = $request->get("rawJson");
+        $days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+
+        // then we set these parameters
+        $searchParameters  = [
+            'driver'  => [
+                'state'   => $driver
+            ],
+            'passenger' => [
+                'state'   => $passenger
+            ],
+            'from'    => [
+                'latitude'  => $fromLatitude,
+                'longitude' => $fromLongitude
+            ],
+            'to'    => [
+                'latitude'  => $toLatitude,
+                'longitude' => $toLongitude
+            ]
+        ];
+
+
+        if ($frequency!=="" && ($frequency=="regular" || $frequency=="punctual")) {
+            $searchParameters['frequency'] = $frequency;
+        }
+
+        if ($outwardMinDate!=="") {
+            $searchParameters['outward']['mindate'] = $outwardMinDate;
+        }
+        if ($outwardMaxDate!=="") {
+            $searchParameters['outward']['maxdate'] = $outwardMaxDate;
+        }
+
+
+        // Days treatment for regular journeys
+        // which days
+        foreach ($days as $day) {
+            $currentday = $request->get("days_".$day);
+            if ($currentday !== "") {
+                $searchParameters['days'][$day] = $currentday;
+            } else {
+                $searchParameters['days'][$day] = 0;
+            }
+        }
+
+        // mintime and maxtime for days
+        foreach ($days as $day) {
+            $mintime = $request->get($day."_mintime");
+            if ($mintime!=="") {
+                $searchParameters['outward'][$day]["mintime"] = $mintime;
+            }
+            $maxtime = $request->get($day."_maxtime");
+            if ($maxtime!=="") {
+                $searchParameters['outward'][$day]["maxtime"] = $maxtime;
+            }
+        }
+
+        $aggregatedResults = [];
+        $providers = $this->getProviders();
+        
+        // If a provider is given in parameters, we take only this one
+        // Otherwise, we use all providers
+        if ($providerName !== '') {
+            foreach ($providers as $provider) {
+                if ($provider->getName() == $providerName) {
+                    $providers = [$provider];
+                }
+            }
+        }
+        
+        // @todo error management (api not responding, bad parameters...)
+        foreach ($providers as $provider) {
+            $this->setCurrentProvider($provider);
+
+            $query = array(
+                'timestamp' => time(),
+                'apikey'    => $provider->getApiKey(),
+                'p'         => $searchParameters
+            );
+            // construct the requested url
+            $url = $provider->getUrl().'/'.$provider->getResource().'?'.http_build_query($query);
+            $signature = hash_hmac(self::EXTERNAL_JOURNEY_HASH, $url, $provider->getPrivateKey());
+            $signedUrl = $url.'&signature='.$signature;
+            // request url
+            $data = $client->request('GET', $signedUrl);
+            $this->setData($data->getBody()->getContents());
+
+            if ($this->data!=="") {
+                if ($rawJson==1) {
+                    // rawJson flag set. We return RDEX format.
+                    $aggregatedResults = json_decode($this->getData(), true);
+                } else {
+                    // No rawJson flag set or set to 0. We return an array
+                    foreach ($this->createCarpoolFromRDEX() as $currentResult) {
+                        $aggregatedResults[] = $currentResult;
+                    }
+                }
+            }
+        }
+        return $aggregatedResults;
+    }
+
+    private function createCarpoolFromRDEX(): array
+    {
+        $results = [];
+        $journeys = json_decode($this->getData(), true);
+        foreach ($journeys as $journey) {
+            $currentJourney = $journey['journeys'];
+            $carpool = [
+                "external" => true,
+                "externalProvider" => $this->getCurrentProvider()->getName()
+            ];
+
+            $results[] = $carpool;
+        }
+        
+        return $results;
     }
 }
