77fec65e1dc73b740b3420ec6769a5b8b2d70eee
Max <maxime.bardot@gmail.com>
2022-01-07 17:18:46 +0100

    authorization_code

--- api/src/DataProvider/Entity/OpenIdSsoProvider.php
+++ api/src/DataProvider/Entity/OpenIdSsoProvider.php
@@ -54,7 +54,7 @@ class OpenIdSsoProvider implements SsoProviderInterface
         ],
         self::SSO_PROVIDER_PASSMOBILITE => [
             self::AUTHORIZATION_URL => 'auth/realms/Passmobilite/protocol/openid-connect/auth/?client_id={CLIENT_ID}&scope=openid profile email&response_type=code&state={SERVICE_NAME}&redirect_uri={REDIRECT_URI}',
-            self::GRANT_TYPE => 'client_credentials',
+            self::GRANT_TYPE => 'authorization_code',
             self::TOKEN_URL => 'auth/realms/Partners/protocol/openid-connect/token/',
             self::USERINFOS_URL => 'auth/realms/Partners/protocol/openid-connect/userinfo',
             self::LOGOUT_URL => 'auth/realms/Partners/protocol/openid-connect/logout',
