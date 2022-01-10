00cccc56f20581d8a329f2410da083a623ccec89
Remi Wortemann <remi.wortemann@gmail.com>
2022-01-10 10:21:10 +0100

    optimization part 2

--- api/src/Gamification/Service/RetroactivelyRewardService.php
+++ api/src/Gamification/Service/RetroactivelyRewardService.php
@@ -221,6 +221,10 @@ class RetroactivelyRewardService
         );
         $stmt->execute();
         $resultsUsers = $stmt->fetchAll();
+        $this->logger->info('end fetchall | '.(new \DateTime('UTC'))->format('Ymd H:i:s.u'));
+
+        $stmt = $this->entityManager->getConnection()->prepare('DROP TABLE tuser;')->execute();
+        $this->logger->info('end drop table | '.(new \DateTime('UTC'))->format('Ymd H:i:s.u'));
 
         // format users
         $users = [];
@@ -359,6 +363,7 @@ class RetroactivelyRewardService
                 ];
             }
         }
+        $this->logger->info('start rewards | '.(new \DateTime('UTC'))->format('Ymd H:i:s.u'));
 
         $rewardSteps = [];
         $rewards = [];
@@ -380,7 +385,7 @@ class RetroactivelyRewardService
         foreach ($rewardSteps as $rewardStep) {
             $string .= $rewardStep.',';
             ++$i;
-            if ($i >= 5000) {
+            if ($i >= 500) {
                 $string = substr($string, 0, -1);
                 $this->entityManager->getConnection()->prepare('
                     INSERT INTO reward_step (sequence_item_id,created_date,user_id,notified_date)
@@ -403,7 +408,7 @@ class RetroactivelyRewardService
         foreach ($rewards as $reward) {
             $string .= $reward.',';
             ++$i;
-            if ($i >= 5000) {
+            if ($i >= 500) {
                 $string = substr($string, 0, -1);
                 $this->entityManager->getConnection()->prepare('
                     INSERT INTO reward (badge_id,user_id,created_date,notified_date)
