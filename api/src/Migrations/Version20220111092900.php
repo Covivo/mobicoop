ea6e48e60bdc79247a760d5ef68ffbf1ee286fed
b3ck54@gmail.com <b3ck54@gmail.com>
2022-01-12 09:01:48 +0100

    change number migration

--- api/src/Migrations/Version20220111092900.php
+++ api/src/Migrations/Version20220111092900.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220111132900 extends AbstractMigration
+final class Version20220111092900 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
