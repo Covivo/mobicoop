968f959d31333331764b557c8ad5c7f964a18137
Max <maxime.bardot@gmail.com>
2022-01-19 15:16:47 +0100

    Active extension for MCommunity + Territories from front sent to the api

--- api/src/Community/DataProvider/MCommunityCollectionDataProvider.php
+++ api/src/Community/DataProvider/MCommunityCollectionDataProvider.php
@@ -19,10 +19,12 @@
  ***************************
  *    Licence MOBICOOP described in the file
  *    LICENSE
- **************************/
+ */
 
 namespace App\Community\DataProvider;
 
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryResultCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGenerator;
 use ApiPlatform\Core\DataProvider\CollectionDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use App\Community\Resource\MCommunity;
@@ -34,28 +36,40 @@ use Symfony\Component\Security\Core\Security;
  * Collection data provider for MCommunity resource.
  *
  * @author Sylvain Briat <sylvain.briat@mobicoop.org>
- *
  */
 final class MCommunityCollectionDataProvider implements CollectionDataProviderInterface, RestrictedDataProviderInterface
 {
-    protected $request;
+    private $request;
     private $communityManager;
     private $security;
+    private $collectionFilters;
 
-    public function __construct(RequestStack $requestStack, CommunityManager $communityManager, Security $security)
+    public function __construct(RequestStack $requestStack, CommunityManager $communityManager, Security $security, iterable $collectionFilters)
     {
         $this->request = $requestStack->getCurrentRequest();
         $this->communityManager = $communityManager;
         $this->security = $security;
+        $this->collectionFilters = $collectionFilters;
     }
 
     public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
     {
-        return MCommunity::class === $resourceClass && $operationName === "get";
+        return MCommunity::class === $resourceClass && 'get' === $operationName;
     }
 
-    public function getCollection(string $resourceClass, string $operationName = null): ?array
+    public function getCollection(string $resourceClass, string $operationName = null, array $context = []): ?array
     {
-        return $this->communityManager->getMCommunities($this->security->getUser(), $this->request->get("userEmail"));
+        $queryBuilder = $this->communityManager->getMCommunitiesRequest($this->security->getUser(), $this->request->get('userEmail'));
+
+        // We're browsing every available filters
+        foreach ($this->collectionFilters as $collectionFilter) {
+            $collectionFilter->applyToCollection($queryBuilder, new QueryNameGenerator(), MCommunity::ENTITY_RELATED, $operationName, $context);
+
+            if ($collectionFilter instanceof QueryResultCollectionExtensionInterface && $collectionFilter->supportsResult(MCommunity::ENTITY_RELATED, $operationName)) {
+                $communities = $collectionFilter->getResult($queryBuilder, MCommunity::ENTITY_RELATED, $operationName);
+            }
+        }
+
+        return $this->communityManager->getMCommunities($communities);
     }
 }
