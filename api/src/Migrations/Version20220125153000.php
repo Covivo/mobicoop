15861e130df6fafac444687976bc313219359e28
Max <maxime.bardot@gmail.com>
2022-01-25 15:33:32 +0100

    rename action

--- api/src/Migrations/Version20220125153000.php
+++ api/src/Migrations/Version20220125153000.php
@@ -12,7 +12,7 @@ final class Version20220125153000 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql("INSERT INTO `action` (`id`, `name`, `position`) VALUES 
-            (110, 'identity_proof_reminder', 0);
+            (110, 'identity_proof_validation_reminder', 0);
         ");
         $this->addSql("INSERT INTO `notification` (`id`, `action_id`, `medium_id`, `template_title`, `active`, `template_body`, `created_date`, `updated_date`, `user_active_default`, `user_editable`, `position`) VALUES 
             (136, '110', '2', NULL, '1', NULL, NULL, NULL, '1', '0', '0');
