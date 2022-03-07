fb22a0af2adbc87b52684b0950fae1d122e6ccbb
Remi Wortemann <remi.wortemann@gmail.com>
2022-03-07 14:48:58 +0100

    fix

--- client/src/MobicoopBundle/Resources/assets/js/components/event/EventCreate.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/event/EventCreate.vue
@@ -531,7 +531,7 @@ export default {
               this.snackbar = true;
               this.loading = false;
             }
-            // else window.location.href = this.$t('redirect.route');
+            else window.location.href = this.$t('redirect.route');
           });
       } else {
         this.snackError = this.$t('error.event.required')
