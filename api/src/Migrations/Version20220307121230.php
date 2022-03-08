ad56824463a5289b4f4404590ad90861a7e08d5a
Remi Wortemann <remi.wortemann@gmail.com>
2022-03-08 09:50:33 +0100

    fix length limit

--- api/src/Migrations/Version20220307121230.php
+++ api/src/Migrations/Version20220307121230.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
@@ -10,19 +12,19 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20220307121230 extends AbstractMigration
 {
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE community CHANGE full_description full_description VARCHAR(2500) NOT NULL');
-        $this->addSql('ALTER TABLE event CHANGE full_description full_description VARCHAR(2500) DEFAULT NULL');
+        $this->addSql('ALTER TABLE community CHANGE full_description full_description VARCHAR(2550) NOT NULL');
+        $this->addSql('ALTER TABLE event CHANGE full_description full_description VARCHAR(2550) DEFAULT NULL');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE community CHANGE full_description full_description LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE event CHANGE full_description full_description LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
