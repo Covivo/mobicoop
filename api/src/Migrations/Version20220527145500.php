166a79b350ece7c5d45866a6243d647f217432bd
Olivier Fillol <olivier.fillol@mobicoop.org>
2022-05-31 10:10:17 +0200

    SQL query update

--- api/src/Migrations/Version20220527145500.php
+++ api/src/Migrations/Version20220527145500.php
@@ -17,15 +17,19 @@ final class Version20220527145500 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('INSERT INTO `auth_item`(`type`, `name`, `description`) VALUES (2, \'ROLE_TERRITORY_CONSULTANT\', \'Territory consultant\')');
+        $this->addSql("
+            INSERT INTO `auth_item`(`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES
+                (306, NULL, 2, 'ROLE_TERRITORY_CONSULTANT', 'Territory consultant')
+        ");
 
-        $this->addSql('INSERT INTO `auth_item_child`(`parent_id`, `child_id`)
-            SELECT (SELECT ai.id FROM `auth_item` ai WHERE ai.name = \'ROLE_TERRITORY_CONSULTANT\'), ai2.id
-            FROM `auth_item_child` aic
-            LEFT JOIN `auth_item` ai ON ai.id = aic.parent_id
-            LEFT JOIN `auth_item` ai2 ON ai2.id = aic.child_id
-            WHERE ai2.name like \'%read%\'
-            AND ai.id = 2');
+        $this->addSql('
+            INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES
+                (306, 100),
+                (306, 155),
+                (306, 162),
+                (306, 238),
+                (306, 301)
+        ');
     }
 
     public function down(Schema $schema): void
