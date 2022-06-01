2f37e1e8bc36c3e602fd577296720d3f57fd9e29
Worti <remi.wortemann@gmail.com>
2022-06-01 12:00:08 +0200

    evol 4534

---
+++ client/translations/components/carpool/Matching/index.js
@@ -0,0 +1,3 @@
+export {default as messages_client_fr} from './Matching_fr.json';
+export {default as messages_client_en} from './Matching_en.json';
+export {default as messages_client_eu} from './Matching_eu.json';
