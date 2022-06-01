215bb29bf9dfbc36a3f10110925b62f0cf9f63f0
Worti <remi.wortemann@gmail.com>
2022-06-01 10:58:57 +0200

    update deleteSubscriber

--- api/src/Image/EventSubscriber/DeleteSubscriber.php
+++ api/src/Image/EventSubscriber/DeleteSubscriber.php
@@ -19,32 +19,35 @@
  ***************************
  *    Licence MOBICOOP described in the file
  *    LICENSE
- **************************/
+ */
 
 namespace App\Image\EventSubscriber;
 
 use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Community\Entity\Community;
+use App\Editorial\Entity\Editorial;
 use App\Event\Entity\Event;
+use App\Gamification\Entity\Badge;
 use App\Image\Entity\Image;
+use App\Image\Service\ImageManager;
+use App\RelayPoint\Entity\RelayPoint;
+use App\RelayPoint\Entity\RelayPointType;
+use App\Solidary\Entity\Structure;
+use App\User\Entity\User;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
-use App\Image\Service\ImageManager;
-use App\User\Entity\User;
-use App\RelayPoint\Entity\RelayPoint;
-use App\RelayPoint\Entity\RelayPointType;
-use App\Community\Entity\Community;
 
 final class DeleteSubscriber implements EventSubscriberInterface
 {
     private $imageManager;
-    
+
     public function __construct(ImageManager $imageManager)
     {
         $this->imageManager = $imageManager;
     }
-    
+
     public static function getSubscribedEvents()
     {
         return [
@@ -58,18 +61,17 @@ final class DeleteSubscriber implements EventSubscriberInterface
         $method = $event->getRequest()->getMethod();
 
         // needs to be modified for each new related entity (event, user etc...)
-        if ((!($object instanceof Image) && !($object instanceof Event) && !($object instanceof Community) && !($object instanceof User) && !($object instanceof RelayPoint) && !($object instanceof RelayPointType)) || Request::METHOD_DELETE !== $method) {
+        if ((!($object instanceof Image) && !($object instanceof Event) && !($object instanceof Community) && !($object instanceof User) && !($object instanceof RelayPoint) && !($object instanceof RelayPointType) && !($object instanceof Editorial) && !($object instanceof Badge) && !($object instanceof Structure)) || Request::METHOD_DELETE !== $method) {
             return;
         }
         if ($object instanceof Image) {
             // deletion of Image => we delete the versions
             $this->imageManager->deleteVersions($object);
-        } elseif ($object instanceof Event || $object instanceof Community || $object instanceof User || $object instanceof RelayPoint || $object instanceof RelayPointType) {
+        } elseif ($object instanceof Event || $object instanceof Community || $object instanceof User || $object instanceof RelayPoint || $object instanceof RelayPointType || $object instanceof Editorial || $object instanceof Badge || $object instanceof Structure) {
             // deletion of Event => we delete the versions of all related images
             foreach ($object->getImages() as $image) {
                 $this->imageManager->deleteVersions($image);
             }
         }
-        return;
     }
 }
