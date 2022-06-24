c9c232928d06eff3a95146a1581ea5fe60775192
Gsk54 <sylvain.briat@free.fr>
2022-06-24 15:43:38 +0200

    refactor params

--- api/docker/api/init-api.sh
+++ api/docker/api/init-api.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-cp -u /srv/api/config/params/analytics.json.dist /srv/api/config/params/analytics.json
-cp -u /srv/api/config/params/contacts.json.dist /srv/api/config/params/contacts.json
-cp -u /srv/api/config/params/modules.json.dist /srv/api/config/params/modules.json
-cp -u /srv/api/config/publicTransport/providers.json.dist /srv/api/config/publicTransport/providers.json
-cp -u /srv/api/config/rdex/clients.json.dist /srv/api/config/rdex/clients.json
-cp -u /srv/api/config/rdex/operator.json.dist /srv/api/config/rdex/operator.json
-cp -u /srv/api/config/rdex/providers.json.dist /srv/api/config/rdex/providers.json
-cp -u /srv/api/config/user/domains.json.dist /srv/api/config/user/domains.json
-cp -u /srv/api/config/user/sso.json.dist /srv/api/config/user/sso.json
+cp -u /srv/api/config/params/analytics/analytics.json.dist /srv/api/config/params/analytics/analytics.json
+cp -u /srv/api/config/params/contacts/contacts.json.dist /srv/api/config/params/contacts/contacts.json
+cp -u /srv/api/config/params/modules/modules.json.dist /srv/api/config/params/modules/modules.json
+cp -u /srv/api/config/params/transit/providers.json.dist /srv/api/config/params/transit/providers.json
+cp -u /srv/api/config/params/rdex/clients.json.dist /srv/api/config/params/rdex/clients.json
+cp -u /srv/api/config/params/rdex/operator.json.dist /srv/api/config/params/rdex/operator.json
+cp -u /srv/api/config/params/rdex/providers.json.dist /srv/api/config/params/rdex/providers.json
+cp -u /srv/api/config/params/user/domains.json.dist /srv/api/config/params/user/domains.json
+cp -u /srv/api/config/params/user/sso.json.dist /srv/api/config/params/user/sso.json
 php /srv/api/bin/console doctrine:database:create --if-not-exists -n
 php /srv/api/bin/console doctrine:migrations:migrate -n
