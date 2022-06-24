4bf02cba2ae45b4e4f0cb7e4276288247342b11e
Gsk54 <sylvain.briat@free.fr>
2022-06-24 16:52:41 +0200

    try dockerize all

--- api/docker/api/docker-entrypoint.sh
+++ api/docker/api/docker-entrypoint.sh
@@ -6,6 +6,17 @@ if [ "${1#-}" != "$1" ]; then
 	set -- php-fpm "$@"
 fi
 
+# copy required param files if needed
+cp -u /srv/api/config/params/analytics/analytics.json.dist /srv/api/config/params/analytics/analytics.json
+cp -u /srv/api/config/params/contacts/contacts.json.dist /srv/api/config/params/contacts/contacts.json
+cp -u /srv/api/config/params/modules/modules.json.dist /srv/api/config/params/modules/modules.json
+cp -u /srv/api/config/params/transit/providers.json.dist /srv/api/config/params/transit/providers.json
+cp -u /srv/api/config/params/rdex/clients.json.dist /srv/api/config/params/rdex/clients.json
+cp -u /srv/api/config/params/rdex/operator.json.dist /srv/api/config/params/rdex/operator.json
+cp -u /srv/api/config/params/rdex/providers.json.dist /srv/api/config/params/rdex/providers.json
+cp -u /srv/api/config/params/user/domains.json.dist /srv/api/config/params/user/domains.json
+cp -u /srv/api/config/params/user/sso.json.dist /srv/api/config/params/user/sso.json
+
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
 	if [ "$APP_ENV" != 'prod' ]; then
--- api/docker/api/init-api.sh
+++ api/docker/api/init-api.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
-cp -u /srv/api/config/params/analytics/analytics.json.dist /srv/api/config/params/analytics/analytics.json
-cp -u /srv/api/config/params/contacts/contacts.json.dist /srv/api/config/params/contacts/contacts.json
-cp -u /srv/api/config/params/modules/modules.json.dist /srv/api/config/params/modules/modules.json
-cp -u /srv/api/config/params/transit/providers.json.dist /srv/api/config/params/transit/providers.json
-cp -u /srv/api/config/params/rdex/clients.json.dist /srv/api/config/params/rdex/clients.json
-cp -u /srv/api/config/params/rdex/operator.json.dist /srv/api/config/params/rdex/operator.json
-cp -u /srv/api/config/params/rdex/providers.json.dist /srv/api/config/params/rdex/providers.json
-cp -u /srv/api/config/params/user/domains.json.dist /srv/api/config/params/user/domains.json
-cp -u /srv/api/config/params/user/sso.json.dist /srv/api/config/params/user/sso.json
 php /srv/api/bin/console doctrine:database:create --if-not-exists -n
 php /srv/api/bin/console doctrine:migrations:migrate -n
