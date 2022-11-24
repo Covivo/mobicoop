9f681f3be6bdea2cb1b929283650b43b2f070f5e
Worti <remi.wortemann@gmail.com>
2022-11-24 15:33:17 +0100

    evol #5203 part 2

--- api/src/ExternalJourney/Service/JourneyProvider.php
+++ api/src/ExternalJourney/Service/JourneyProvider.php
@@ -1,8 +1,31 @@
 <?php
 
+/**
+ * Copyright (c) 2022, MOBICOOP. All rights reserved.
+ * This project is dual licensed under AGPL and proprietary licence.
+ ***************************
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <gnu.org/licenses>.
+ ***************************
+ *    Licence MOBICOOP described in the file
+ *    LICENSE
+ */
+
 declare(strict_types=1);
 
 namespace App\ExternalJourney\Service;
+
+use Symfony\Component\HttpFoundation\Request;
 
 abstract class JourneyProvider
 {
@@ -128,5 +151,5 @@ abstract class JourneyProvider
         return $this;
     }
 
-    abstract public function getJourneys(array $params): array;
+    abstract public function getJourneys(JourneyProvider $provider, Request $request): array;
 }
--- api/src/ExternalJourney/Service/JourneySearcher.php
+++ api/src/ExternalJourney/Service/JourneySearcher.php
@@ -1,12 +1,36 @@
 <?php
 
+/**
+ * Copyright (c) 2022, MOBICOOP. All rights reserved.
+ * This project is dual licensed under AGPL and proprietary licence.
+ ***************************
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <gnu.org/licenses>.
+ ***************************
+ *    Licence MOBICOOP described in the file
+ *    LICENSE
+ */
+
 declare(strict_types=1);
 
 namespace App\ExternalJourney\Service;
 
+use Symfony\Component\HttpFoundation\Request;
+
 class JourneySearcher
 {
-    private $_providers;
+    private $providers;
+    private $_providers = [];
 
     public function __construct(?array $providers = [])
     {
@@ -23,14 +47,13 @@ class JourneySearcher
         }
     }
 
-    public function search(): array
+    public function search(Request $request): array
     {
+        // todo remove providers if only one selected...  $providerName = $request->get('provider');
         $journeys = [];
         foreach ($this->_providers as $provider) {
-            /**
-             * @var JourneyProvider $provider
-             */
-            $journeys = array_merge($journeys, $provider->getJourneys([]));
+            // @var JourneyProvider $provider
+            $journeys[] = $provider->getJourneys($provider, $request);
         }
 
         return $journeys;
