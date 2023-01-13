705e7d89c64eff300f0a786dfe5755fa261958bf
Olivier Fillol <olivier.fillol@mobicoop.org>
2023-01-13 19:18:49 +0100

    #5303: Convert & store distance in dec format

---
+++ api/src/Migrations/Version20230113174742.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230113181809 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE mobconnect__long_distance_journey CHANGE distance distance NUMERIC(5, 1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE mobconnect__short_distance_journey CHANGE distance distance NUMERIC(5, 1) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE mobconnect__long_distance_journey CHANGE distance distance INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE mobconnect__short_distance_journey CHANGE distance distance INT DEFAULT NULL');
+    }
+}
