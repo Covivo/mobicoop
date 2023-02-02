c283fd77156976c2edfb1df4eb71c6a221288c42
Olivier Fillol <olivier.fillol@mobicoop.org>
2023-02-02 10:07:17 +0100

    Improved Logs

--- api/src/Incentive/Controller/TestController.php
+++ api/src/Incentive/Controller/TestController.php
@@ -4,13 +4,16 @@ namespace App\Incentive\Controller;
 
 use App\Carpool\Entity\CarpoolProof;
 use App\Incentive\Service\MobConnectSubscriptionManager;
+use App\User\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/incentives")
+ * @Route("/incentives/test")
  */
 class TestController extends AbstractController
 {
@@ -31,7 +34,7 @@ class TestController extends AbstractController
     }
 
     /**
-     * @Route("/test")
+     * @Route("/")
      */
     public function test()
     {
@@ -40,5 +43,58 @@ class TestController extends AbstractController
         $this->_mobConnectSubscriptionManager->updateSubscription($carpoolProof);
 
         return new Response('Ok');
+    }
+
+    /**
+     * TODO:
+     * Writes the eligibility test logs of carpool proofs passed in parameters.
+     *
+     * @Route("/carpools/check")
+     */
+    public function checkCarpools(Request $request)
+    {
+        $carpoolProofIds = explode(',', $request->get('carpoolProofs'));
+
+        $carpoolProofs = [];
+
+        foreach ($carpoolProofIds as $key => $id) {
+            $carpoolProof = $this->_em->getRepository(CarpoolProof::class)->find(intval($id));
+
+            if (!is_null($carpoolProof)) {
+                array_push($carpoolProofs, $carpoolProof);
+            }
+        }
+
+        return new JsonResponse($carpoolProofs);
+    }
+
+    /**
+     * Returns users EEC subscriptions.
+     *
+     * @Route("/users/check")
+     */
+    public function checkSubscriptions(Request $request)
+    {
+        $ids = explode(',', $request->get('users'));
+
+        $returnedSubscriptions = [];
+
+        foreach ($ids as $key => $id) {
+            $user = $this->_em->getRepository(User::class)->find(intval($id));
+
+            $userSubscriptions = $this->_mobConnectSubscriptionManager->getUserSubscriptions($user);
+
+            $subscription = new \stdClass();
+            $subscription->userId = $user->getId();
+            $subscription->shortDistanceSubscription = $userSubscriptions[0]->getShortDistanceSubscriptions();
+            $subscription->longDistanceSubscription = $userSubscriptions[0]->getLongDistanceSubscriptions();
+            $subscription->nbPendingProofs = $userSubscriptions[0]->getNbPendingProofs();
+            $subscription->nbValidatedProofs = $userSubscriptions[0]->getNbValidatedProofs();
+            $subscription->nbRejectedProofs = $userSubscriptions[0]->getNbRejectedProofs();
+
+            array_push($returnedSubscriptions, $subscription);
+        }
+
+        return new JsonResponse($returnedSubscriptions);
     }
 }
--- api/src/Incentive/Entity/Log.php
+++ api/src/Incentive/Entity/Log.php
@@ -10,8 +10,11 @@ class Log
     public const CARPOOL_PROOF_ID = 'carpoolProofId';                   // CarpoolProof entity ID
     public const DATETIME = 'datetime';                                 // The log timestamp
     public const IS_CARPOOL_PROOFS_VALID = 'isCarpoolProofsValid';      // The journeys of the user, already made, correspond to the standard defined within the framework of the CEE
+    public const IS_DATE_AFTER_REFERENCE_DATE = 'isDateAfterReference'; // Is the date after the référene date
+    public const IS_DATE_IN_PERIOD = 'isDateInPeriod';                  // Is the date in the périod
     public const IS_FROM_FRANCE = 'isFromFrance';                       // Does the journey have an origin and/or a destination from France
     public const IS_LONG_DISTANCE = 'isLongDistance';                   // Is the trip long distance
+    public const IS_SHORT_DISTANCE = 'isShortDistance';                 // Is the trip short distance
     public const IS_PAYMENT_REGULARIZED = 'isPaymentRegularized';       // Has the payment been regularized
     public const IS_USER_VALID = 'isUserValid';                         // Is the user data valid, according to the standard defined within the framework of the CEE
     public const MATCHING_ID = 'matchingId';                            // The Matching entity ID
@@ -23,8 +26,11 @@ class Log
         self::CARPOOL_PROOF_ID,
         self::DATETIME,
         self::IS_CARPOOL_PROOFS_VALID,
+        self::IS_DATE_AFTER_REFERENCE_DATE,
+        self::IS_DATE_IN_PERIOD,
         self::IS_FROM_FRANCE,
         self::IS_LONG_DISTANCE,
+        self::IS_SHORT_DISTANCE,
         self::IS_PAYMENT_REGULARIZED,
         self::IS_USER_VALID,
         self::MATCHING_ID,
