3e6ccdcb7749f9758453080a205bfef98cfa790c
Olivier Fillol <olivier.fillol@mobicoop.org>
2023-02-21 13:57:36 +0100

    #5756: Remove verify reason

--- api/src/Incentive/Service/MobConnectSubscriptionManager.php
+++ api/src/Incentive/Service/MobConnectSubscriptionManager.php
@@ -202,8 +202,6 @@ class MobConnectSubscriptionManager
         $response = $this->_mobConnectApiProvider->verifyUserSubscription($this->__getSubscriptionId());
 
         $this->_userSubscription->setStatus($response->getStatus());
-        $this->_userSubscription->setRejectionReason($response->getRejectReason());
-        $this->_userSubscription->setComment($response->getComment());
         $this->_userSubscription->setVerificationDate();
 
         $this->_userSubscription->setLastTimestamp($response->getTimestamp());
