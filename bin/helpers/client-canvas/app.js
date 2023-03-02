1a494c26f043da0b19bcce61519bfe9c34cd0262
Maxime Bardot <maxime.bardot@gmail.com>
2023-03-02 10:10:47 +0100

    update canvas app.js

--- bin/helpers/client-canvas/app.js
+++ bin/helpers/client-canvas/app.js
@@ -1,5 +1,29 @@
-'use strict';
+'use strict'
+// No overcharge, only import app.js from MobicoopBundle
+// import '@js/app.js';
 
-// any CSS you require will output into a single css file (app.css in this case)
-import '../../src/MobicoopBundle/Resources/assets/js/app.js';
-import '../css/app.scss';
+// Else
+// Import base css
+import '@css/main.scss';
+// Import custom css
+import '@clientCss/main.scss';
+
+// If you want to overcharge, overcharge the app.js file that loads vue js, you need to reimport everything
+// You can overcharge vuetify, i18n etc, juste create the files on the instance and import them in place of config/app.js ones from bundle
+import 'babel-polyfill';
+import { Vue, vuetify, i18n } from '@js/config/app'
+
+import {merge} from 'lodash'
+import bundleComponents from '@js/config/components'
+import clientComponents from '@clientJs/config/components'
+import { store } from '@js/store'
+
+let components = merge(bundleComponents, clientComponents);
+
+new Vue({
+  el: '#app',
+  vuetify,
+  i18n,
+  store,
+  components: components
+})
