f927827c7d202b9c89175efa30bf64ce2c60921f
Olivier Fillol <olivier.fillol@mobicoop.org>
2023-03-22 11:41:22 +0100

    #5631: Adds regular proposals as carpoolers

--- api/src/Carpool/Service/MyAdManager.php
+++ api/src/Carpool/Service/MyAdManager.php
@@ -221,7 +221,7 @@ class MyAdManager
              */
             // we exclude private proposals and expired matchings for the carpooler count
             // We need them though to treat former ask without sending another request
-            if (!$matchingOffer->getProposalOffer()->isPrivate() && $matchingOffer->getCriteria()->getFromDate()->format('Y-m-d') >= $today->format('Y-m-d')) {
+            if (!$matchingOffer->getProposalOffer()->isPrivate() && !$this->_hasJourneyReachedDeadline($matchingOffer->getCriteria())) {
                 $carpoolers[] = $matchingOffer->getProposalOffer()->getUser()->getId();
             }
             // check for asks (driver)
@@ -284,7 +284,7 @@ class MyAdManager
                 !$matchingRequest->getProposalRequest()->isPrivate()
                 && !$matchingRequest->getProposalRequest()->getSolidary()
                 && !in_array($matchingRequest->getProposalRequest()->getUser()->getId(), $carpoolers)
-                && $matchingRequest->getCriteria()->getFromDate()->format('Y-m-d') >= $today->format('Y-m-d')
+                && !$this->_hasJourneyReachedDeadline($matchingRequest->getCriteria())
             ) {
                 $carpoolers[] = $matchingRequest->getProposalRequest()->getUser()->getId();
             }
@@ -342,6 +342,15 @@ class MyAdManager
         $myAd->setPassengers($passengers);
 
         return $myAd;
+    }
+
+    private function _hasJourneyReachedDeadline(Criteria $criteria): bool
+    {
+        $today = new \DateTime('now');
+
+        $date = !is_null($criteria->getToDate()) ? $criteria->getToDate() : $criteria->getFromDate();
+
+        return is_null($date) ? false : $date->format('Y-m-d') >= $today->format('Y-m-d');
     }
 
     /**
