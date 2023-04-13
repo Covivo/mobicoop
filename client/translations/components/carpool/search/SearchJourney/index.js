7a7148aabd82dab18f464298da039ade8fd4f817
Worti <remi.wortemann@gmail.com>
2023-04-13 14:51:17 +0200

    fix on evol #2011

--- client/translations/components/carpool/search/SearchJourney/index.js
+++
@@ -1,4 +0,0 @@
-export {default as messages_client_fr} from './SearchJourney_fr.json';
-export {default as messages_client_en} from './SearchJourney_en.json';
-export {default as messages_client_eu} from './SearchJourney_eu.json';
-export {default as messages_client_nl} from './SearchJourney_nl.json';
