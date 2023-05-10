b6c0830908033842a1dd17911d1ffd3e843ee74f
Worti <remi.wortemann@gmail.com>
2023-05-10 13:37:32 +0200

    resolve issue #6043

--- api/src/User/Entity/User.php
+++ api/src/User/Entity/User.php
@@ -858,7 +858,7 @@ class User implements UserInterface, EquatableInterface
      *
      * @ORM\Column(type="string", length=255, nullable=true)
      *
-     * @Groups({"aRead","aWrite","readUser","write","checkPhoneToken","results", "readSolidary"})
+     * @Groups({"aRead","aWrite","readUser","write","checkPhoneToken", "readSolidary"})
      */
     private $telephone;
 
