b3b8a1566ad403412405afe5ba2b00bf1a408370
Max <maxime.bardot@gmail.com>
2023-05-24 11:00:32 +0200

    #6117 use .env to select the list of gender

--- client/src/MobicoopBundle/Resources/assets/js/components/user/Signupform.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/user/Signupform.vue
@@ -381,7 +381,7 @@
                     </v-chip>
                   </template>
                   <template v-slot:item="data">
-                    <template v-if="typeof data.item !== 'object'">
+                    <template v-if="(typeof data.item) !== 'object'">
                       <v-list-item-content v-text="data.item" />
                     </template>
                     <template v-else>
@@ -632,6 +632,10 @@ export default {
     specificSsoServices: {
       type: Array,
       default: () => []
+    },
+    gendersList: {
+      type: Array,
+      default: () => []
     }
   },
   data() {
@@ -817,7 +821,10 @@ export default {
       return social;
     },
     genderItems(){
-      return this.$t("gender.values");
+      return this.$t("gender.values").filter((genderItem) => {
+      // Spécifiez ici les critères de filtrage
+        return this.gendersList.includes(parseInt(genderItem.value)); // Exemple : Filtrer les valeurs supérieures à 3
+      });
     },
   },
   watch: {
