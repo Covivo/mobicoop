70ff468a8c58bf1cc1764bf066d4769918450245
Maxime Bardot <maxime.bardot@gmail.com>
2023-06-12 15:21:26 +0200

    #6251 Sentry for front client

--- client/src/MobicoopBundle/Resources/assets/js/app.js
+++ client/src/MobicoopBundle/Resources/assets/js/app.js
@@ -5,9 +5,38 @@ import { Vue, vuetify, i18n } from '@js/config/app'
 import { store } from './store';
 
 import components from '@js/config/components'
+import * as Sentry from "@sentry/vue";
 
 // Import css
 import '@css/main.scss'
+
+let env = 'production';
+if (window.location.href.indexOf(".test.") >= 0){
+  env = 'test';
+}
+if (window.location.href.indexOf("localhost") >= 0){
+  env = 'dev';
+}
+
+
+
+
+Sentry.init({
+  Vue,
+  dsn: "https://757990e3434043f8895ffa6f320a838f@sentry.mobicoop.io/3",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost:8081"],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  environment: env,
+});
+
 
 new Vue({
   el: '#app',
