57f714bf9d8f2fc3367b3a34509f8f9aae1cba1c
Max <maxime.bardot@gmail.com>
2023-06-21 15:05:09 +0200

    #6235 component + route

---
+++ client/src/MobicoopBundle/Resources/assets/js/components/payment/Donation/DonorBox.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <iframe
+        :src="$t('src')"
+        name="donorbox"
+        allowpaymentrequest="allowpaymentrequest"
+        seamless="seamless"
+        frameborder="0"
+        scrolling="no"
+        height="900px"
+        width="100%"
+        style="max-width: 500px; min-width: 250px; max-height:none!important"
+      />
+    </v-col>
+  </v-row>
+</template>
+<script>
+
+import {messages_en, messages_fr, messages_eu, messages_nl} from "@translations/components/payment/Donation/DonorBox/";
+
+export default {
+  i18n: {
+    messages: {
+      'en': messages_en,
+      'nl': messages_nl,
+      'fr': messages_fr,
+      'eu':messages_eu
+    }
+  },
+  components: {
+  },
+  props: {
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
--- client/src/MobicoopBundle/Resources/assets/js/config/components.js
+++ client/src/MobicoopBundle/Resources/assets/js/config/components.js
@@ -83,6 +83,7 @@ import MSnackInfos from "@components/utilities/MSnackInfos";
 // PAYMENT
 import Payment from "@components/payment/Payment"
 import PaymentPaid from "@components/user/profile/payment/PaymentPaid"
+import DonorBox from "@components/payment/Donation/DonorBox"
 
 // AssistiveDevices
 import Assistive from "@components/assistiveDevices/Assistive"
@@ -133,6 +134,7 @@ export default {
   DayListChips,
   Payment,
   PaymentPaid,
+  DonorBox,
   SsoLoginReturn,
   ErrorPage,
   MSnackInfos,
