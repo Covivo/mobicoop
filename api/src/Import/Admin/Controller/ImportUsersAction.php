80927b50d6e41c07fc57d71492ec98ec4c85232d
Maxime Bardot <maxime.bardot@gmail.com>
2023-06-22 14:46:06 +0200

    #4833 ImportUsers resource and controller for vich uploader

---
+++ api/src/Import/Admin/Controller/ImportUsersAction.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Copyright (c) 2022, MOBICOOP. All rights reserved.
+ * This project is dual licensed under AGPL and proprietary licence.
+ ***************************
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <gnu.org/licenses>.
+ ***************************
+ *    Licence MOBICOOP described in the file
+ *    LICENSE
+ */
+
+namespace App\Import\Admin\Controller;
+
+use App\Import\Admin\Resource\ImportUsers;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ *  @author Maxime Bardot <maxime.bardot@mobicoop.org>
+ */
+final class ImportUsersAction
+{
+    public function __construct()
+    {
+    }
+
+    public function __invoke(Request $request): ?ImportUsers
+    {
+        if (!$request->files->get('file')) {
+            throw new \Exception('File is mandatory');
+        }
+
+        //        return $this->communitySecurityManager->createSecurity($request->files->get('file'), (int) $request->get('communityId'));
+        return null;
+    }
+}
---
+++ api/src/Import/Admin/Resource/ImportUsers.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * Copyright (c) 2023, MOBICOOP. All rights reserved.
+ * This project is dual licensed under AGPL and proprietary licence.
+ ***************************
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <gnu.org/licenses>.
+ ***************************
+ *    Licence MOBICOOP described in the file
+ *    LICENSE
+ */
+
+namespace App\Import\Admin\Resource;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Import\Admin\Controller\ImportUsersAction;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ORM\Entity
+ *
+ * @ApiResource(
+ *      attributes={
+ *          "force_eager"=false,
+ *          "normalization_context"={"groups"={"read"}, "enable_max_depth"="true"},
+ *          "denormalization_context"={"groups"={"write"}}
+ *      },
+ *      collectionOperations={
+ *          "get"={
+ *              "security"="is_granted('reject',object)",
+ *              "swagger_context" = {
+ *                  "summary"="Not implemented",
+ *                  "tags"={"Administration"}
+ *              }
+ *          },
+ *          "ADMIN_post_import"={
+ *              "path"="/admin/import_users",
+ *              "method"="POST",
+ *              "controller"=ImportUsersAction::class,
+ *              "denormalization_context"={"groups"={"aWrite"}},
+ *              "deserialize"=false,
+ *              "swagger_context" = {
+ *                  "tags"={"Administration"}
+ *              }
+ *          },
+ *      },
+ *      itemOperations={
+ *          "get"={
+ *              "security"="is_granted('reject',object)",
+ *              "swagger_context" = {
+ *                  "summary"="Not implemented",
+ *                  "tags"={"Administration"}
+ *              }
+ *          },
+ *      }
+ * )
+ *
+ * @Vich\Uploadable
+ */
+class ImportUsers
+{
+    /**
+     * @var int the id of this community security
+     *
+     * @Groups({"aRead","readCommunity"})
+     */
+    private $id;
+
+    /**
+     * @var null|File The document's file
+     *
+     * @Vich\UploadableField(mapping="communitySecurityFile", fileNameProperty="filename", originalName="originalName")
+     *
+     * @Groups({"aWrite"})
+     */
+    private $file;
+
+    /**
+     * @var string the filename of the community security
+     *
+     * @Groups({"read","aRead","write"})
+     */
+    private $filename;
+
+    /**
+     * @var string the original file name of the proof
+     *
+     * @Groups({"read","aRead"})
+     */
+    private $originalName;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFile(): ?File
+    {
+        return $this->file;
+    }
+
+    public function setFile(?File $file)
+    {
+        $this->file = $file;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(?string $filename)
+    {
+        $this->filename = $filename;
+    }
+
+    public function getOriginalName(): ?string
+    {
+        return $this->originalName;
+    }
+
+    public function setOriginalName(?string $originalName)
+    {
+        $this->originalName = $originalName;
+    }
+}
