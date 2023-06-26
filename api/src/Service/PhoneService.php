6d9b50902916e7603aa6567fdd0dfe62e633cfa7
Olivier Fillol <olivier.fillol@mobicoop.org>
2023-06-26 11:21:56 +0200

    #6368: Add phone prefix

--- api/src/Service/PhoneService.php
+++ api/src/Service/PhoneService.php
@@ -230,10 +230,10 @@ class PhoneService
      */
     private $_internationalPhoneNumber;
 
-    public function __construct(?string $phoneNumber, string $countryCode)
+    public function __construct(string $countryCode, ?string $phoneNumber)
     {
-        $this->_setOriginalPhoneNumber($phoneNumber);
         $this->_setCountryCode($countryCode);
+        $this->_setOriginalPhoneNumber($phoneNumber);
     }
 
     /**
@@ -294,7 +294,7 @@ class PhoneService
         return $this;
     }
 
-    private function _isCountryCodeSupported()
+    private function _isCountryCodeSupported(): bool
     {
         return array_key_exists($this->_getCountryCode(), self::COUNTRY_PHONE_PREFIX);
     }
@@ -325,8 +325,6 @@ class PhoneService
 
     /**
      * Set the value of _countryCode.
-     *
-     * @param string $_countryCode
      */
     private function _setCountryCode(string $countryCode): self
     {
@@ -345,8 +343,6 @@ class PhoneService
 
     /**
      * Set the value of _normalizedPhoneNumber.
-     *
-     * @param string $_normalizedPhoneNumber
      */
     private function _setNormalizedPhoneNumber(): self
     {
@@ -357,9 +353,14 @@ class PhoneService
 
     private function _setOriginalPhoneNumber(?string $phoneNumber): self
     {
-        $this->_originalPhoneNumber = $phoneNumber;
+        $phoneNumber = str_replace(' ', '', $this->_originalPhoneNumber);
 
-        $this->_setNormalizedPhoneNumber();
+        // We add the phone prefix char before the phone number if it has not been set
+        if (preg_match('/^'.self::COUNTRY_PHONE_PREFIX[$this->_getCountryCode()].'/', $phoneNumber)) {
+            $phoneNumber = self::PHONE_PREFIX.$phoneNumber;
+        }
+
+        $this->_normalizedPhoneNumber = $phoneNumber;
 
         return $this;
     }
