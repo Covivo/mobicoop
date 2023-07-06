399c99a4531fdea23753996868f68367027936b6
Bertrand Mathelier <bertrand.mathelier@mobicoop.org>
2023-07-06 16:26:33 +0200

    issue6424 allow community managers to display analytic dashbords

---
+++ api/src/Migrations/Version20230706162010.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20230706162010 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('
+        INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES
+        (8, 302)
+        ');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+    }
+}
