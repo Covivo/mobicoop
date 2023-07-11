016c7ec9e29f5abf1b99331fd55e09d0bf76f1c3
Remi Wortemann <remi.wortemann@gmail.com>
2023-07-11 15:54:29 +0200

    evol #5268 part4

---
+++ client/src/MobicoopBundle/Resources/translations/components/user/mailbox/ThreadActionsCarpoolStandard/index.js
@@ -0,0 +1,4 @@
+export {default as messages_fr} from './ThreadActionsCarpoolStandard_fr.json';
+export {default as messages_en} from './ThreadActionsCarpoolStandard_en.json';
+export {default as messages_eu} from './ThreadActionsCarpoolStandard_eu.json';
+export {default as messages_nl} from './ThreadActionsCarpoolStandard_nl.json';
