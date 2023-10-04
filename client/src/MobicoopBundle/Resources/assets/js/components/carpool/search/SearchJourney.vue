da90774ecb0c8798a3b881a3f368c3ddec131b53
Olivier Fillol <olivier.fillol@mobicoop.org>
2023-10-04 09:58:13 +0200

    #6337: Date format

--- client/src/MobicoopBundle/Resources/assets/js/components/carpool/search/SearchJourney.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/carpool/search/SearchJourney.vue
@@ -241,7 +241,6 @@ import moment from "moment";
 import Geocomplete from "@components/utilities/geography/Geocomplete";
 import {messages_en, messages_fr, messages_eu, messages_nl} from "@translations/components/carpool/search/SearchJourney/";
 
-
 export default {
   i18n: {
     messages: {
@@ -369,22 +368,11 @@ export default {
   },
   computed: {
     computedDateFormat() {
-      console.log(`this.datetime (l-372): ${this.dateTime}`);
-
-      let datetime = null;
-
       if (this.dateTimePicker) {
-        console.log(`this.locale (l-377): ${this.locale}`);
-        datetime = this.dateTime ? moment(this.dateTime, this.$t("fullDateTime")) : null;
+        return this.dateTime ? moment(this.dateTime, this.$t('defaultDatetime')).format(this.$t("fullDateTime")) : null;
       } else {
-        console.log(`this.locale (l-380): ${this.locale}`);
-        console.log(`this.date (l-381): ${this.date}`);
-        datetime = this.date ? moment(this.date, this.$t("fullDate")) : null;
+        return this.date ? moment(this.date, this.$t('defaultDate')).format(this.$t("fullDate")) : null;
       }
-
-      console.log(`this.datetime (l-387): ${this.dateTime}`);
-
-      return datetime;
     },
     checkOutwardDate() {
       if (this.outwardDateClicked && !this.regular && !this.date && !this.punctualDateOptional) {
@@ -409,16 +397,6 @@ export default {
       this.destination = this.initDestination;
     }
   },
-  mounted() {
-    console.log(this.initOutwardDate);
-    console.log(this.initOutwardTime);
-    console.log(
-      'mounted',
-      `this.locale (l-419): ${this.locale}`,
-      `this.datetime (l-420): ${this.dateTime}`,
-      `this.date (l-421): ${this.dateTime}`
-    );
-  },
   created() {
     this.setMomentLocale();
     switch (this.type) {
@@ -431,21 +409,9 @@ export default {
     default:
       this.showDate = true;
     }
-    console.log(
-      'mounted',
-      `this.locale (l-438): ${this.locale}`,
-      `this.datetime (l-439): ${this.dateTime}`,
-      `this.date (l-440): ${this.dateTime}`
-    );
   },
   beforeUpdate() {
     this.setMomentLocale();
-    console.log(
-      'mounted',
-      `this.locale (l-447): ${this.locale}`,
-      `this.datetime (l-448): ${this.dateTime}`,
-      `this.date (l-449): ${this.dateTime}`
-    );
   },
   methods: {
     setMomentLocale() {
