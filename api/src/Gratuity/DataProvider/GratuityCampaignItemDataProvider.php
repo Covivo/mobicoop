f1cd0bcd3ab02d26b5eed493c1eaa68159604b27
Max <maxime.bardot@gmail.com>
2023-11-22 12:02:48 +0100

    data providers

---
+++ api/src/Gratuity/DataProvider/GratuityCampaignItemDataProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright (c) 2023, MOBICOOP. All rights reserved.
+ * This project is dual licensed under AGPL and proprietary licence.
+ ***************************
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <gnu.org/licenses>.
+ ***************************
+ *    Licence MOBICOOP described in the file
+ *    LICENSE
+ */
+
+namespace App\Gratuity\DataProvider;
+
+use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
+use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
+use App\Gratuity\Resource\GratuityCampaign;
+
+/**
+ * @author Maxime Bardot <maxime.bardot@mobicoop.org>
+ */
+final class GratuityCampaignItemDataProvider implements RestrictedDataProviderInterface, ItemDataProviderInterface
+{
+    public function __construct() {}
+
+    public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
+    {
+        return GratuityCampaign::class === $resourceClass && 'get' === $operationName;
+    }
+
+    public function getItem(string $resourceClass, $id, string $operationName = null, array $context = [])
+    {
+        echo $id;
+
+        exit;
+
+        return null;
+    }
+}
