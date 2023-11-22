f8d215d0b949ed583820a3a36e5765cf8c11c172
Max <maxime.bardot@gmail.com>
2023-11-22 12:07:21 +0100

    data persister

---
+++ api/src/Gratuity/DataPersister/GratuityCampaignDataPersister.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright (c) 2023, MOBICOOP. All rights reserved.
+ * This project is dual licensed under AGPL and proprietary licence.
+ ***************************
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <gnu.org/licenses>.
+ ***************************
+ *    Licence MOBICOOP described in the file
+ *    LICENSE
+ */
+
+namespace App\Gratuity\DataPersister;
+
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
+use App\Gratuity\Resource\GratuityCampaign;
+use Symfony\Component\Security\Core\Security;
+
+/**
+ * @author Maxime Bardot <maxime.bardot@mobicoop.org>
+ */
+final class GratuityCampaignDataPersister implements ContextAwareDataPersisterInterface
+{
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function supports($data, array $context = []): bool
+    {
+        return $data instanceof GratuityCampaign && isset($context['collection_operation_name']) && 'post' == $context['collection_operation_name'];
+    }
+
+    public function persist($data, array $context = [])
+    {
+        return $data;
+    }
+
+    public function remove($data, array $context = [])
+    {
+        // call your persistence layer to delete $data
+    }
+}
