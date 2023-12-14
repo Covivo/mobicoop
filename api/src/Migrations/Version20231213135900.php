1aa8b4f3c4b440dbfafd5a252327c3df00b5e484
Maxime Bardot <maxime.bardot@gmail.com>
2023-12-14 14:40:05 +0100

    fix migration

--- api/src/Migrations/Version20231213135900.php
+++ api/src/Migrations/Version20231213135900.php
@@ -17,8 +17,6 @@ final class Version20231213135900 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE gratuity_notification DROP notified_date');
-
         $this->addSql('UPDATE mobconnect__long_distance_subscription SET version = 0');
         $this->addSql('UPDATE mobconnect__short_distance_subscription SET version = 0');
 
@@ -31,7 +29,6 @@ final class Version20231213135900 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE gratuity_notification ADD notified_date DATETIME DEFAULT NULL');
         $this->addSql('ALTER TABLE mobconnect__long_distance_subscription CHANGE version version SMALLINT DEFAULT NULL');
         $this->addSql('ALTER TABLE mobconnect__short_distance_subscription CHANGE version version SMALLINT DEFAULT NULL');
     }
