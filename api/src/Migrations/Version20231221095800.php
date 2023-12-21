405f82205d3a28478c8902f5c2cc72ce70c4e201
Maxime Bardot <maxime.bardot@gmail.com>
2023-12-21 10:07:30 +0100

    #7094 migration to create HITCHHIKING roles

---
+++ api/src/Migrations/Version20231221095800.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Import item migration.
+ */
+final class Version20231221095800 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // #7094 - create hitchhiking roles
+
+        $this->addSql("
+        INSERT INTO `auth_item` (`id`, `auth_rule_id`, `type`, `name`, `description`) VALUES
+        (318, NULL, 2, 'ROLE_HITCHHIKING_WATCHER', 'List and read hitchhiking users'),
+        (319, NULL, 2, 'ROLE_HITCHHIKING_MANAGER', 'List, read, create and edit hitchiking users'),
+        (320, NULL, 2, 'ROLE_HITCHHIKING_ADMINISTRATOR', 'All operations related to hitchhiking including identity proof validation')
+        ");
+
+        $this->addSql('
+        INSERT INTO `auth_item_child` (`parent_id`, `child_id`) VALUES
+        (318, 309),
+        (319, 318),
+        (319, 308),
+        (320, 319),
+        (320, 304)
+        ');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+    }
+}
