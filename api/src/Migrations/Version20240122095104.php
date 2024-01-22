261f46f5bc54e2a3944bf13def59fdca69ba8130
Remi Wortemann <remi.wortemann@gmail.com>
2024-01-22 14:11:08 +0100

    evol #7351

---
+++ api/src/Migrations/Version20240122095104.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240122095104 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE carpool_proof ADD driver_phone_unique_id VARCHAR(255) DEFAULT NULL, ADD passenger_phone_unique_id VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE carpool_proof DROP driver_phone_unique_id, DROP passenger_phone_unique_id');
+    }
+}
