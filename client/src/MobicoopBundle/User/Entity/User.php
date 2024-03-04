37ab59a5ebce157479c403bd55eaae6a6f98178b
Remi Wortemann <remi.wortemann@gmail.com>
2024-03-04 10:59:33 +0100

    update entity user bundle side

--- client/src/MobicoopBundle/User/Entity/User.php
+++ client/src/MobicoopBundle/User/Entity/User.php
@@ -576,6 +576,21 @@ class User extends GamificationEntity implements ResourceInterface, UserInterfac
      */
     private $gratuityNotifications;
 
+    /**
+     * @var null|string the email of the user's legal guardian
+     *
+     * @Assert\Email()
+     * @Groups({"post","put"})
+     */
+    private $legalGuardianEmail;
+
+    /**
+     * @var null|\DateTimeInterface Date of the parental consent
+     *
+     * @Groups({"post","put"})
+     */
+    private $parentalConsentDate;
+
     public function __construct($id = null, $status = null)
     {
         if ($id) {
@@ -1728,6 +1743,30 @@ class User extends GamificationEntity implements ResourceInterface, UserInterfac
         return $this;
     }
 
+    public function getLegalGuardianEmail(): ?string
+    {
+        return $this->legalGuardianEmail;
+    }
+
+    public function setLegalGuardianEmail(?string $legalGuardianEmail): self
+    {
+        $this->legalGuardianEmail = $legalGuardianEmail;
+
+        return $this;
+    }
+
+    public function getParentalConsentDate(): ?\DateTimeInterface
+    {
+        return $this->parentalConsentDate;
+    }
+
+    public function setParentalConsentDate(?\DateTimeInterface $parentalConsentDate): self
+    {
+        $this->parentalConsentDate = $parentalConsentDate;
+
+        return $this;
+    }
+
     // If you want more info from user you just have to add it to the jsonSerialize function
     public function jsonSerialize()
     {
@@ -1787,6 +1826,8 @@ class User extends GamificationEntity implements ResourceInterface, UserInterfac
             'gratuity' => $this->hasGratuity(),
             'gratuityNotifications' => $this->getGratuityNotifications(),
             'phoneCode' => $this->getPhoneCode(),
+            'legalGuardianEmail' => $this->getLegalGuardianEmail(),
+            'parentalConsentDate' => $this->getParentalConsentDate(),
         ];
 
         if (!is_null($this->getIsCommunityReferrer())) {
