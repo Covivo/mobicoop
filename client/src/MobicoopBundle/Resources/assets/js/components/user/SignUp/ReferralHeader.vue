431b94456cc6e1d0a770c1c162704d8be339f3c1
Max <maxime.bardot@gmail.com>
2024-03-13 10:18:47 +0100

    v-alert and translation with key word of the referral

--- client/src/MobicoopBundle/Resources/assets/js/components/user/SignUp/ReferralHeader.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/user/SignUp/ReferralHeader.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>{{ referral }}</div>
+  <v-alert
+    type="info"
+    color="primary"
+  >
+    {{ $t(referral) }}
+  </v-alert>
 </template>
 <script>
 import {messages_en, messages_fr, messages_eu, messages_nl} from "@translations/components/user/ReferralHeader/";
@@ -13,9 +18,9 @@ export default {
   i18n: {
     messages: {
       'en': messages_en,
-      'nl': messages_fr,
-      'fr': messages_eu,
-      'eu': messages_nl
+      'nl': messages_nl,
+      'fr': messages_fr,
+      'eu': messages_eu
     },
   },
 }
