219086bebfd633d09bc270ae829ef09c717a8d61
Max <maxime.bardot@gmail.com>
2024-03-13 09:58:53 +0100

    translation files

--- client/src/MobicoopBundle/Resources/assets/js/components/user/SignUp/ReferralHeader.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/user/SignUp/ReferralHeader.vue
@@ -2,12 +2,21 @@
   <div>{{ referral }}</div>
 </template>
 <script>
+import {messages_en, messages_fr, messages_eu, messages_nl} from "@translations/components/user/ReferralHeader/";
 export default {
   props: {
     referral: {
       type: String,
       default: null,
     }
-  }
+  },
+  i18n: {
+    messages: {
+      'en': messages_en,
+      'nl': messages_fr,
+      'fr': messages_eu,
+      'eu': messages_nl
+    },
+  },
 }
 </script>
