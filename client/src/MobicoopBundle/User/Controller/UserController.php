91773afe52dde735c2d71b1b81887fe06a6cc53b
Worti54 <remi.wortemann@gmail.com>
2024-03-14 16:58:44 +0100

    add new route to access new parental consent page

--- client/src/MobicoopBundle/User/Controller/UserController.php
+++ client/src/MobicoopBundle/User/Controller/UserController.php
@@ -1824,6 +1824,13 @@ class UserController extends AbstractController
         return new JsonResponse($this->ceeSubscriptionManager->myEecSubscriptionsEligibility());
     }
 
+    public function parentalConsentRequest(string $email)
+    {
+        return $this->render('@Mobicoop/user/parentalConsent.html.twig', [
+            'legalGuardianEmail' => $email,
+        ]);
+    }
+
     private function mobileRedirect(string $host, string $path, array $params)
     {
         $redirectUri = $host.'/#/carpools/user/sso/'.$path.'?'.http_build_query($params, '', '&');
