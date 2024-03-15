449a366b230bfb2ef2b2842f1c5d6e9aaa4adb53
Maxime Bardot <maxime.bardot@gmail.com>
2024-03-15 11:04:33 +0100

    add id setter for CarpoolProof

--- api/src/Carpool/Entity/CarpoolProof.php
+++ api/src/Carpool/Entity/CarpoolProof.php
@@ -298,6 +298,13 @@ class CarpoolProof
      */
     private $passengerPhoneUniqueId;
 
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
