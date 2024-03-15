2127f25cdc4e7a75d08c7c8981f7bedca7607bd3
Worti54 <remi.wortemann@gmail.com>
2024-03-15 16:42:47 +0100

    add parental consent translation files

---
+++ client/src/MobicoopBundle/Resources/translations/components/user/ParentalConsent/index.js
@@ -0,0 +1,4 @@
+export {default as messages_fr} from './ParentalConsent_fr.json';
+export {default as messages_en} from './ParentalConsent_en.json';
+export {default as messages_eu} from './ParentalConsent_eu.json';
+export {default as messages_nl} from './ParentalConsent_nl.json';
