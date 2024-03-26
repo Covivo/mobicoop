57ed0132b040b3e96c4a49de00054e5ef90fceb4
Olivier Fillol <olivier.fillol@mobicoop.org>
2024-03-26 10:11:48 +0100

    Fix Delivery::campaign

--- api/src/MassCommunication/Entity/Delivery.php
+++ api/src/MassCommunication/Entity/Delivery.php
@@ -19,22 +19,24 @@
  ***************************
  *    Licence MOBICOOP described in the file
  *    LICENSE
- **************************/
+ */
 
 namespace App\MassCommunication\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Annotation\ApiProperty;
-use Symfony\Component\Serializer\Annotation\MaxDepth;
-use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\User\Entity\User;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
 
 /**
  * A mass communication delivery related to a campaign.
  *
  * @ORM\Entity
+ *
  * @ORM\HasLifecycleCallbacks
+ *
  * @ApiResource(
  *      attributes={
  *          "normalization_context"={"groups"={"read_campaign"}, "enable_max_depth"="true"},
@@ -73,85 +75,101 @@ use App\User\Entity\User;
  */
 class Delivery
 {
-    const STATUS_PENDING = 0;
-    const STATUS_SENT = 1;
-    const STATUS_ERROR = 2;
+    public const STATUS_PENDING = 0;
+    public const STATUS_SENT = 1;
+    public const STATUS_ERROR = 2;
 
     /**
-     * @var int The id of this delivery.
+     * @var int the id of this delivery
      *
      * @ORM\Id
+     *
      * @ORM\GeneratedValue
+     *
      * @ORM\Column(type="integer")
+     *
      * @ApiProperty(identifier=true)
+     *
      * @Groups("read_campaign")
      */
     private $id;
 
     /**
-     * @var bool The status of the delivery.
+     * @var bool the status of the delivery
      *
      * @ORM\Column(type="smallint")
+     *
      * @Groups({"read_campaign","write_campaign"})
      */
     private $status;
 
     /**
-     * @var Campaign The campaign.
+     * @var Campaign the campaign
      *
-     * @ORM\ManyToOne(targetEntity="\App\MassCommunication\Entity\Campaign")
+     * @ORM\ManyToOne(targetEntity="\App\MassCommunication\Entity\Campaign", inversedBy="deliveries")
+     *
      * @ORM\JoinColumn(nullable=false, onDelete="CASCADE")
+     *
      * @Groups({"read_campaign","write_campaign","update_campaign"})
+     *
      * @MaxDepth(1)
      */
     private $campaign;
 
     /**
-     * @var User The user recipient.
+     * @var User the user recipient
      *
      * @ORM\ManyToOne(targetEntity="\App\User\Entity\User", inversedBy="deliveries")
+     *
      * @ORM\JoinColumn(nullable=false, onDelete="CASCADE")
+     *
      * @Groups({"read_campaign","write_campaign","update_campaign"})
+     *
      * @MaxDepth(1)
      */
     private $user;
 
     /**
-     * @var \DateTimeInterface Creation date.
+     * @var \DateTimeInterface creation date
      *
      * @ORM\Column(type="datetime", nullable=true)
+     *
      * @Groups({"read_campaign"})
      */
     private $createdDate;
 
     /**
-     * @var \DateTimeInterface Updated date.
+     * @var \DateTimeInterface updated date
      *
      * @ORM\Column(type="datetime", nullable=true)
+     *
      * @Groups({"read_campaign"})
      */
     private $updatedDate;
 
     /**
-     * @var \DateTimeInterface Sent date.
+     * @var \DateTimeInterface sent date
      *
      * @ORM\Column(type="datetime", nullable=true)
+     *
      * @Groups({"read_campaign","write_campaign"})
      */
     private $sentDate;
 
     /**
-     * @var \DateTimeInterface Received date.
+     * @var \DateTimeInterface received date
      *
      * @ORM\Column(type="datetime", nullable=true)
+     *
      * @Groups({"read_campaign","write_campaign"})
      */
     private $receivedDate;
 
     /**
-     * @var \DateTimeInterface Read date.
+     * @var \DateTimeInterface read date
      *
      * @ORM\Column(type="datetime", nullable=true)
+     *
      * @Groups({"read_campaign","write_campaign"})
      */
     private $readDate;
@@ -167,11 +185,11 @@ class Delivery
     {
         return $this->id;
     }
-    
+
     public function setId(int $id): self
     {
         $this->id = $id;
-        
+
         return $this;
     }
 
@@ -191,11 +209,11 @@ class Delivery
     {
         return $this->campaign;
     }
-    
+
     public function setCampaign(?Campaign $campaign): self
     {
         $this->campaign = $campaign;
-        
+
         return $this;
     }
 
@@ -203,11 +221,11 @@ class Delivery
     {
         return $this->user;
     }
-    
+
     public function setUser(?User $user): self
     {
         $this->user = $user;
-        
+
         return $this;
     }
 
@@ -272,7 +290,7 @@ class Delivery
     }
 
     // DOCTRINE EVENTS
-    
+
     /**
      * Creation date.
      *
@@ -280,7 +298,7 @@ class Delivery
      */
     public function setAutoCreatedDate()
     {
-        $this->setCreatedDate(new \Datetime());
+        $this->setCreatedDate(new \DateTime());
     }
 
     /**
@@ -290,6 +308,6 @@ class Delivery
      */
     public function setAutoUpdatedDate()
     {
-        $this->setUpdatedDate(new \Datetime());
+        $this->setUpdatedDate(new \DateTime());
     }
 }
