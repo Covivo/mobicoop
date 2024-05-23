2b5acbcf65a22b132b0832f804fb5f8b33bf2e48
Worti54 <remi.wortemann@gmail.com>
2024-05-23 16:54:51 +0200

    update admin communityterritoryfilterextension

--- api/src/Community/Admin/Extension/CommunityTerritoryFilterExtension.php
+++ api/src/Community/Admin/Extension/CommunityTerritoryFilterExtension.php
@@ -19,59 +19,64 @@
  ***************************
  *    Licence MOBICOOP described in the file
  *    LICENSE
- **************************/
+ */
 
 namespace App\Community\Admin\Extension;
 
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
-use App\Community\Entity\Community;
-use App\Geography\Entity\Territory;
 use App\Auth\Service\AuthManager;
+use App\Community\Entity\Community;
+use App\Community\Entity\CommunityUser;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Security;
 
 /**
- * Extension used to limit the list of communities to the territories allowed for the requester (admin)
+ * Extension used to limit the list of communities to the territories allowed for the requester (admin).
  */
 final class CommunityTerritoryFilterExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
 {
     private $authManager;
+    private $security;
 
     public function __construct(Security $security, AuthManager $authManager, RequestStack $request)
     {
         $this->authManager = $authManager;
+        $this->security = $security;
     }
 
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
         // concerns only admin get collection
-        if ($resourceClass == Community::class && $operationName == "ADMIN_get") {
+        if (Community::class == $resourceClass && 'ADMIN_get' == $operationName) {
             $this->addWhere($queryBuilder, $resourceClass, false, $operationName);
         }
     }
 
     public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = [])
     {
-        return;
     }
 
     private function addWhere(QueryBuilder $queryBuilder, string $resourceClass, bool $isItem, string $operationName = null, array $identifiers = [], array $context = []): void
     {
         $territories = [];
+        $user = $this->security->getUser();
 
         // we check if the user has limited territories
-        $territories = $this->authManager->getTerritoriesForItem("community_list");
-        
-        if (count($territories)>0) {
+        $territories = $this->authManager->getTerritoriesForItem('community_list');
+
+        if (count($territories) > 0) {
             $rootAlias = $queryBuilder->getRootAliases()[0];
             $queryBuilder
-            ->leftJoin($rootAlias.".address", 'actfe')
-            ->leftJoin("actfe.territories", 'tctfe')
-            ->andWhere('tctfe.id in (:territories)')
-            ->setParameter('territories', $territories)
+                ->leftJoin($rootAlias.'.address', 'actfe')
+                ->leftJoin('actfe.territories', 'tctfe')
+                ->leftjoin($rootAlias.'.communityUsers', 'c')
+                ->andWhere('tctfe.id in (:territories) OR (c.user = :user AND c.status = :status)')
+                ->setParameter('territories', $territories)
+                ->setParameter('user', $user)
+                ->setParameter('status', CommunityUser::STATUS_ACCEPTED_AS_MODERATOR)
             ;
         }
     }
