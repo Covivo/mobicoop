fa7578f81efa41ba0ab86b50a3153856774e47a1
Olivier Fillol <olivier.fillol@mobicoop.org>
2024-06-13 11:23:04 +0200

    Removes Autorecommit command

--- api/src/Incentive/Service/Manager/SubscriptionManager.php
+++ api/src/Incentive/Service/Manager/SubscriptionManager.php
@@ -21,7 +21,6 @@ use App\Incentive\Resource\EecInstance;
 use App\Incentive\Service\LoggerService;
 use App\Incentive\Service\NotificationsPresenceChecker;
 use App\Incentive\Service\Provider\JourneyProvider;
-use App\Incentive\Service\Provider\SubscriptionProvider;
 use App\Incentive\Service\Stage\AutoRecommitSubscription;
 use App\Incentive\Service\Stage\CreateSubscription;
 use App\Incentive\Service\Stage\PatchSubscription;
@@ -244,25 +243,6 @@ class SubscriptionManager extends MobConnectManager
 
         $stage = new $commitClass($this->_em, $this->_timestampTokenManager, $this->_eecInstance, $subscription, $referenceObject, $pushOnlyMode);
         $stage->execute();
-    }
-
-    public function autoRecommitSubscriptions(): void
-    {
-        // Processing subscriptions that simply need to be reset
-        $sdSubscriptions = SubscriptionProvider::getSubscriptionsCanBeReset($this->_shortDistanceSubscriptionRepository->getSubscriptionsReadyToBeRecommited(), true);
-        $ldSubscriptions = SubscriptionProvider::getSubscriptionsCanBeReset($this->_longDistanceSubscriptionRepository->getSubscriptionsReadyToBeRecommited(), true);
-
-        foreach (array_merge($sdSubscriptions, $ldSubscriptions) as $subscription) {
-            $this->resetSubscription($subscription);
-        }
-
-        // Processing subscriptions that need to be recommit
-        $sdSubscriptions = SubscriptionProvider::getSubscriptionsCanBeReset($this->_shortDistanceSubscriptionRepository->getSubscriptionsReadyToBeRecommited());
-        $ldSubscriptions = SubscriptionProvider::getSubscriptionsCanBeReset($this->_longDistanceSubscriptionRepository->getSubscriptionsReadyToBeRecommited());
-
-        foreach (array_merge($sdSubscriptions, $ldSubscriptions) as $subscription) {
-            $this->recommitSubscription($subscription);
-        }
     }
 
     public function recommitSubscription($subscription): void
