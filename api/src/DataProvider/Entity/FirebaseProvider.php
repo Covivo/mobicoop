89828669dfeb29908eece905d2dbce7c3518d831
Max <maxime.bardot@gmail.com>
2024-09-10 11:08:15 +0200

    feat(Push): Add new push package for FCM

--- api/src/DataProvider/Entity/FirebaseProvider.php
+++ api/src/DataProvider/Entity/FirebaseProvider.php
@@ -19,68 +19,55 @@
  ***************************
  *    Licence MOBICOOP described in the file
  *    LICENSE
- **************************/
+ */
 
 namespace App\DataProvider\Entity;
 
 use App\Communication\Entity\Push;
 use App\DataProvider\Interfaces\ProviderInterface;
-use Fcm\FcmClient;
-use Fcm\Push\Notification;
+use phpFCMv1\Client;
+use phpFCMv1\Notification;
+use phpFCMv1\Recipient;
 
 /**
  * Firebase management service.
  *
  * @author Sylvain Briat <sylvain.briat@mobicoop.org>
- *
  */
 class FirebaseProvider implements ProviderInterface
 {
     private $client;
 
-    public function __construct(string $apiToken, string $senderId)
+    public function __construct($serviceAccount)
     {
-        $this->client = new FcmClient($apiToken, $senderId);
+        $this->client = new Client($serviceAccount);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function postCollection(Push $push)
     {
         $notification = new Notification();
-        $notification
-            ->addRecipient($push->getRecipientDeviceIds())
-            ->setTitle($push->getTitle())
-            ->setBody($push->getMessage());
-            
-        // send the notification
-        $this->client->send($notification);
-    
-        // todo : get the response and treat the bad device ids
 
+        $notification->setNotification($push->getTitle(), $push->getMessage());
+
+        // send the notification
+
+        foreach ($push->getRecipientDeviceIds() as $recipientId) {
+            $recipient = new Recipient();
+            $recipient->setSingleRecipient($recipientId);
+            $this->client->build($recipient, $notification);
+            $this->client->fire();
+        }
+
+        // todo : get the response and treat the bad device ids
         return new Response();
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getCollection(string $class, string $apikey, array $params)
-    {
-    }
+    public function getCollection(string $class, string $apikey, array $params) {}
 
-    /**
-    * {@inheritdoc}
-    */
-    public function getItem(string $class, string $apikey, array $params)
-    {
-    }
+    public function getItem(string $class, string $apikey, array $params) {}
 
-    /**
-    * {@inheritdoc}
-    */
     public function deserialize(string $class, array $data)
     {
-        $this->logger->info("Firebase API return");
+        $this->logger->info('Firebase API return');
     }
 }
