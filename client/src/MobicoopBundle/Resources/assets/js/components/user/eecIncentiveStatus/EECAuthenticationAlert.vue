b04b19fa86bd4d56f08d5421a968982410238d42
Olivier Fillol <olivier.fillol@mobicoop.org>
2024-09-25 09:58:28 +0200

    feat (eec authentication): add alert message when moB auth is invalid

---
+++ client/src/MobicoopBundle/Resources/assets/js/components/user/eecIncentiveStatus/EECAuthenticationAlert.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-alert
+    v-if="!eecSubscriptions.authenticationValidity"
+    class="mt-5"
+    outlined
+    type="warning"
+    :prominent="true"
+  >
+    Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Suspendisse non nisl sit amet velit
+    hendrerit rutrum. Nullam vel sem. Pellentesque dapibus hendrerit tortor.
+  </v-alert>
+</template>
+<script>
+import { merge } from "lodash";
+import {messages_en, messages_fr, messages_eu, messages_nl} from "@translations/components/user/EECIncentiveStatus/";
+import {messages_client_en, messages_client_fr, messages_client_eu, messages_client_nl} from "@clientTranslations/components/user/EECIncentiveStatus/";
+
+let MessagesMergedEn = merge(messages_en, messages_client_en);
+let MessagesMergedNl = merge(messages_nl, messages_client_nl);
+let MessagesMergedFr = merge(messages_fr, messages_client_fr);
+let MessagesMergedEu = merge(messages_eu, messages_client_eu);
+
+export default {
+  i18n: {
+    messages: {
+      'en': MessagesMergedEn,
+      'nl': MessagesMergedNl,
+      'fr': MessagesMergedFr,
+      'eu': MessagesMergedEu
+    }
+  },
+  props: {
+    eecSubscriptions: {
+      type: Object,
+      default: () => ({})
+    },
+  }
+}
+</script>
