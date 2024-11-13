b9d1a990dbebc23e68ef070e283ef5e94116d6b8
Olivier Fillol <olivier.fillol@mobicoop.org>
2024-11-13 10:08:21 +0100

    feat(toolbox): fix the layouts and add new property to control the links color

--- client/src/MobicoopBundle/Resources/assets/js/components/utilities/ToolBox/ToolBox.vue
+++ client/src/MobicoopBundle/Resources/assets/js/components/utilities/ToolBox/ToolBox.vue
@@ -52,9 +52,9 @@
           <v-col
             v-for="(item, i) in itemsVisual"
             :key="i"
-            cols="4"
+            cols="12"
+            md="6"
             lg="4"
-            md="8"
           >
             <ToolBoxItem
               :title="item.title"
