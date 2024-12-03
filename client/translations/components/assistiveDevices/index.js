a17c922e6938dab533e5040e20439b85580f1acf
Worti54 <remi.wortemann@gmail.com>
2024-12-03 10:21:37 +0100

    evol #8734 mobicoop part

---
+++ client/translations/components/assistiveDevices/index.js
@@ -0,0 +1,4 @@
+export {default as messages_client_fr} from './AssistiveDevices_fr.json';
+export {default as messages_client_en} from './AssistiveDevices_en.json';
+export {default as messages_client_eu} from './AssistiveDevices_eu.json';
+export {default as messages_client_nl} from './AssistiveDevices_nl.json';
